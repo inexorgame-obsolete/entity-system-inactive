13ed4e41fbe06874ff5ee2837cd1a9cd1d034d30
IAmNotHanni <cmdfreak@web.de>
2018-07-17 16:02:20 +0200

    Implemented test: create 1 million entities. I forgot to push main.cpp.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,35 @@
 using namespace std;
 
 // entity system
-#include "entsys\EntSys.hpp"
+#include "entsys\EntitySystem.hpp"
 using namespace inexor;
+using namespace entsys;
 
 // create singleton instance
 // a singleton class can only be instanced once!
-entsys::CEntitySystem* sys = nullptr;
+// TODO: why does placing this in EntitySystem.hpp cause a linker error?
+EntitySystem* sys = EntitySystem::create_entity_system();
 
 int main()
 {
-    // initialize singleton instance
-    sys = entsys::CEntitySystem::create_instance();
-    entsys::CEntityBase new_entity(0.0f, 0.0f, 0.0f);    
-    sys->add_entity(new_entity);
+    EntityTypeBase spawnpoint(std::string("spawnpoint"));
+    sys->create_entity_type(spawnpoint);
+
+    cout << "numer of entity types available: " << sys->get_entity_type_cound() << endl;
+
+    EntityTypeBase i_am_duplicate(std::string("spawnpoint"));
+    sys->create_entity_type(i_am_duplicate);
+
+    cout << "numer of entity types available: " << sys->get_entity_type_cound() << endl;
+
+    // create 1 million entities
+    for(size_t i=0; i<1000000; i++)
+    {
+        EntityTypeBase i_am_duplicate(std::string(std::to_string(i)));
+        sys->create_entity_type(i_am_duplicate);
+    }
+
+    cout << "numer of entity types available: " << sys->get_entity_type_cound() << endl;
+
     return 0;
 }
