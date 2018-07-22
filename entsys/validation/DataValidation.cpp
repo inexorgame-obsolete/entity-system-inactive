64759e362a16f54a8095cdc40c43e2dae0472ee1
IAmNotHanni <cmdfreak@web.de>
2018-07-22 13:56:57 +0200

    Added data validation class.

---
+++ entsys/validation/DataValidation.cpp
@@ -0,0 +1,24 @@
+// Inexor entity system prototype
+// (c)2018 Inexor
+
+#include "DataValidation.hpp"
+
+namespace inexor {
+namespace entsys {
+
+    DataValidation::DataValidation()
+    {
+    }
+
+    DataValidation::~DataValidation()
+    {
+    }
+
+    
+    bool DataValidation::isvalid()
+    {
+        return data_is_valid;
+    }
+
+};
+};
