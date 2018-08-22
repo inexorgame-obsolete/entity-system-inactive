71fa68d05a4d82651f86acbbe3ca4e5e1f302ca2
IAmNotHanni <cmdfreak@web.de>
2018-08-22 23:55:32 +0200

    Added Google Test project.

---
+++ entsys/tests/inexor-entity-system-tests/test.cpp
@@ -0,0 +1,11 @@
+// Inexor entity system prototype
+// Automatic software tests using Google Test
+// (c)2018 Inexor
+
+#include "gtest/gtest.h"
+
+// Example test
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
