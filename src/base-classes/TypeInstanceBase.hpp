11ecbe4fa2bc97778b1bf863e0411f2feef6ab59
IAmNotHanni <cmdfreak@web.de>
2018-12-09 00:15:44 +0100

    Certain methods in TypeInstanceBase should not be public!

--- src/base-classes/TypeInstanceBase.hpp
+++ src/base-classes/TypeInstanceBase.hpp
@@ -23,6 +23,23 @@ namespace entity_system {
 			/// A pointer to the type of this type instance.
 			std::shared_ptr<T1> type_pointer;
 
+			/// Sets the name of the type.
+			/// @param type_name a const reference to
+			/// a std::string which is the name of this type.
+			void set_type_name(const std::string& type_name)
+			{
+				type_title = type_name;
+			}
+
+
+			/// Sets the pointer of a type.
+			/// @param type_ptr A const reference to a shared pointer of the source type.
+			void set_type_pointer(const std::shared_ptr<T1>& type_ptr)
+			{
+				type_pointer = type_ptr;
+			}
+
+
 		public:
 
 			/// Constructor.
@@ -45,23 +62,6 @@ namespace entity_system {
 			}
 
 
-			/// Sets the name of the type.
-			/// @param type_name a const reference to
-			/// a std::string which is the name of this type.
-			void set_type_name(const std::string& type_name)
-			{
-				type_title = type_name;
-			}
-
-
-			/// Sets the pointer of a type.
-			/// @param type_ptr A const reference to a shared pointer of the source type.
-			void set_type_pointer(const std::shared_ptr<T1>& type_ptr)
-			{
-				type_pointer = type_ptr;
-			}
-
-			
 			// TODO: Implement UUID get methods.
 
 	};
