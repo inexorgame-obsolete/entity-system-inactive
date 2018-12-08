cd5910c099f60790eb63b6a4108d65be347a153d
IAmNotHanni <cmdfreak@web.de>
2018-12-09 00:49:36 +0100

    Removed old tests.

--- tests/test-components/data-container-tests/constructor-tests/DataContainer_ConstructorTests.hpp
+++
@@ -1,7 +0,0 @@
-// Inexor entity system prototype
-// Automatic software tests using Google Test
-// (c)2018 Inexor
-
-#pragma once
-
-#include "components/DataContainer_ConstructorTests.hpp"
--- tests/test-components/manager-tests/entity-attribute-type-instance-manager-tests/EntityAttributeTypeInstanceManagerTests.hpp
+++
@@ -1,5 +0,0 @@
-// Inexor entity system prototype
-// Automatic software tests using Google Test
-// (c)2018 Inexor
-
-#pragma once
