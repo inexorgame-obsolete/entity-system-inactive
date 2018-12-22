322ef9f1814a7ceb6a72aefa783fcbe1f09fc62f
IAmNotHanni <cmdfreak@web.de>
2018-12-22 22:02:45 +0100

    Remove example code. We should implement google tests instead!

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,8 @@
 using namespace inexor::entity_system;
 
 
-// ENTITY-SYSTEM EXAMPLES
-#include "../examples/examples.hpp"
-
-
 int main(int argc, char* argv[])
 {
-	// Run entity-system examples.
-	run_entsys_examples();
-	// TODO: Move tests from here into a separate gtest project.
-	// TODO: Setup google testing framework with CMake.
-
 	std::cin.get();
     return 0;
 }
