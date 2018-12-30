f29b9fb46b51968647ff7b1f56ae6a641fc0eaa8
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-12-30 22:44:27 +0100

    [grpc] Add gRPC setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 // Inexor entity system prototype
 // (c)2018 Inexor
 
+#include <iostream>
+#include <memory>
+#include <grpcpp/grpcpp.h>
+
 #include "EntitySystem.hpp"
+#include "generated/entity-system.grpc.pb.h"
+
 using namespace inexor::entity_system;
 
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
 
 int main(int argc, char* argv[])
 {
-	std::cin.get();
+    std::cin.get();
     return 0;
 }
