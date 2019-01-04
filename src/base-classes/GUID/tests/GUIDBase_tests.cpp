58779c8fd01297f6272c85f7e8853d79162d06b1
IAmNotHanni <cmdfreak@web.de>
2019-01-04 21:32:54 +0100

    Move tests to another folder.

--- src/base-classes/GUID/tests/GUIDBase_tests.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(GUIDBase, some_test_variation_name) {
-    EXPECT_EQ(0, 0) << "Expected the guid base to ...";
-}
-
--- src/base-classes/tests/TypeBase_tests.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(TypeBase, some_test_variation_name) {
-    EXPECT_EQ(0, 0) << "Expected the attribute base to ...";
-}
-
