56ac86ef3f08bbb391048f109b6d3494d8ec276b
IAmNotHanni <cmdfreak@web.de>
2019-01-24 20:53:23 +0100

    Fix https://github.com/inexorgame/entity-system/issues/77: Pass REST-API messages to spdlog.

--- src/entity-system-rest/RestServerLogger.hpp
+++ src/entity-system-rest/RestServerLogger.hpp
@@ -18,37 +18,53 @@ using namespace std;
 using namespace restbed;
 
 #define LOGGER_ENTITY_REST "inexor.entity.rest"
+#define INEXOR_REST_API_MAX_MESSAGE_SIZE 4096
 
 namespace inexor {
 namespace entity_system {
 
-	class RestServerLogger: public Logger {
+	class RestServerLogger: public Logger
+    {
 		public:
 			RestServerLogger() {};
 			virtual ~RestServerLogger() {};
 			void stop( void ) { return; };
 			void start( const shared_ptr< const Settings >& ) { return; };
+
+
+            /// Write down a server message in spdlog.
 			void log(const Level level, const char* format, ...)
 			{
+                char spdlog_message[INEXOR_REST_API_MAX_MESSAGE_SIZE];
 
-	            va_list arguments;
-	            va_start( arguments, format );
-	             vfprintf( stderr, format, arguments );
-	             fprintf( stderr, "\n" );
-	            va_end( arguments );
+                va_list log_arguments;
+                va_start(log_arguments, format);
+                std::vsnprintf(spdlog_message, INEXOR_REST_API_MAX_MESSAGE_SIZE, format, log_arguments);
+                va_end(format);
 
-//	            va_list arguments;
-//				va_start( arguments, format );
-//				switch (level) {
-//					case restbed::Logger::DEBUG:
-//						spdlog::get(LOGGER_ENTITY_REST)->debug(fmt::sprintf(format, arguments));
-//						break;
-//					default:
-//						spdlog::get(LOGGER_ENTITY_REST)->trace(format, arguments);
-//						break;
-//				}
-//				va_end( arguments );
+                auto spdlog_handle = spdlog::get(LOGGER_ENTITY_REST);
+
+                switch(level)
+                {
+                    case restbed::Logger::INFO:
+                        spdlog_handle->info(spdlog_message);
+                        break;
+                    case restbed::Logger::DEBUG:
+                        spdlog_handle->debug(spdlog_message);
+                        break;
+                    case restbed::Logger::FATAL:
+                    case restbed::Logger::SECURITY: // Security messages are seen as critical errors!
+                        spdlog_handle->critical(spdlog_message);
+                        break;
+                    case 3000: // ERROR
+                        spdlog_handle->error(spdlog_message);
+                        break;
+                    case restbed::Logger::WARNING:
+                        spdlog_handle->warn(spdlog_message);
+                        break;
+                }
 			};
+
 
 			void log_if(bool expression, const Level level, const char* format, ...)
 			{
