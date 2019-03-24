1f0488ce5c3f24fbfe412d6d1c3fa2f8d1aa4c0b
IAmNotHanni <cmdfreak@web.de>
2019-03-24 15:03:59 +0100

    Documentation of ConfigurationModule.

--- src/configuration/ConfigurationModule.hpp
+++ src/configuration/ConfigurationModule.hpp
@@ -8,7 +8,8 @@
 namespace inexor {
 namespace configuration {
 
-    /// Module Configuration
+    /// @class ConfigurationModule
+    /// @brief Module Configuration.
 	class ConfigurationModule
 	{
 		using ConfigurationManangerPtr = std::shared_ptr<ConfigurationManager>;
@@ -18,6 +19,12 @@ namespace configuration {
 
 		public:
 
+            /// @brief Constructor.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param configuration_manager The configuration manager.
+            /// @param command_line_arguments_configuration_initializer The initializer for the command line arguments.
+            /// @param env_var_configuration_initializer The initializer for environment variables.
+            /// @param system_configuration_initializer The initializer for system information.
 			ConfigurationModule(
 				ConfigurationManangerPtr configuration_manager,
 				CommandLineArgumentsConfigurationInitializerPtr command_line_arguments_configuration_initializer,
@@ -25,23 +32,26 @@ namespace configuration {
 				SystemConfigurationInitializerPtr system_configuration_initializer
 			);
 
+            /// Destructor.
 			~ConfigurationModule();
 
 			/// Initialization of the configuration module
+            /// @param argc The total number of parameters given to the application.
+            /// @param argv An array of parameters given to the application.
 			void init(int argc, char* argv[]);
 
 		private:
 
-			/// The configuration manager
+			/// The configuration manager.
 			ConfigurationManangerPtr configuration_manager;
 
-			/// The initializer for the command line arguments
+			/// The initializer for the command line arguments.
 			CommandLineArgumentsConfigurationInitializerPtr command_line_arguments_configuration_initializer;
 
-			/// The initializer for environment variables
+			/// The initializer for environment variables.
 			EnvVarConfigurationInitializerPtr env_var_configuration_initializer;
 
-			/// The initializer for system information
+			/// The initializer for system information.
 			SystemConfigurationInitializerPtr system_configuration_initializer;
 
 	};
