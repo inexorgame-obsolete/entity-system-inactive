20c3e320f85df80d797adc5ec45ba0745d8e12bc
IAmNotHanni <cmdfreak@web.de>
2019-03-24 14:56:47 +0100

    Documentation of CommandModule.

--- src/command/CommandModule.hpp
+++ src/command/CommandModule.hpp
@@ -5,25 +5,30 @@
 namespace inexor {
 namespace command {
 
-    /// Module Command
+    /// @class CommandModule
+    /// @brief Command module.
 	class CommandModule
 	{
 		using CommandManangerPtr = std::shared_ptr<CommandManager>;
 
 		public:
 
+            /// @brief Constructor.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param command_manager The command manager.
 			CommandModule(
 				CommandManangerPtr command_manager
 			);
 
+            /// Destructor.
 			~CommandModule();
 
-			/// Initialization of the command module
+			/// Initialization of the command module.
 			void init();
 
 		private:
 
-			/// The command manager
+			/// The command manager.
 			CommandManangerPtr command_manager;
 
 	};
