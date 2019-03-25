f5c01a5807ac989a6d325478726c73ec08470016
IAmNotHanni <cmdfreak@web.de>
2019-03-25 13:00:38 +0100

    Documentation of RelationAttributeType.

--- src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp
+++ src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp
@@ -19,24 +19,24 @@ namespace inexor {
 namespace entity_system {
     
 
-	/// A base class for entity relation attribute types.
+    /// @class RelationAttributeType
+	/// @brief A base class for entity relation attribute types.
     class RelationAttributeType : public TypeBase, public AttributeBase, public GUIDBase
     {
         public:
 
-			/// Constructor.
+			/// @brief Constructor.
+            /// @note The dependencies of this class will be injected automatically.
+			/// @note The GUID of the new relation attribute type will be created automatically by the inheritance of GUIDBase!
 			/// @param rel_attr_type_name The name of the new relation attribute type.
 			/// @param rel_attr_type_data_type The data type of the new relation attribute type.
 			/// @param rel_attr_type_features The features of the new relation attribute type.
-			/// @note the GUID of the new relation attribute type will
-			/// be created automatically by the inheritance of GUIDBase!
-			RelationAttributeType(const std::string&, const DataType&, const EnumSet<Feature>&);
+			RelationAttributeType(const std::string& rel_attr_type_name, const DataType& rel_attr_type_data_type, const EnumSet<Feature>& rel_attr_type_features);
 
 			/// Destructor.
 			~RelationAttributeType();
 
-			/// Implement a data validation method as
-			/// required by class inheritance of DataValidation!
+			/// Implement a data validation method as required by class inheritance of DataValidation!
 			virtual ENTSYS_DATA_VALIDATION_RESULT validate() override;
 
 	};
