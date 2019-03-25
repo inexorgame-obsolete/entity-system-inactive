c467ec44320806829d375c73784ba01c5568a692
IAmNotHanni <cmdfreak@web.de>
2019-03-25 13:12:47 +0100

    Documentation of EntityAttributeType.

--- src/entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.hpp
+++ src/entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.hpp
@@ -11,32 +11,31 @@
 namespace inexor {
 namespace entity_system {
 
-    /// @class EntityAttributeType.
+    /// @class EntityAttributeType
 	/// @brief A base class for entity attribute types.
     class EntityAttributeType : public TypeBase, public AttributeBase, public GUIDBase
     {
         public:
 				
 			/// @brief Constructor.
+			/// @note the GUID of the new entity attribute type will be created automatically by the inheritance of GUIDBase!
 			/// @param ent_attr_type_name The name of the new entity attribute type.
 			/// @param ent_attr_type_datatype The data type of the new entity attribute type.
 			/// @param ent_attr_type_features The features of the new entity attribute type.
-			/// @note the GUID of the new entity attribute type will be created automatically by the inheritance of GUIDBase!
-			EntityAttributeType(const std::string&, const DataType&, const EnumSet<Feature>&);
+			EntityAttributeType(const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features);
 
             /// @brief Constructor
+			/// @note the GUID of the new entity attribute type will be created automatically by the inheritance of GUIDBase!
             /// @param ent_attr_type_GUID The GUID of the new entity attribute type.
 			/// @param ent_attr_type_name The name of the new entity attribute type.
 			/// @param ent_attr_type_datatype The data type of the new entity attribute type.
 			/// @param ent_attr_type_features The features of the new entity attribute type.
-			/// @note the GUID of the new entity attribute type will be created automatically by the inheritance of GUIDBase!
-            EntityAttributeType(const xg::Guid&, const std::string&, const DataType&, const EnumSet<Feature>&);
+            EntityAttributeType(const xg::Guid& ent_attr_type_GUID, const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features);
 
-			/// @brief Destructor.
+			/// Destructor.
 			~EntityAttributeType();
 
-			/// Implement a data validation method as
-			/// required by class inheritance of DataValidation!
+			/// Implement a data validation method as required by class inheritance of DataValidation!
 			virtual ENTSYS_DATA_VALIDATION_RESULT validate() override;
 
 	};
