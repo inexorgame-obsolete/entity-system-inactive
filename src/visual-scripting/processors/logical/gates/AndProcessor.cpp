a6836bfacaa9a3f30d180c4e56b14a966d6237a0
IAmNotHanni <cmdfreak@web.de>
2019-03-25 20:54:05 +0100

    Documentation of AndProcessor.

--- src/visual-scripting/processors/logical/gates/AndProcessor.cpp
+++ src/visual-scripting/processors/logical/gates/AndProcessor.cpp
@@ -46,10 +46,12 @@ namespace visual_scripting {
 	void AndProcessor::make_signals(const EntityInstancePtr& entity_instance)
 	{
 		spdlog::get(LOGGER_NAME)->debug("Initializing processor AND for newly created entity instance {} of type {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name());
-		auto o_and_input_1 = entity_instance->get_attribute_instance(AndEntityTypeProvider::AND_INPUT_1);
+		
+        auto o_and_input_1 = entity_instance->get_attribute_instance(AndEntityTypeProvider::AND_INPUT_1);
 		auto o_and_input_2 = entity_instance->get_attribute_instance(AndEntityTypeProvider::AND_INPUT_2);
 		auto o_and_result = entity_instance->get_attribute_instance(AndEntityTypeProvider::AND_RESULT);
-		if (o_and_input_1.has_value() && o_and_input_2.has_value() && o_and_result.has_value())
+		
+        if(o_and_input_1.has_value() && o_and_input_2.has_value() && o_and_result.has_value())
 		{
 			signals[entity_instance->get_GUID()] = MakeSignal(
 				With(
@@ -62,7 +64,9 @@ namespace visual_scripting {
 				}
 			);
 			o_and_result.value()->value = signals[entity_instance->get_GUID()];
-		} else {
+		}
+        else
+        {
 			spdlog::get(LOGGER_NAME)->error("Failed to initialize processor signals for entity instance {} of type {}: Missing one of these attributes: {} {} {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name(), AndEntityTypeProvider::AND_INPUT_1, AndEntityTypeProvider::AND_INPUT_2, AndEntityTypeProvider::AND_RESULT);
 		}
 	}
