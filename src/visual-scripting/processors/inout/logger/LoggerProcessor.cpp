6fbfe95a191782ddf87bdfc3fef9279df2fafe18
IAmNotHanni <cmdfreak@web.de>
2019-03-25 22:01:40 +0100

    Documentation of LoggerProcessor.

--- src/visual-scripting/processors/inout/logger/LoggerProcessor.cpp
+++ src/visual-scripting/processors/inout/logger/LoggerProcessor.cpp
@@ -54,15 +54,19 @@ namespace visual_scripting {
 	void LoggerProcessor::make_signals(const EntityInstancePtr& entity_instance)
 	{
 		logger->debug("Initializing processor LOGGER for newly created entity instance {} of type {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name());
-		auto o_logger_name = entity_instance->get_attribute_instance(LoggerEntityTypeProvider::LOGGER_NAME);
+		
+        auto o_logger_name = entity_instance->get_attribute_instance(LoggerEntityTypeProvider::LOGGER_NAME);
 		auto o_log_message = entity_instance->get_attribute_instance(LoggerEntityTypeProvider::LOG_MESSAGE);
-		if (o_logger_name.has_value() && o_log_message.has_value())
+		
+        if(o_logger_name.has_value() && o_log_message.has_value())
 		{
 			std::string logger_name = std::get<DataType::STRING>(o_logger_name.value()->value.Value());
 			observers[entity_instance->get_GUID()] = Observe(o_log_message.value()->value, [logger_name] (DataValue log_message) {
 				spdlog::get(logger_name)->info(std::get<DataType::STRING>(log_message));
 			});
-		} else {
+		}
+        else
+        {
 			logger->error("Failed to initialize processor signals for entity instance {} of type {}: Missing one of these attributes {} {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name(), LoggerEntityTypeProvider::LOGGER_NAME, LoggerEntityTypeProvider::LOG_MESSAGE);
 		}
 	}
