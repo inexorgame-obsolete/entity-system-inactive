20396d5aba2fd1e238a761079a63c1a694145261
IAmNotHanni <cmdfreak@web.de>
2019-03-25 23:23:53 +0100

    Documentation of TimerProcessors.

--- src/visual-scripting/processors/generators/timers/TimerProcessors.hpp
+++ src/visual-scripting/processors/generators/timers/TimerProcessors.hpp
@@ -7,16 +7,20 @@ namespace visual_scripting {
 
 	using TimerTickProcessorPtr = std::shared_ptr<TimerTickProcessor>;
 
-	/// Provides processors for timers.
+    /// @class TimerProcessors
+	/// @brif Provides processors for timers.
 	class TimerProcessors
 	{
 		public:
 
-			/// Constructs the processors for timers.
+			/// @brief Constructs the processors for timers.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param timer_tick_processor Processor for the entity type TIMER_TICK.
 			TimerProcessors(
 				TimerTickProcessorPtr timer_tick_processor
 			);
 
+            /// Destructor.
 			~TimerProcessors();
 
 			/// Initialization of the processors for timers.
@@ -24,7 +28,7 @@ namespace visual_scripting {
 
 		private:
 
-			// Processor for the entity type TIMER_TICK.
+			/// Processor for the entity type TIMER_TICK.
 			TimerTickProcessorPtr timer_tick_processor;
 
 	};
