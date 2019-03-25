7d052afa055888aae9d6b87e301d88e93490a763
IAmNotHanni <cmdfreak@web.de>
2019-03-25 20:48:23 +0100

    Documentation of NandProcessor.

--- src/visual-scripting/processors/logical/gates/NandProcessor.cpp
+++ src/visual-scripting/processors/logical/gates/NandProcessor.cpp
@@ -46,10 +46,12 @@ namespace visual_scripting {
 	void NandProcessor::make_signals(const EntityInstancePtr& entity_instance)
 	{
 		spdlog::get(LOGGER_NAME)->debug("Initializing processor NAND for newly created entity instance {} of type {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name());
-		auto o_nand_input_1 = entity_instance->get_attribute_instance(NandEntityTypeProvider::NAND_INPUT_1);
+		
+        auto o_nand_input_1 = entity_instance->get_attribute_instance(NandEntityTypeProvider::NAND_INPUT_1);
 		auto o_nand_input_2 = entity_instance->get_attribute_instance(NandEntityTypeProvider::NAND_INPUT_2);
 		auto o_nand_result = entity_instance->get_attribute_instance(NandEntityTypeProvider::NAND_RESULT);
-		if (o_nand_input_1.has_value() && o_nand_input_2.has_value() && o_nand_result.has_value())
+		
+        if(o_nand_input_1.has_value() && o_nand_input_2.has_value() && o_nand_result.has_value())
 		{
 			signals[entity_instance->get_GUID()] = MakeSignal(
 				With(
@@ -62,7 +64,9 @@ namespace visual_scripting {
 				}
 			);
 			o_nand_result.value()->value = signals[entity_instance->get_GUID()];
-		} else {
+		}
+        else
+        {
 			spdlog::get(LOGGER_NAME)->error("Failed to initialize processor signals for entity instance {} of type {}: Missing one of these attributes: {} {} {}", entity_instance->get_GUID().str(), entity_instance->get_entity_type()->get_type_name(), NandEntityTypeProvider::NAND_INPUT_1, NandEntityTypeProvider::NAND_INPUT_2, NandEntityTypeProvider::NAND_RESULT);
 		}
 	}
