be79aef909a4a83d251de82c8c30636ddfe388a2
IAmNotHanni <cmdfreak@web.de>
2019-03-25 13:47:24 +0100

    Documentation of MathProcessors.

--- src/visual-scripting/processors/math/MathProcessors.hpp
+++ src/visual-scripting/processors/math/MathProcessors.hpp
@@ -9,17 +9,22 @@ namespace visual_scripting {
 	using ArithmeticProcessorsPtr = std::shared_ptr<ArithmeticProcessors>;
 	using TrigonometricProcessorsPtr = std::shared_ptr<TrigonometricProcessors>;
 
-    /// Provides processors.
+    /// @class MathProcessors
+    /// @brief Provides processors.
 	class MathProcessors
 	{
 		public:
 
-			/// Constructs the processors.
+			/// @brief Constructs the processors.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param arithmetic_processors The processors for arithmetic operations.
+            /// @param trigonometric_processors The processors for processors trigonometric operations.
 			MathProcessors(
 				ArithmeticProcessorsPtr arithmetic_processors,
 				TrigonometricProcessorsPtr trigonometric_processors
 			);
 
+            /// Destructor.
 			~MathProcessors();
 
 			/// Initialization of the Processors.
