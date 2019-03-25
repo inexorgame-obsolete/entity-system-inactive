d8ae0a6cb43a832f0e46670444b8ba1f817da335
IAmNotHanni <cmdfreak@web.de>
2019-03-25 13:14:21 +0100

    Documentation of EntityAttributeInstance.

--- src/entity-system/model/entity-attributes/entity-attribute-instances/EntityAttributeInstance.hpp
+++ src/entity-system/model/entity-attributes/entity-attribute-instances/EntityAttributeInstance.hpp
@@ -10,16 +10,15 @@ namespace entity_system {
 
 	using EntityAttributeTypePtr = std::shared_ptr<EntityAttributeType>;
 
-	/// A base class for entity attribute type instances.
+    /// @class EntityAttributeInstance
+	/// @brief A base class for entity attribute type instances.
 	class EntityAttributeInstance : public InstanceBase<EntityAttributeType>, public DataContainer, public GUIDBase
 	{
 		public:
 
-			/// Constructor.
-			/// @param ent_attr_type A reference to a shared pointer of
-			/// an entity attribute type of which an instance will be created.
-			/// @note the GUID of the new entity attribute instance will
-			/// be created automatically by the inheritance of GUIDBase!
+			/// @brief Constructor.
+			/// @note the GUID of the new entity attribute instance will be created automatically by the inheritance of GUIDBase!
+			/// @param ent_attr_type A reference to a shared pointer of an entity attribute type of which an instance will be created.
 			EntityAttributeInstance(const EntityAttributeTypePtr&);
 
 			/// Destructor.
