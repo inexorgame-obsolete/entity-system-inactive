913e2d252133ab70a4c500187edec139551d15ab
IAmNotHanni <cmdfreak@web.de>
2019-03-25 20:11:26 +0100

    Documentation of SinProcessor.

--- src/visual-scripting/processors/math/trigonometric/SinProcessor.hpp
+++ src/visual-scripting/processors/math/trigonometric/SinProcessor.hpp
@@ -23,11 +23,10 @@ namespace visual_scripting {
 	using DataType = entity_system::DataType;
 	using DataValue = entity_system::DataValue;
 
-	/// Processor which listens on the creation of entity instances
-	/// of type SIN. Newly created entity instances of type SIN
-	/// will be initialized by connecting the input attributes with
-	/// a calculation function and the result with the output
-	/// attribute.
+    /// @class SinProcessor
+	/// @brief Processor which listens on the creation of entity instances of type SIN.
+    /// @note Newly created entity instances of type SIN will be initialized by connecting
+    /// the input attributes with a calculation function and the result with the output attribute.
     class SinProcessor
     	: public Processor,
 		  public entity_system::EntityInstanceCreatedListener,
@@ -37,43 +36,50 @@ namespace visual_scripting {
 
 		public:
 
+            /// 
     		USING_REACTIVE_DOMAIN(entity_system::D)
 
-    		/// Constructs the SIN processor which listens on the creation
-    		/// of entity instances of type SIN. Newly created entity
-    		/// instances of type SIN will be initialized by connecting the
-    		/// input attributes with a calculation function and the result
-    		/// with the output attribute.
+    		/// @brief Constructs the SIN processor which listens on the creation of entity instances of type SIN.
+            /// @note Newly created entity instances of type SIN will be initialized by connecting the input attributes
+            /// with a calculation function and the result with the output attribute.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider The entity type provider for this processor.
+            /// @param entity_instance_manager The entity instance manager.
+            /// @param log_manager The log manager.
 			SinProcessor(
 				SinEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceManagerPtr entity_instance_manager,
 				LogManagerPtr log_manager
 			);
 
+            /// Destructor.
 			~SinProcessor();
 
-			/// Initializes the SIN processor by registering listeners
-			/// on newly created entity instances of type SIN.
+			/// Initializes the SIN processor by registering listeners on newly created entity instances of type SIN.
 			void init();
 
-			/// Called when an entity instance of type SIN has been created
+			/// @brief Called when an entity instance of type SIN has been created.
+            /// @param entity_instance ?
 			void on_entity_instance_created(EntityInstancePtr entity_instance);
 
-			/// Called when an entity instance of type SIN has been deleted
+			/// @brief Called when an entity instance of type SIN has been deleted.
+            /// @param type_GUID ?
+            /// @param inst_GUID ?
 			void on_entity_instance_deleted(const xg::Guid& type_GUID, const xg::Guid& inst_GUID);
 
-			/// Initialization of the processor signals.
+			/// @brief Initialization of the processor signals.
+            /// @param entity_instance ? 
 			void make_signals(const EntityInstancePtr& entity_instance);
 
 		private:
 
-			/// The entity type provider for this processor
+			/// The entity type provider for this processor.
 			SinEntityTypeProviderPtr entity_type_provider;
 
-			/// The entity instance manager
+			/// The entity instance manager.
 			EntityInstanceManagerPtr entity_instance_manager;
 
-			/// The log manager
+			/// The log manager.
 			LogManagerPtr log_manager;
 
 			/// The signals per entity instance.
