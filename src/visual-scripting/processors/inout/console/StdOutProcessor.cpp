799b422fd38e5a304b0627b5fc476782de48f285
IAmNotHanni <cmdfreak@web.de>
2019-03-25 23:09:42 +0100

    Documentation of StdOutProcessor.

--- src/visual-scripting/processors/inout/console/StdOutProcessor.cpp
+++ src/visual-scripting/processors/inout/console/StdOutProcessor.cpp
@@ -53,12 +53,15 @@ namespace visual_scripting {
 	{
 		std::cout << "Initializing processor CONSOLE_STDOUT for newly created entity instance " << entity_instance->get_GUID().str() << " of type " << entity_instance->get_entity_type()->get_type_name() << std::endl;
 		auto o_console_stdout = entity_instance->get_attribute_instance(StdOutEntityTypeProvider::CONSOLE_STDOUT);
-		if (o_console_stdout.has_value())
+		
+        if(o_console_stdout.has_value())
 		{
 			observers[entity_instance->get_GUID()] = Observe(o_console_stdout.value()->value, [] (DataValue console_stdout) {
 				std::cout << std::get<DataType::STRING>(console_stdout) << std::endl;
 			});
-		} else {
+		}
+        else
+        {
 			std::cout << "Failed to initialize processor signals for entity instance " << entity_instance->get_GUID().str() << " of type " << entity_instance->get_entity_type()->get_type_name() << ": Missing attribute" << StdOutEntityTypeProvider::CONSOLE_STDOUT << std::endl;
 		}
 	}
--- src/visual-scripting/processors/inout/console/StdOutProcessor.hpp
+++ src/visual-scripting/processors/inout/console/StdOutProcessor.hpp
@@ -12,7 +12,8 @@ namespace visual_scripting {
 
 	using namespace react;
 
-	/// Processor for entity instances of type CONSOLE_STDOUT.
+    /// @class StdOutProcessor
+	/// @brief Processor for entity instances of type CONSOLE_STDOUT.
     class StdOutProcessor
     	: public Processor,
 		  public entity_system::EntityInstanceCreatedListener,
@@ -26,26 +27,35 @@ namespace visual_scripting {
 
 		public:
 
+            /// 
     		USING_REACTIVE_DOMAIN(entity_system::D)
 
     		/// @brief Constructs a new entity instance of type CONSOLE_STDOUT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type CONSOLE_STDOUT.
+            /// @param entity_instance_manager The entity instance manager.
 			StdOutProcessor(
 				StdOutEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceManagerPtr entity_instance_manager
 			);
 
+            /// Destructor
 			~StdOutProcessor();
 
 			/// Initialization of the processor.
 			void init();
 
-			/// Called when an entity instance of type CONSOLE_STDOUT has been created
+			/// @brief Called when an entity instance of type CONSOLE_STDOUT has been created
+            /// @param entity_instance ?
 			void on_entity_instance_created(EntityInstancePtr entity_instance);
 
-			/// Called when an entity instance of type CONSOLE_STDOUT has been deleted
+			/// @brief Called when an entity instance of type CONSOLE_STDOUT has been deleted
+            /// @param type_GUID ?
+            /// @param inst_GUID ?
 			void on_entity_instance_deleted(const xg::Guid& type_GUID, const xg::Guid& inst_GUID);
 
-			/// Initialization of the processor signals.
+			/// @brief Initialization of the processor signals.
+            /// @param entity_instance
 			void make_signals(const EntityInstancePtr& entity_instance);
 
 		private:
@@ -53,7 +63,7 @@ namespace visual_scripting {
 			/// Provides the entity type CONSOLE_STDOUT.
 			StdOutEntityTypeProviderPtr entity_type_provider;
 
-			/// The entity instance manager
+			/// The entity instance manager.
 			EntityInstanceManagerPtr entity_instance_manager;
 
 			/// The signals per entity instance.
