2e9cf1466a8589fc8e7bba0487f24fcac2b3c5c8
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:07:39 +0100

    Documentation of NorEntityTypeProvider.

--- src/type-system/providers/logical/gates/NorEntityTypeProvider.hpp
+++ src/type-system/providers/logical/gates/NorEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "NOR".
+    /// @class NorEntityTypeProvider
+	/// @brief Provides an entity type "NOR".
 	class NorEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type NOR
+			/// @brief Constructs the specialized provider for the entity type NOR
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			NorEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~NorEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "NOR";
 
-			/// The name of the attribute nor_input_1
+			/// The name of the attribute nor_input_1.
 			static constexpr char NOR_INPUT_1[] = "nor_input_1";
 
-			/// The name of the attribute nor_input_1
+			/// The name of the attribute nor_input_1.
 			static constexpr char NOR_INPUT_2[] = "nor_input_2";
 
-			/// The name of the attribute nor_result
+			/// The name of the attribute nor_result.
 			static constexpr char NOR_RESULT[] = "nor_result";
 
 	};
