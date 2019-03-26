cbfef5b3085de90025e2c107abe0f9640104be8b
IAmNotHanni <cmdfreak@web.de>
2019-03-26 16:45:57 +0100

    Documentation of RandomFactories.

--- src/type-system/factories/generators/random/RandomFactories.hpp
+++ src/type-system/factories/generators/random/RandomFactories.hpp
@@ -12,17 +12,22 @@ namespace type_system {
 	using RandomNextIntFactoryPtr = std::shared_ptr<RandomNextIntFactory>;
 	using RandomNextFloatFactoryPtr = std::shared_ptr<RandomNextFloatFactory>;
 
-	/// Provides the factories for counters.
+    /// @class RandomFactories
+	/// @brief Provides the factories for counters.
 	class RandomFactories
 	{
 		public:
 
-			/// Constructs the factories for counters.
+			/// @brief Constructs the factories for counters.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param random_next_int_factory Factory for creating entity instances of type COUNTER_INT.
+            /// @param random_next_float_factory Factory for creating entity instances of type COUNTER_FLOAT.
 			RandomFactories(
 				RandomNextIntFactoryPtr random_next_int_factory,
 				RandomNextFloatFactoryPtr random_next_float_factory
 			);
 
+            /// Destructor.
 			~RandomFactories();
 
 			/// Initializes the factories for counters.
