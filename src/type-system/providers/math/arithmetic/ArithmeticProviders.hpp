88e5f54385f46f4d39bbb8d6003ce371327d4ce6
IAmNotHanni <cmdfreak@web.de>
2019-03-26 12:54:59 +0100

    Documentation of ArithmeticProviders.

--- src/type-system/providers/math/arithmetic/ArithmeticProviders.hpp
+++ src/type-system/providers/math/arithmetic/ArithmeticProviders.hpp
@@ -10,17 +10,22 @@ namespace type_system {
 	using AddIntEntityTypeProviderPtr = std::shared_ptr<AddIntEntityTypeProvider>;
 	using AddFloatEntityTypeProviderPtr = std::shared_ptr<AddFloatEntityTypeProvider>;
 
-	/// The arithmetic entity type providers.
+    /// @class ArithmeticProviders
+	/// @brief The arithmetic entity type providers.
 	class ArithmeticProviders
 	{
 		public:
 
-			/// Constructs the arithmetic entity type providers.
+			/// @brief Constructs the arithmetic entity type providers.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @brief add_int_entity_type_provider Provides the entity type ADD_INT.
+            /// @brief add_float_entity_type_provider Provides the entity type ADD_FLOAT.
 			ArithmeticProviders(
 				AddIntEntityTypeProviderPtr add_int_entity_type_provider,
 				AddFloatEntityTypeProviderPtr add_float_entity_type_provider
 			);
 
+            /// Destructor.
 			~ArithmeticProviders();
 
 			/// Initializes the arithmetic entity type providers.
