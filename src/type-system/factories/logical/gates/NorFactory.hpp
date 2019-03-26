8bf44b0915a4e4b0f8d1a450e82479530d020261
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:48:59 +0100

    Documentation of NorFactory.

--- src/type-system/factories/logical/gates/NorFactory.hpp
+++ src/type-system/factories/logical/gates/NorFactory.hpp
@@ -10,7 +10,8 @@ namespace type_system {
 
 	using EntityInstancePtrOpt = std::optional<std::shared_ptr<EntityInstance>>;
 
-    /// Factory for creating entity instances of type NOR.
+    /// @class NorFactory
+    /// @brief Factory for creating entity instances of type NOR.
 	class NorFactory
 	{
 		public:
@@ -19,11 +20,15 @@ namespace type_system {
 			using EntityInstanceBuilderFactoryPtr = std::shared_ptr<EntityInstanceBuilderFactory>;
 
 			/// Constructs a factory for creating entity instances of type NOR.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type NOR.
+            /// @param entity_instance_builder_factory Factory for creating entity instance builders.
 			NorFactory(
 				NorEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceBuilderFactoryPtr entity_instance_builder_factory
 			);
 
+            /// Destructor.
 			~NorFactory();
 
 			/// Initializes the factory.
