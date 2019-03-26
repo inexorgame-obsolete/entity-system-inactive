4ddccce8fa36f2cfdb57823e9a0201e0634940f8
IAmNotHanni <cmdfreak@web.de>
2019-03-26 16:45:05 +0100

    Documentation of RandomNextFloatFactory.

--- src/type-system/factories/generators/random/RandomNextFloatFactory.hpp
+++ src/type-system/factories/generators/random/RandomNextFloatFactory.hpp
@@ -13,17 +13,22 @@ namespace type_system {
 	using EntityInstanceBuilderFactoryPtr = std::shared_ptr<EntityInstanceBuilderFactory>;
 	using RandomNextFloatEntityTypeProviderPtr = std::shared_ptr<RandomNextFloatEntityTypeProvider>;
 
-    /// Factory for creating entity instances of type RANDOM_NEXT_FLOAT.
+    /// @class RandomNextFloatFactory
+    /// @brief Factory for creating entity instances of type RANDOM_NEXT_FLOAT.
 	class RandomNextFloatFactory
 	{
 		public:
 
-			/// Constructs a factory for creating entity instances of type RANDOM_NEXT_FLOAT.
+			/// @brief Constructs a factory for creating entity instances of type RANDOM_NEXT_FLOAT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type RANDOM_NEXT_FLOAT.
+            /// @param entity_instance_builder_factory Factory for creating entity instance builders.
 			RandomNextFloatFactory(
 				RandomNextFloatEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceBuilderFactoryPtr entity_instance_builder_factory
 			);
 
+            /// Destructor.
 			~RandomNextFloatFactory();
 
 			/// Initializes the factory.
@@ -32,7 +37,7 @@ namespace type_system {
 			/// Creates a counter with the default values: millis = 1000, step = 1.0f, start_value = 0.0f.
 			EntityInstancePtrOpt create_instance();
 
-			/// Creates an counter with the specified
+			/// Creates an counter with the specified.
 			EntityInstancePtrOpt create_instance(float min, float max);
 
 		private:
