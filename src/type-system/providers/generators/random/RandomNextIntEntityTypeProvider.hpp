9c524441963a8522551500e160dc771b652931f3
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:15:12 +0100

    Documentation of RandomNextIntEntityTypeProvider.

--- src/type-system/providers/generators/random/RandomNextIntEntityTypeProvider.hpp
+++ src/type-system/providers/generators/random/RandomNextIntEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "RANDOM_NEXT_INT".
+    /// @class RandomNextIntEntityTypeProvider
+	/// @brief Provides an entity type "RANDOM_NEXT_INT".
 	class RandomNextIntEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type RANDOM_NEXT_INT
+			/// @brief Constructs the specialized provider for the entity type RANDOM_NEXT_INT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			RandomNextIntEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -26,21 +29,22 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~RandomNextIntEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "RANDOM_NEXT_INT";
 
-			/// The name of the attribute random_next_int_on_activation
+			/// The name of the attribute random_next_int_on_activation.
 			static constexpr char RANDOM_NEXT_INT_ON_ACTIVATION[] = "random_next_int_on_activation";
 
-			/// The name of the attribute random_next_int_min
+			/// The name of the attribute random_next_int_min.
 			static constexpr char RANDOM_NEXT_INT_MIN[] = "random_next_int_min";
 
-			/// The name of the attribute random_next_int_max
+			/// The name of the attribute random_next_int_max.
 			static constexpr char RANDOM_NEXT_INT_MAX[] = "random_next_int_max";
 
-			/// The name of the attribute random_next_int_value
+			/// The name of the attribute random_next_int_value.
 			static constexpr char RANDOM_NEXT_INT_VALUE[] = "random_next_int_value";
 
 	};
