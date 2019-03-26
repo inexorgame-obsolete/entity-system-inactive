38db4866fd4927855e82b440a7ae17a471fb059c
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:16:13 +0100

    Documentation of RandomNextFloatEntityTypeProvider.

--- src/type-system/providers/generators/random/RandomNextFloatEntityTypeProvider.hpp
+++ src/type-system/providers/generators/random/RandomNextFloatEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "RANDOM_NEXT_FLOAT".
+    /// @class RandomNextFloatEntityTypeProvider
+	/// @brief Provides an entity type "RANDOM_NEXT_FLOAT".
 	class RandomNextFloatEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type RANDOM_NEXT_FLOAT
+			/// @brief Constructs the specialized provider for the entity type RANDOM_NEXT_FLOAT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			RandomNextFloatEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -26,21 +29,22 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~RandomNextFloatEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "RANDOM_NEXT_FLOAT";
 
-			/// The name of the attribute random_next_float_on_activation
+			/// The name of the attribute random_next_float_on_activation.
 			static constexpr char RANDOM_NEXT_FLOAT_ON_ACTIVATION[] = "random_next_float_on_activation";
 
-			/// The name of the attribute random_next_float_min
+			/// The name of the attribute random_next_float_min.
 			static constexpr char RANDOM_NEXT_FLOAT_MIN[] = "random_next_float_min";
 
-			/// The name of the attribute random_next_float_max
+			/// The name of the attribute random_next_float_max.
 			static constexpr char RANDOM_NEXT_FLOAT_MAX[] = "random_next_float_max";
 
-			/// The name of the attribute random_next_float_value
+			/// The name of the attribute random_next_float_value.
 			static constexpr char RANDOM_NEXT_FLOAT_VALUE[] = "random_next_float_value";
 
 	};
