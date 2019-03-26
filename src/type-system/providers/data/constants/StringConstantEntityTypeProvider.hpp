b5d27c979c617809205f7e8eac55f2270310045a
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:27:16 +0100

    Documentation of StringConstantEntityTypeProvider.

--- src/type-system/providers/data/constants/StringConstantEntityTypeProvider.hpp
+++ src/type-system/providers/data/constants/StringConstantEntityTypeProvider.hpp
@@ -6,13 +6,16 @@
 namespace inexor {
 namespace entity_system {
 namespace type_system {
-
+    
+    /// @class StringConstantEntityTypeProvider
 	/// Provides an entity type "STRING_CONSTANT" which is a pure string constant variable.
 	class StringConstantEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
-
-			/// Constructs the specialized provider for the STRING_CONSTANT entity type
+            
+			/// @brief Constructs the specialized provider for the STRING_CONSTANT entity type
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			StringConstantEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -24,15 +27,16 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~StringConstantEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "STRING_CONSTANT";
 
-			/// The name of the output attribute string_constant_name
+			/// The name of the output attribute string_constant_name.
 			static constexpr char STRING_CONSTANT_NAME[] = "string_constant_name";
 
-			/// The name of the output attribute string_constant_value
+			/// The name of the output attribute string_constant_value.
 			static constexpr char STRING_CONSTANT_VALUE[] = "string_constant_value";
 
 	};
