120f1aea25e1e48a10b3cb2076938399b557af02
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:17:12 +0100

    Documentation of CounterProviders.

--- src/type-system/providers/generators/counters/CounterProviders.hpp
+++ src/type-system/providers/generators/counters/CounterProviders.hpp
@@ -12,17 +12,22 @@ namespace type_system {
 	using CounterIntEntityTypeProviderPtr = std::shared_ptr<CounterIntEntityTypeProvider>;
 	using CounterFloatEntityTypeProviderPtr = std::shared_ptr<CounterFloatEntityTypeProvider>;
 
-	/// The entity type providers for counters.
+    /// @class CounterProviders
+	/// @brief The entity type providers for counters.
 	class CounterProviders
 	{
 		public:
 
-			/// Constructs the entity type providers for counters.
+			/// @brief Constructs the entity type providers for counters.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param counter_int_entity_type_provider Provides the entity type COUNTER_INT.
+            /// @param counter_float_entity_type_provider Provides the entity type COUNTER_FLOAT.
 			CounterProviders(
 				CounterIntEntityTypeProviderPtr counter_int_entity_type_provider,
 				CounterFloatEntityTypeProviderPtr counter_float_entity_type_provider
 			);
 
+            /// Destructor.
 			~CounterProviders();
 
 			/// Initializes the providers for logical operations.
