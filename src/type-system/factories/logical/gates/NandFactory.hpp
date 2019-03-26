75a36fda52816078b7a9a63c1275ad7bec5f1d62
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:50:03 +0100

    Documentation of NandFactory.

--- src/type-system/factories/logical/gates/NandFactory.hpp
+++ src/type-system/factories/logical/gates/NandFactory.hpp
@@ -10,7 +10,8 @@ namespace type_system {
 
 	using EntityInstancePtrOpt = std::optional<std::shared_ptr<EntityInstance>>;
 
-    /// Factory for creating entity instances of type NAND.
+    /// @class NandFactory
+    /// @brief Factory for creating entity instances of type NAND.
 	class NandFactory
 	{
 		public:
@@ -18,12 +19,16 @@ namespace type_system {
 			using NandEntityTypeProviderPtr = std::shared_ptr<NandEntityTypeProvider>;
 			using EntityInstanceBuilderFactoryPtr = std::shared_ptr<EntityInstanceBuilderFactory>;
 
-			/// Constructs a factory for creating entity instances of type NAND.
+			/// @brief Constructs a factory for creating entity instances of type NAND.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type NAND.
+            /// @param entity_instance_builder_factory Factory for creating entity instance builders.
 			NandFactory(
 				NandEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceBuilderFactoryPtr entity_instance_builder_factory
 			);
 
+            /// Destructor.
 			~NandFactory();
 
 			/// Initializes the factory.
