048150d1ad97be120d026c969354c13d90695381
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:42:32 +0100

    Documentation of StdInEntityTypeProvider.

--- src/type-system/providers/inout/console/StdInEntityTypeProvider.hpp
+++ src/type-system/providers/inout/console/StdInEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
+    /// @class StdInEntityTypeProvider
 	/// Provides an entity type "CONSOLE_STDIN".
 	class StdInEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type CONSOLE_STDIN
+			/// @brief Constructs the specialized provider for the entity type CONSOLE_STDIN.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			StdInEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -23,12 +26,13 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~StdInEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "CONSOLE_STDIN";
 
-			/// The name of the attribute console_std_out
+			/// The name of the attribute console_std_out.
 			static constexpr char CONSOLE_STDIN[] = "console_stdin";
 
 	};
