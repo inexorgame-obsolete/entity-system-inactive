c0b054eb8409b5ed3f30db02cad5803d6d6f490a
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:22:31 +0100

    Documentation of EqualsIntEntityTypeProvider.

--- src/type-system/providers/logical/comparison/EqualsIntEntityTypeProvider.hpp
+++ src/type-system/providers/logical/comparison/EqualsIntEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "EQUALS_INT".
+    /// @class EqualsIntEntityTypeProvider
+	/// @brief Provides an entity type "EQUALS_INT".
 	class EqualsIntEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type EQUALS_INT
+			/// @brief Constructs the specialized provider for the entity type EQUALS_INT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			EqualsIntEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~EqualsIntEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "EQUALS_INT";
 
-			/// The name of the attribute equals_int_input_1
+			/// The name of the attribute equals_int_input_1.
 			static constexpr char EQUALS_INT_INPUT_1[] = "equals_int_input_1";
 
-			/// The name of the attribute equals_int_input_1
+			/// The name of the attribute equals_int_input_1.
 			static constexpr char EQUALS_INT_INPUT_2[] = "equals_int_input_2";
 
-			/// The name of the attribute equals_int_result
+			/// The name of the attribute equals_int_result.
 			static constexpr char EQUALS_INT_RESULT[] = "equals_int_result";
 
 	};
