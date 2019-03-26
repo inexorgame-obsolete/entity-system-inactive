2f7cfe34432007a6f1eab62d99dc850f831f52b1
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:18:57 +0100

    Documentation of CounterFloatEntityTypeProvider.

--- src/type-system/providers/generators/counters/CounterFloatEntityTypeProvider.hpp
+++ src/type-system/providers/generators/counters/CounterFloatEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "COUNTER_FLOAT".
+    /// @class CounterFloatEntityTypeProvider
+	/// @brief Provides an entity type "COUNTER_FLOAT".
 	class CounterFloatEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type COUNTER_FLOAT
+			/// @brief Constructs the specialized provider for the entity type COUNTER_FLOAT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			CounterFloatEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -26,21 +29,22 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~CounterFloatEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "COUNTER_FLOAT";
 
-			/// The name of the attribute counter_float_millis
+			/// The name of the attribute counter_float_millis.
 			static constexpr char COUNTER_FLOAT_MILLIS[] = "counter_float_millis";
 
-			/// The name of the attribute counter_float_step
+			/// The name of the attribute counter_float_step.
 			static constexpr char COUNTER_FLOAT_STEP[] = "counter_float_step";
 
-			/// The name of the attribute counter_float_reset
+			/// The name of the attribute counter_float_reset.
 			static constexpr char COUNTER_FLOAT_RESET[] = "counter_float_reset";
 
-			/// The name of the attribute counter_float_count
+			/// The name of the attribute counter_float_count.
 			static constexpr char COUNTER_FLOAT_COUNT[] = "counter_float_count";
 
 	};
