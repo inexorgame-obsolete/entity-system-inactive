665282e5ab52b28da17d9897f29d6bdaf0c0968e
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:01:49 +0100

    Documentation of XorEntityTypeProvider.

--- src/type-system/providers/logical/gates/XorEntityTypeProvider.hpp
+++ src/type-system/providers/logical/gates/XorEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
+    /// @class XorEntityTypeProvider
 	/// Provides an entity type "XOR".
 	class XorEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type XOR
+			/// Constructs the specialized provider for the entity type XOR.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			XorEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~XorEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "XOR";
 
-			/// The name of the attribute xor_input_1
+			/// The name of the attribute xor_input_1.
 			static constexpr char XOR_INPUT_1[] = "xor_input_1";
 
-			/// The name of the attribute xor_input_1
+			/// The name of the attribute xor_input_1.
 			static constexpr char XOR_INPUT_2[] = "xor_input_2";
 
-			/// The name of the attribute xor_result
+			/// The name of the attribute xor_result.
 			static constexpr char XOR_RESULT[] = "xor_result";
 
 	};
