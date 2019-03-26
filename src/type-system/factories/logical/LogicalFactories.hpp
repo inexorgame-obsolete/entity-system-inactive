88a9769122c513c0e00a537b95dd7c33d668fd9c
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:44:31 +0100

    Documentation of LogicalFactories.

--- src/type-system/factories/logical/LogicalFactories.hpp
+++ src/type-system/factories/logical/LogicalFactories.hpp
@@ -10,17 +10,22 @@ namespace type_system {
 	using ComparisonFactoriesPtr = std::shared_ptr<ComparisonFactories>;
 	using GateFactoriesPtr = std::shared_ptr<GateFactories>;
 
-	/// The factories for logical operations.
+    /// @class LogicalFactories
+	/// @brief The factories for logical operations.
 	class LogicalFactories
 	{
 		public:
 
-			/// Constructs the factories for logical operations.
+			/// @brief Constructs the factories for logical operations.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param comparison_factories The factories for comparison operations.
+            /// @param gate_factories The factories for logical gates.
 			LogicalFactories(
 				ComparisonFactoriesPtr comparison_factories,
 				GateFactoriesPtr gate_factories
 			);
 
+            /// Destructor.
 			~LogicalFactories();
 
 			/// Initializes the factories for logical operations.
