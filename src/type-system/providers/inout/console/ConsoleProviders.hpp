a84e03dca41cc99533c800753abf8935c8744380
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:50:38 +0100

    Documentation of ConsoleProviders.

--- src/type-system/providers/inout/console/ConsoleProviders.hpp
+++ src/type-system/providers/inout/console/ConsoleProviders.hpp
@@ -14,18 +14,24 @@ namespace type_system {
 	using StdOutEntityTypeProviderPtr = std::shared_ptr<StdOutEntityTypeProvider>;
 	using StdErrEntityTypeProviderPtr = std::shared_ptr<StdErrEntityTypeProvider>;
 
-	/// The console entity type providers.
+    /// @class ConsoleProviders
+	/// @brief The console entity type providers.
 	class ConsoleProviders
 	{
 		public:
 
-			/// Constructs the entity type providers for the console handling.
+			/// @brief Constructs the entity type providers for the console handling.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param std_in_entity_type_provider Provides the entity type CONSOLE_STDIN.
+            /// @param std_out_entity_type_provider Provides the entity type CONSOLE_STDOUT.
+            /// @param std_err_entity_type_provider Provides the entity type CONSOLE_STDERR.
 			ConsoleProviders(
 				StdInEntityTypeProviderPtr std_in_entity_type_provider,
 				StdOutEntityTypeProviderPtr std_out_entity_type_provider,
 				StdErrEntityTypeProviderPtr std_err_entity_type_provider
 			);
 
+            /// Destructor.
 			~ConsoleProviders();
 
 			/// Initializes the console entity type providers for the console handling.
