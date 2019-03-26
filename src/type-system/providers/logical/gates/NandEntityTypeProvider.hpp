2d158998bcb533a9ff1a5b2da941d589bea06949
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:08:34 +0100

    Documentation of NandEntityTypeProvider.

--- src/type-system/providers/logical/gates/NandEntityTypeProvider.hpp
+++ src/type-system/providers/logical/gates/NandEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "NAND".
+    /// @class NandEntityTypeProvider
+	/// @brief Provides an entity type "NAND".
 	class NandEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type NAND
+			/// @brief Constructs the specialized provider for the entity type NAND.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			NandEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~NandEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "NAND";
 
-			/// The name of the attribute nand_input_1
+			/// The name of the attribute nand_input_1.
 			static constexpr char NAND_INPUT_1[] = "nand_input_1";
 
-			/// The name of the attribute nand_input_1
+			/// The name of the attribute nand_input_1.
 			static constexpr char NAND_INPUT_2[] = "nand_input_2";
 
-			/// The name of the attribute nand_result
+			/// The name of the attribute nand_result.
 			static constexpr char NAND_RESULT[] = "nand_result";
 
 	};
