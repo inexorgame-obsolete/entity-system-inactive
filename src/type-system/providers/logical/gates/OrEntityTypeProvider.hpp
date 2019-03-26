89a87a702e8aa839cc86f0ff757a59d4fffb5b66
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:03:32 +0100

    Documentation of OrEntityTypeProvider.

--- src/type-system/providers/logical/gates/OrEntityTypeProvider.hpp
+++ src/type-system/providers/logical/gates/OrEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "OR".
+    /// @class OrEntityTypeProvider
+	/// @brief Provides an entity type "OR".
 	class OrEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type OR
+			/// @brief Constructs the specialized provider for the entity type OR.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			OrEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~OrEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "OR";
 
-			/// The name of the attribute or_input_1
+			/// The name of the attribute or_input_1.
 			static constexpr char OR_INPUT_1[] = "or_input_1";
 
-			/// The name of the attribute or_input_1
+			/// The name of the attribute or_input_1.
 			static constexpr char OR_INPUT_2[] = "or_input_2";
 
-			/// The name of the attribute or_result
+			/// The name of the attribute or_result.
 			static constexpr char OR_RESULT[] = "or_result";
 
 	};
