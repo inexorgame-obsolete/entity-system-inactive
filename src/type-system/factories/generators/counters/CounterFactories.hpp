90bf40cbd4bd5ed1dca16a28da89af5da85d07cb
IAmNotHanni <cmdfreak@web.de>
2019-03-26 16:50:28 +0100

    Documentation of CounterFactories.

--- src/type-system/factories/generators/counters/CounterFactories.hpp
+++ src/type-system/factories/generators/counters/CounterFactories.hpp
@@ -12,17 +12,22 @@ namespace type_system {
 	using CounterIntFactoryPtr = std::shared_ptr<CounterIntFactory>;
 	using CounterFloatFactoryPtr = std::shared_ptr<CounterFloatFactory>;
 
-	/// Provides the factories for counters.
+    /// @class CounterFactories
+	/// @brief Provides the factories for counters.
 	class CounterFactories
 	{
 		public:
 
-			/// Constructs the factories for counters.
+			/// @brief Constructs the factories for counters.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param counter_int_factory Factory for creating entity instances of type COUNTER_INT.
+            /// @param counter_float_factory Factory for creating entity instances of type COUNTER_FLOAT.
 			CounterFactories(
 				CounterIntFactoryPtr counter_int_factory,
 				CounterFloatFactoryPtr counter_float_factory
 			);
 
+            /// Destructor.
 			~CounterFactories();
 
 			/// Initializes the factories for counters.
