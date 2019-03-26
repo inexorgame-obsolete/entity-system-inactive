6bf0be2f7d0c5d5e61df4f8c36862f04253f0059
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:06:20 +0100

    Documentation of NotEntityTypeProvider.

--- src/type-system/providers/logical/gates/NotEntityTypeProvider.hpp
+++ src/type-system/providers/logical/gates/NotEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "NOT".
+    /// @class NotEntityTypeProvider
+	/// @brief Provides an entity type "NOT".
 	class NotEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type NOT
+			/// @brief Constructs the specialized provider for the entity type NOT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			NotEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -24,15 +27,16 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~NotEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "NOT";
 
-			/// The name of the attribute not_input
+			/// The name of the attribute not_input.
 			static constexpr char NOT_INPUT[] = "not_input";
 
-			/// The name of the attribute not_input
+			/// The name of the attribute not_input.
 			static constexpr char NOT_RESULT[] = "not_result";
 
 	};
