bc9f1f13f3b508bd888302a05d57681663562d37
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:24:20 +0100

    Documentation of FloatStoreEntityTypeProvider.

--- src/type-system/providers/data/stores/FloatStoreEntityTypeProvider.hpp
+++ src/type-system/providers/data/stores/FloatStoreEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "FLOAT_STORE".
+    /// @class FloatStoreEntityTypeProvider
+	/// @brief Provides an entity type "FLOAT_STORE".
 	class FloatStoreEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the FLOAT_STORE entity type
+			/// @brief Constructs the specialized provider for the FLOAT_STORE entity type.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			FloatStoreEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -26,21 +29,22 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~FloatStoreEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "FLOAT_STORE";
 
-			/// The name of the store
+			/// The name of the store.
 			static constexpr char FLOAT_STORE_NAME[] = "float_store_name";
 
-			/// The name of the input attribute
+			/// The name of the input attribute.
 			static constexpr char FLOAT_STORE_INPUT_VALUE[] = "float_store_input_value";
 
-			/// The name of the default value attribute
+			/// The name of the default value attribute.
 			static constexpr char FLOAT_STORE_DEFAULT_VALUE[] = "float_store_default_value";
 
-			/// The name of the output value attribute
+			/// The name of the output value attribute.
 			static constexpr char FLOAT_STORE_OUTPUT_VALUE[] = "float_store_output_value";
 
 	};
