3e316c83099e83d0550dfa4916ada87d069fd592
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:52:28 +0100

    Documentation of AndFactory.

--- src/type-system/factories/logical/gates/AndFactory.hpp
+++ src/type-system/factories/logical/gates/AndFactory.hpp
@@ -10,7 +10,8 @@ namespace type_system {
 
 	using EntityInstancePtrOpt = std::optional<std::shared_ptr<EntityInstance>>;
 
-    /// Factory for creating entity instances of type AND.
+    /// @class AndFactory
+    /// @brief Factory for creating entity instances of type AND.
 	class AndFactory
 	{
 		public:
@@ -18,12 +19,16 @@ namespace type_system {
 			using AndEntityTypeProviderPtr = std::shared_ptr<AndEntityTypeProvider>;
 			using EntityInstanceBuilderFactoryPtr = std::shared_ptr<EntityInstanceBuilderFactory>;
 
-			/// Constructs a factory for creating entity instances of type AND.
+			/// @brief Constructs a factory for creating entity instances of type AND.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type AND.
+            /// @param entity_instance_builder_factory Factory for creating entity instance builders.
 			AndFactory(
 				AndEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceBuilderFactoryPtr entity_instance_builder_factory
 			);
 
+            /// Destructor.
 			~AndFactory();
 
 			/// Initializes the factory.
