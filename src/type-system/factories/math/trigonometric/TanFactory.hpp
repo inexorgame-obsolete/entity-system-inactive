15ae90f70e3682adf588501659b9cebbefcce0ba
IAmNotHanni <cmdfreak@web.de>
2019-03-26 14:37:12 +0100

    Documentation of TanFactory.

--- src/type-system/factories/math/trigonometric/TanFactory.hpp
+++ src/type-system/factories/math/trigonometric/TanFactory.hpp
@@ -10,7 +10,8 @@ namespace type_system {
 
 	using EntityInstancePtrOpt = std::optional<std::shared_ptr<EntityInstance>>;
 
-    /// Factory for creating entity instances of type TAN.
+    /// @class TanFactory
+    /// @brief Factory for creating entity instances of type TAN.
 	class TanFactory
 	{
 		public:
@@ -19,20 +20,25 @@ namespace type_system {
 			using EntityInstanceBuilderFactoryPtr = std::shared_ptr<EntityInstanceBuilderFactory>;
 
 			/// Constructs a factory for creating entity instances of type TAN.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_provider Provides the entity type TAN.
+            /// @param entity_instance_builder_factory Factory for creating entity instance builders.
 			TanFactory(
 				TanEntityTypeProviderPtr entity_type_provider,
 				EntityInstanceBuilderFactoryPtr entity_instance_builder_factory
 			);
 
+            /// Destructor.
 			~TanFactory();
 
 			/// Initializes the factory.
 			void init();
 
-			/// Creates an instance with default values
+			/// Creates an instance with default values.
 			EntityInstancePtrOpt create_instance();
 
-			/// Creates an instance with the given input value.
+			/// @brief Creates an instance with the given input value.
+            /// @param input The given input value of type float.
 			EntityInstancePtrOpt create_instance(float input);
 
 		private:
