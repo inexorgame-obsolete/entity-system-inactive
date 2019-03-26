c64de645903d1b600ca49ef3d5f92f06526c900f
IAmNotHanni <cmdfreak@web.de>
2019-03-26 13:19:56 +0100

    Documentation of GreaterThanFloatEntityTypeProvider.

--- src/type-system/providers/logical/comparison/GreaterThanFloatEntityTypeProvider.hpp
+++ src/type-system/providers/logical/comparison/GreaterThanFloatEntityTypeProvider.hpp
@@ -7,12 +7,15 @@ namespace inexor {
 namespace entity_system {
 namespace type_system {
 
-	/// Provides an entity type "GREATER_THAN_FLOAT".
+    /// @class GreaterThanFloatEntityTypeProvider
+	/// @brief Provides an entity type "GREATER_THAN_FLOAT".
 	class GreaterThanFloatEntityTypeProvider : public EntityTypeProvider
 	{
 		public:
 
-			/// Constructs the specialized provider for the entity type GREATER_THAN_FLOAT
+			/// @brief Constructs the specialized provider for the entity type GREATER_THAN_FLOAT.
+            /// @note The dependencies of this class will be injected automatically.
+            /// @param entity_type_builder_manager The entity type builder manager.
 			GreaterThanFloatEntityTypeProvider(
 				std::shared_ptr<EntityTypeBuilderFactory> entity_type_builder_manager
 			) : EntityTypeProvider(
@@ -25,18 +28,19 @@ namespace type_system {
 				}
 			) {};
 
+            /// Destructor.
 			~GreaterThanFloatEntityTypeProvider() {};
 
-			/// Defines the name of this entity type
+			/// Defines the name of this entity type.
 			static constexpr char TYPE_NAME[] = "GREATER_THAN_FLOAT";
 
-			/// The name of the attribute greater_than_float_input_1
+			/// The name of the attribute greater_than_float_input_1.
 			static constexpr char GREATER_THAN_FLOAT_INPUT_1[] = "greater_than_float_input_1";
 
-			/// The name of the attribute greater_than_float_input_1
+			/// The name of the attribute greater_than_float_input_1.
 			static constexpr char GREATER_THAN_FLOAT_INPUT_2[] = "greater_than_float_input_2";
 
-			/// The name of the attribute greater_than_float_result
+			/// The name of the attribute greater_than_float_result.
 			static constexpr char GREATER_THAN_FLOAT_RESULT[] = "greater_than_float_result";
 
 	};
