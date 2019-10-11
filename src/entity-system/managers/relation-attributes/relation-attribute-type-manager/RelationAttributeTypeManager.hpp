ad1b40f2422a0fa19e9094c2fcbe9e16b93629fe
IAmNotHanni <cmdfreak@web.de>
2019-10-11 22:45:42 +0200

    Improved documentation of RelationAttributeTypeManager.

--- src/entity-system/managers/relation-attributes/relation-attribute-type-manager/RelationAttributeTypeManager.hpp
+++ src/entity-system/managers/relation-attributes/relation-attribute-type-manager/RelationAttributeTypeManager.hpp
@@ -12,29 +12,30 @@ namespace entity_system {
 	using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
 	using RelationAttributeTypePtrOpt = std::optional<RelationAttributeTypePtr>;
 
-	/// A manager class for relation attribute types.
+	/// @class RelationAttributeTypeManager
+	/// @brief A manager class for relation attribute types.
 	class RelationAttributeTypeManager : public TypeManagerTemplate<RelationAttributeType>
 	{
 		public:
 
-			/// Constructor.
+			/// @brief Constructor.
 			RelationAttributeTypeManager();
 
-			/// Destructor.
+			/// @brief Destructor.
 			~RelationAttributeTypeManager();
 
-			/// Creates an relation attribute type.
+			/// @brief Creates an relation attribute type.
 			/// @param rel_attr_type_name The name of the new relation attribute type.
 			/// @param rel_attr_type_data_type The data type of the new relation attribute type.
 			/// @param rel_attr_type_features The features of the new relation attribute type.
 			/// @return A shared pointer to the relation attribute type which was created.
 			RelationAttributeTypePtrOpt create_relation_attribute_type(const std::string&, const DataType&, const EnumSet<Feature>&);
 
-			/// Returns the number of existing types of relation attributes.
+			/// @brief Returns the number of existing types of relation attributes.
 			/// @return The number of existing types of relation attributes.
 			std::size_t get_relation_attribute_type_count() const;
 
-			/// Deletes all types of relation attributes.
+			/// @brief Deletes all types of relation attributes.
 			void delete_all_relation_attribute_types();
 
 	};
