48b1b63e7f9178ed3452c9090957e0e0442ca903
IAmNotHanni <cmdfreak@web.de>
2019-10-11 22:45:54 +0200

    Improved documentation of RelationAttributeInstanceManager.

--- src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.hpp
+++ src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.hpp
@@ -15,32 +15,33 @@ namespace entity_system {
 	using RelationAttributeInstancePtr = std::shared_ptr<RelationAttributeInstance>;
 	using RelationAttributeInstancePtrOpt = std::optional<RelationAttributeInstancePtr>;
 
+	/// @class RelationAttributeInstanceManager
 	/// A manager class for relation attribute instances.
 	class RelationAttributeInstanceManager : public InstanceManagerTemplate<RelationAttributeInstance>
 	{
 		public:
 
-			/// Constructor.
+			/// @brief Constructor.
 			RelationAttributeInstanceManager();
 
-			/// Destructor.
+			/// @brief Destructor.
 			~RelationAttributeInstanceManager();
 
 			/// @brief Creates an relation attribute instance.
-			/// @param ent_rel_attr_type A const reference to a shared pointer of an relation attribute type of which an instance will be created.
+			/// @param rel_attr_type A const reference to a shared pointer of an relation attribute type of which an instance will be created.
 			/// @return A shared pointer to the relation attribute instance which was created. <br>If the creation fails std::nullopt will be returned.
-			RelationAttributeInstancePtr create_relation_attribute_instance(const RelationAttributeTypePtr&);
+			RelationAttributeInstancePtr create_relation_attribute_instance(const RelationAttributeTypePtr& rel_attr_type);
 
-			/// 
-			/// 
-			/// 
-			std::optional<RelationAttributeInstancePtr> get_relation_attribute_instance(const xg::Guid&);
+			/// @brief Gets a relation attribute instance.
+			/// @param rel_attr_inst_GUID The GUID of the relation attribute instance.
+			/// @return A std::optional shared pointer to the relation attribute instance.
+			std::optional<RelationAttributeInstancePtr> get_relation_attribute_instance(const xg::Guid& rel_attr_inst_GUID);
 
-			/// Returns the number of existing relation attribute type instances.
+			/// @brief Returns the number of existing relation attribute type instances.
 			/// @return The number of existing relation attribute type instances.
 			std::size_t get_relation_attribute_instance_count() const;
 
-			/// Deletes all instances of relation attributes.
+			/// @brief Deletes all instances of relation attributes.
 			void delete_all_relation_attribute_instances();
 
 	};
