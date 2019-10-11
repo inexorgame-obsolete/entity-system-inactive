3136d2c7c1ad1a470132d783bc99b48500e8f4e6
IAmNotHanni <cmdfreak@web.de>
2019-10-11 22:40:54 +0200

    Improved documentation of EntityAttributeTypeManager.

--- src/entity-system/managers/entity-attributes/entity-attribute-type-manager/EntityAttributeTypeManager.cpp
+++ src/entity-system/managers/entity-attributes/entity-attribute-type-manager/EntityAttributeTypeManager.cpp
@@ -11,42 +11,28 @@ namespace entity_system {
 	{
 	}
 
-	bool EntityAttributeTypeManager::does_entity_attribute_type_exist(const std::string& ent_attr_type_name)
+	bool EntityAttributeTypeManager::does_entity_attribute_type_exist(const std::string& attr_type_name)
 	{
 		// Call template base class method.
-		return does_type_exist(ent_attr_type_name);
+		return does_type_exist(attr_type_name);
 	}
 
-	bool EntityAttributeTypeManager::does_entity_attribute_type_exist(const EntityAttributeTypePtr& ent_attr_type)
+	bool EntityAttributeTypeManager::does_entity_attribute_type_exist(const xg::Guid& type_GUID)
 	{
-		return does_entity_attribute_type_exist(ent_attr_type->get_type_name());
+		// Call template base class method.
+		return does_entity_attribute_type_exist(type_GUID);
 	}
 
-	EntityAttributeTypePtrOpt EntityAttributeTypeManager::create_entity_attribute_type(const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features)
+	bool EntityAttributeTypeManager::does_entity_attribute_type_exist(const EntityAttributeTypePtr& attr_type)
+	{
+		// Call template base class method.
+		return does_entity_attribute_type_exist(attr_type->get_GUID());
+	}
+
+	EntityAttributeTypePtrOpt EntityAttributeTypeManager::create_entity_attribute_type(const std::string& attr_type_name, const DataType& attr_type_datatype, const EnumSet<Feature>& attr_type_features)
 	{
 		// Check if entity attribute type's name is not empty.
-		if(! is_type_name_valid(ent_attr_type_name))
-		{
-		// This entity attribute type does already exist.
-		// TODO: Throw error message.
-
-		// Since we've not created a new entity attribute type we can
-		// now return std::nullopt thanks to std::optional.
-		return std::nullopt;
-		}
-
-		// Create entity attribute type.
-		EntityAttributeTypePtr new_ent_attr_type = std::make_shared<EntityAttributeType>(ent_attr_type_name, ent_attr_type_datatype, ent_attr_type_features);
-
-		// Call template base class method.
-		add_type(ent_attr_type_name, new_ent_attr_type->get_GUID(), new_ent_attr_type);
-
-		return EntityAttributeTypePtrOpt { new_ent_attr_type };
-	}
-
-	EntityAttributeTypePtrOpt EntityAttributeTypeManager::create_entity_attribute_type(const xg::Guid& ent_attr_type_GUID, const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features)
-	{
-		if(! is_type_name_valid(ent_attr_type_name))
+		if(! is_type_name_valid(attr_type_name))
 		{
 			// This entity attribute type does already exist.
 			// TODO: Throw error message.
@@ -56,26 +42,48 @@ namespace entity_system {
 			return std::nullopt;
 		}
 
-		EntityAttributeTypePtr new_ent_attr_type = std::make_shared<EntityAttributeType>(ent_attr_type_name, ent_attr_type_datatype, ent_attr_type_features);
+		// Create entity attribute type.
+		EntityAttributeTypePtr new_ent_attr_type = std::make_shared<EntityAttributeType>(attr_type_name, attr_type_datatype, attr_type_features);
 
-		return EntityAttributeTypePtrOpt {  };
+		// Call template base class method.
+		add_type(attr_type_name, new_ent_attr_type->get_GUID(), new_ent_attr_type);
+
+		return EntityAttributeTypePtrOpt { new_ent_attr_type };
 	}
 
-	/// Get an entity attribute type by name.
-	/// @param ? ?
+	EntityAttributeTypePtrOpt EntityAttributeTypeManager::create_entity_attribute_type(const xg::Guid& attr_type_GUID, const std::string& attr_type_name, const DataType& attr_type_datatype, const EnumSet<Feature>& attr_type_features)
+	{
+		if(! is_type_name_valid(attr_type_name))
+		{
+			// This entity attribute type does already exist.
+			// TODO: Throw error message.
+
+			// Since we've not created a new entity attribute type we can
+			// now return std::nullopt thanks to std::optional.
+			return std::nullopt;
+		}
+
+		// Create entity attribute type.
+		EntityAttributeTypePtr new_ent_attr_type = std::make_shared<EntityAttributeType>(attr_type_GUID, attr_type_name, attr_type_datatype, attr_type_features);
+		
+		// Call template base class method.
+		add_type(attr_type_name, new_ent_attr_type->get_GUID(), new_ent_attr_type);
+
+		return EntityAttributeTypePtrOpt { new_ent_attr_type };
+	}
+
 	EntityAttributeTypePtrOpt EntityAttributeTypeManager::get_entity_attribute_type(const std::string& type_name) const
 	{
+		/// TODO Implement!
 		/*
 		if(does_type_exist(type_name))
 		{
-		//return std::optional<EntityAttributeTypePtr> {  };
+			//return std::optional<EntityAttributeTypePtr> {  };
 		}
 		*/
 		return std::nullopt;
 	}
 
-	/// Get an entity attribute type by GUID.
-	/// @param ? ?
 	EntityAttributeTypePtrOpt EntityAttributeTypeManager::get_entity_attribute_type(const xg::Guid& type_GUID) const
 	{
 		return std::nullopt;
@@ -87,16 +95,16 @@ namespace entity_system {
 		return get_type_count();
 	}
 
-	void EntityAttributeTypeManager::delete_entity_attribute_type(const std::string& ent_attr_type)
+	void EntityAttributeTypeManager::delete_entity_attribute_type(const std::string& attr_type)
 	{
 		// Call template base class method.
-		delete_type(ent_attr_type);
+		delete_type(attr_type);
 	}
 
-	void EntityAttributeTypeManager::delete_entity_attribute_type(const EntityAttributeTypePtr& ent_attr_type)
+	void EntityAttributeTypeManager::delete_entity_attribute_type(const EntityAttributeTypePtr& attr_type)
 	{
 		// Call template base class method.
-		delete_type(ent_attr_type->get_type_name());
+		delete_type(attr_type->get_GUID());
 	}
 
 	void EntityAttributeTypeManager::delete_entity_attribute_type(const xg::Guid& type_GUID)
--- src/entity-system/managers/entity-attributes/entity-attribute-type-manager/EntityAttributeTypeManager.hpp
+++ src/entity-system/managers/entity-attributes/entity-attribute-type-manager/EntityAttributeTypeManager.hpp
@@ -22,69 +22,68 @@ namespace entity_system {
 	{
 		public:
 
-		/// @brief Constructor.
-		EntityAttributeTypeManager();
+			/// @brief Constructor.
+			EntityAttributeTypeManager();
 
-		/// @brief Destructor.
-		~EntityAttributeTypeManager();
+			/// @brief Destructor.
+			~EntityAttributeTypeManager();
 
-		/// @brief Creates a new entity attribute type.
-		/// @param ent_attr_type_name The name of the new entity attribute type.
-		/// @param ent_attr_type_data_type The data type of the new entity attribute type.
-		/// @param ent_attr_type_features The features of the new entity attribute type.
-		/// @return ?
-		EntityAttributeTypePtrOpt create_entity_attribute_type(const std::string&, const DataType&, const EnumSet<Feature>&);
+			/// @brief Creates a new entity attribute type.
+			/// @param ent_attr_type_name The name of the new entity attribute type.
+			/// @param ent_attr_type_data_type The data type of the new entity attribute type.
+			/// @param ent_attr_type_features The features of the new entity attribute type.
+			/// @return A std::optional shared pointer to the entity attribute type which was created.
+			EntityAttributeTypePtrOpt create_entity_attribute_type(const std::string& attr_type_name, const DataType& attr_type_datatype, const EnumSet<Feature>& attr_type_features);
 
-		/// @brief Creates a new entity attribute type.
-		/// @param ?
-		/// @param ?
-		/// @param ?
-		/// @return ?
-		EntityAttributeTypePtrOpt create_entity_attribute_type(const xg::Guid&, const std::string&, const DataType&, const EnumSet<Feature>&);
+			/// @brief Creates a new entity attribute type.
+			/// @param attr_type_GUID The GUID of the new entity attribute type.
+			/// @param ent_attr_type_name The name of the new entity attribute type.
+			/// @param ent_attr_type_data_type The data type of the new entity attribute type.
+			/// @param ent_attr_type_features The features of the new entity attribute type.
+			/// @return A std::optional shared pointer to the entity attribute type which was created.
+			EntityAttributeTypePtrOpt create_entity_attribute_type(const xg::Guid& attr_type_GUID, const std::string& attr_type_name, const DataType& attr_type_datatype, const EnumSet<Feature>& attr_type_features);
 
-		/// @brief Checks if an entity attribute type does already exist.
-		/// @param ent_attr_type_name The name of an entity
-		/// attribute type of which we will check if it does already exist.
-		/// @return True if the entity attribute type
-		/// does already exist, false otherwise.
-		bool does_entity_attribute_type_exist(const std::string&);
+			/// @brief Checks if an entity attribute type does already exist.
+			/// @param attr_type_name The name of an entity attribute type of which we will check if it does already exist.
+			/// @return True if the entity attribute type does already exist, false otherwise.
+			bool does_entity_attribute_type_exist(const std::string& attr_type_name);
 
-		bool does_entity_attribute_type_exist(const xg::Guid&);
+			/// @brief Checks if an entity attribute type does already exist.
+			/// @param type_GUID The GUID of this type.
+			/// @return True if the entity attribute type does already exist, false otherwise.
+			bool does_entity_attribute_type_exist(const xg::Guid& type_GUID);
 
-		/// Checks if an entity attribute type does already exist.
-		/// @param ent_attr_type A const reference to a shared pointer of
-		/// an entity attribute type of which we will check if it
-		/// does already exist.
-		/// @return True if the entity attribute type
-		/// does already exist, false otherwise.
-		bool does_entity_attribute_type_exist(const EntityAttributeTypePtr&);
+			/// Checks if an entity attribute type does already exist.
+			/// @param attr_type A const reference to a shared pointer of an entity attribute type of which we will check if it does already exist.
+			/// @return True if the entity attribute type does already exist, false otherwise.
+			bool does_entity_attribute_type_exist(const EntityAttributeTypePtr& attr_type);
 
-		/// Get an entity attribute type by name.
-		/// @param type_name The name of the type we tant to check for.
-		EntityAttributeTypePtrOpt get_entity_attribute_type(const std::string&) const;
+			/// @brief Get an entity attribute type by name.
+			/// @param type_name The name of the type we tant to check for.
+			EntityAttributeTypePtrOpt get_entity_attribute_type(const std::string&) const;
 
-		/// Get an entity attribute type by GUID.
-		/// @param type_GUID The GUID of the type we tant to check for.
-		EntityAttributeTypePtrOpt get_entity_attribute_type(const xg::Guid&) const;
+			/// @brief Get an entity attribute type by GUID.
+			/// @param type_GUID The GUID of the type we tant to check for.
+			EntityAttributeTypePtrOpt get_entity_attribute_type(const xg::Guid& type_GUID) const;
 
-		/// Returns the number of available entity attribute types.
-		/// @return The number of available entity attribute types.
-		std::size_t get_entity_attribute_type_count() const;
+			/// @brief Returns the number of available entity attribute types.
+			/// @return The number of available entity attribute types.
+			std::size_t get_entity_attribute_type_count() const;
 
-		/// Deletes an entity attribute type.
-		/// @param ent_attr_type The name of the entity attribute type which will be deleted.
-		void delete_entity_attribute_type(const std::string&);
+			/// @brief Deletes an entity attribute type.
+			/// @param attr_type The name of the entity attribute type which will be deleted.
+			void delete_entity_attribute_type(const std::string& attr_type);
 
-		/// Deletes an entity attribute type.
-		/// @param ent_attr_type A const reference to a shared pointer of an entity attribute type which will be deleted.
-		void delete_entity_attribute_type(const EntityAttributeTypePtr&);
+			/// @brief Deletes an entity attribute type.
+			/// @param attr_type A const reference to a shared pointer of an entity attribute type which will be deleted.
+			void delete_entity_attribute_type(const EntityAttributeTypePtr& attr_type);
 
-		/// Deletes an entity attribute type.
-		/// @param type_GUID The GUID of the entity attribute type.
-		void delete_entity_attribute_type(const xg::Guid&);
+			/// @brief Deletes an entity attribute type.
+			/// @param type_GUID The GUID of the entity attribute type.
+			void delete_entity_attribute_type(const xg::Guid& type_GUID);
 
-		/// Delete all entity attribute types.
-		void delete_all_entity_attribute_types();
+			/// @brief Delete all entity attribute types.
+			void delete_all_entity_attribute_types();
 
 };
 
