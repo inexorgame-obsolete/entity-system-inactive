d7743f1389b103b62ed8d7ff1b0f370b837aeebd
IAmNotHanni <cmdfreak@web.de>
2019-10-12 23:18:49 +0200

    Improved documentation of EntityTypeCreatedListener.

--- src/entity-system/listeners/entities/EntityTypeCreatedListener.hpp
+++ src/entity-system/listeners/entities/EntityTypeCreatedListener.hpp
@@ -13,6 +13,7 @@ namespace entity_system {
 
 			/// @brief Called if a new entity type has been created.
 			/// @param entity_type The entity type which has been created.
+			/// @TODO const std::shared_ptr<T>& instead?
 			virtual void on_entity_type_created(std::shared_ptr<EntityType> entity_type) = 0;
 
 	};
