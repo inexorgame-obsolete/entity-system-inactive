f68ec54d605a48c3302bb1026d413957c9a2a2e3
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-20 15:07:25 +0200

    Fix travis build

--- .travis/run_project_build.sh
+++ .travis/run_project_build.sh
@@ -4,9 +4,13 @@ conan --version
 
 cp .travis/registry.json ~/.conan/registry.json
 ln -s ~/.conan/registry.json ~/.conan/remotes.json
-rm -rf build
-conan install . --install-folder ./build --build=missing
-conan build . --build-folder ./build
+rm -rf build && mkdir -p build && cd build
+cmake ../
+make
 cd bin/
+#rm -rf build
+#conan install . --install-folder ./build --build=missing
+#conan build . --build-folder ./build
+#cd build/bin/
 ./inexor-tests
 ./inexor-benchmarks
