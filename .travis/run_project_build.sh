36a3bcd2aa845e6dedca0b33bc632fe308f18893
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-20 14:39:23 +0200

    Fix build folder

--- .travis/run_project_build.sh
+++ .travis/run_project_build.sh
@@ -2,19 +2,11 @@
 
 conan --version
 
-pwd
-ls -lah .travis/registry.json
-echo ".travis/registry.json"
-cat .travis/registry.json
-echo "~/.conan/registry.json"
-cat ~/.conan/registry.json
-echo "~/.conan/remotes.json"
-cat ~/.conan/remotes.json
 cp .travis/registry.json ~/.conan/registry.json
 ln -s ~/.conan/registry.json ~/.conan/remotes.json
 rm -rf build
-conan install . --install-folder build --build=missing
-conan build . --build-folder build
+conan install . --install-folder ./build --build=missing
+conan build . --build-folder ./build
 cd bin/
 ./inexor-tests
 ./inexor-benchmarks
