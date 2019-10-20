a96328696739ea99414ca49bc165836e628c6336
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-20 13:08:58 +0200

    Fix copy registry.json

--- .travis/run_project_build.sh
+++ .travis/run_project_build.sh
@@ -4,7 +4,9 @@ conan --version
 
 pwd
 ls -lah .travis/registry.json
-cp .travis/registry.json ~/.conan/registry.json
+cat .travis/registry.json
+ln -s ~/.conan/registry.json ~/.conan/remotes.json
+# cp .travis/registry.json ~/.conan/registry.json
 rm -rf build
 conan install . --install-folder build --build=missing
 conan build . --build-folder build
