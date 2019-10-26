191f89041cd5cd008056de8bef675295f58dddfb
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-26 21:15:13 +0200

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/command/CommandModule.hpp
+++ src/command/CommandModule.hpp
@@ -2,36 +2,29 @@
 
 #include "command/managers/CommandManager.hpp"
 
-namespace inexor {
-namespace command {
+namespace inexor::command {
 
-	/// @class CommandModule
-	/// @brief Command module.
-	class CommandModule
-	{
-		using CommandManangerPtr = std::shared_ptr<CommandManager>;
+/// @class CommandModule
+/// @brief The command module provides a system for executing commands.
+class CommandModule
+{
+    using CommandManangerPtr = std::shared_ptr<CommandManager>;
 
-		public:
+    public:
+    /// @brief Constructor.
+    /// @note The dependencies of this class will be injected automatically.
+    /// @param command_manager The command manager.
+    explicit CommandModule(CommandManangerPtr command_manager);
 
-			/// @brief Constructor.
-			/// @note The dependencies of this class will be injected automatically.
-			/// @param command_manager The command manager.
-			CommandModule(
-			CommandManangerPtr command_manager
-			);
+    /// Destructor.
+    ~CommandModule();
 
-			/// Destructor.
-			~CommandModule();
+    /// Initialization of the command module.
+    void init();
 
-			/// Initialization of the command module.
-			void init();
+    private:
+    /// The command manager.
+    CommandManangerPtr command_manager;
+};
 
-		private:
-
-			/// The command manager.
-			CommandManangerPtr command_manager;
-
-	};
-
-}
-}
+} // namespace inexor::command
