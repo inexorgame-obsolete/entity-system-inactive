ad4fbe1d7aff4dd3ce8fe3f6b17fa81fe88a20a9
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-26 23:41:24 +0200

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/managers/manager-templates/InstanceManagerTemplate.hpp
+++ src/entity-system/managers/manager-templates/InstanceManagerTemplate.hpp
@@ -1,139 +1,121 @@
 #pragma once
 
-#include <vector>
+#include <crossguid/guid.hpp>
 #include <memory>
 #include <mutex>
 #include <optional>
 #include <unordered_map>
-#include <crossguid/guid.hpp>
+#include <vector>
 
 // When using template classes it is not possible
 // to separate definitions of class methods from their declaration.
 // This is the reason why there is no cpp file for this header file!
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class InstanceManagerTemplate
-	/// @brief A template base class for instance manager classes.
-	/// @param T The type for this instance manager.
-	template <typename T> class InstanceManagerTemplate
-	{
-		private:
+/// @class InstanceManagerTemplate
+/// @brief A template base class for instance manager classes.
+/// @param T The type for this instance manager.
+template <typename T> class InstanceManagerTemplate
+{
+    private:
+    // A map of stored instances.
+    std::unordered_map<xg::Guid, std::shared_ptr<T>> stored_instances;
 
-			// A map of stored instances.
-			std::unordered_map<xg::Guid, std::shared_ptr<T>> stored_instances;
+    // Mutex for this base class.
+    std::mutex instance_manager_mutex;
 
-			// Mutex for this base class.
-			std::mutex instance_manager_mutex;
+    protected:
+    /// @brief Constructor.
+    InstanceManagerTemplate() = default;
 
-		protected:
+    /// @brief Destructor.
+    ~InstanceManagerTemplate() = default;
 
-			/// @brief Constructor.
-			InstanceManagerTemplate()
-			{
-			}
+    /// @brief Adds an instance to the instance buffer.
+    /// @param instance_GUID The GUID of the instance.
+    /// @param instance The instance which will be added.
+    void add_instance(const xg::Guid instance_GUID, const std::shared_ptr<T> &instance)
+    {
+        // Use lock guard to ensure thread safety during write operations!
+        std::lock_guard<std::mutex> lock(instance_manager_mutex);
 
-			/// @brief Destructor.
-			~InstanceManagerTemplate()
-			{
-			}
+        // TODO: But what if the instance does already exist?
+        stored_instances[instance_GUID] = instance;
+    }
 
+    /// @brief Checks if an instance does already exist.
+    /// @param instance_GUID The GUID of the instance.
+    /// @return True of the instance exists, false otherwise.
+    bool does_instance_exist(const xg::Guid instance_GUID)
+    {
+        // No mutex required as this is a read-only operation.
+        return !(stored_instances.end() == stored_instances.find(instance_GUID));
+    }
 
-			/// @brief Adds an instance to the instance buffer.
-			/// @param instance_GUID The GUID of the instance.
-			/// @param instance The instance which will be added.
-			void add_instance(const xg::Guid instance_GUID, const std::shared_ptr<T>& instance)
-			{
-				// Use lock guard to ensure thread safety during write operations!
-				std::lock_guard<std::mutex> lock(instance_manager_mutex);
+    /// @brief Returns the number of existing instances.
+    /// @return The number of existing instances.
+    [[nodiscard]] std::size_t get_instance_count() const
+    {
+        // No mutex required as this is a read-only operation.
+        return stored_instances.size();
+    }
 
-				// TODO: But what if the instance does already exist?
-				stored_instances[instance_GUID] = instance;
-			}
-			
+    /// @brief Returns a specific instance.
+    /// @param instance_GUID The GUID of the instance.
+    /// @return If the instance exists it will be returned, std::nullopt otherwise.
+    std::optional<std::shared_ptr<T>> get_instance(const xg::Guid &instance_GUID)
+    {
+        // Check if an instance with this GUID exists.
+        auto lookup = stored_instances.find(instance_GUID);
 
-			/// @brief Checks if an instance does already exist.
-			/// @param instance_GUID The GUID of the instance.
-			/// @return True of the instance exists, false otherwise.
-			bool does_instance_exist(const xg::Guid instance_GUID)
-			{
-				// No mutex required as this is a read-only operation.
-				return !(stored_instances.end() == stored_instances.find(instance_GUID));
-			}
+        // We've found a key value pair!
+        if (!(stored_instances.end() == lookup))
+        {
+            return std::optional<std::shared_ptr<T>>{lookup->second};
+        }
 
+        // We've found nothing!
+        return std::nullopt;
+    }
 
-			/// @brief Returns the number of existing instances.
-			/// @return The number of existing instances.
-			std::size_t get_instance_count() const
-			{
-				// No mutex required as this is a read-only operation.
-				return stored_instances.size();
-			}
+    /// @brief Returns all instances.
+    /// @return A std::unordered_map of GUIDs (key) and stored instances (value).
+    std::unordered_map<xg::Guid, std::shared_ptr<T>> get_all_instances() const
+    {
+        // TODO: Does this make sense?
+        return stored_instances;
+    }
 
+    /// @brief Deletes a specific instance
+    /// @param instance_GUID The GUID of the instance which should be deleted!
+    /// @return The number of deleted instances.
+    std::size_t delete_instance(const xg::Guid &instance_GUID)
+    {
+        // Use lock guard to ensure thread safety for this write operation!
+        std::lock_guard<std::mutex> lock(instance_manager_mutex);
 
+        // Erase the instance.
+        return stored_instances.erase(instance_GUID);
+    }
 
-			/// @brief Returns a specific instance.
-			/// @param instance_GUID The GUID of the instance.
-			/// @return If the instance exists it will be returned, std::nullopt otherwise.
-			std::optional<std::shared_ptr<T>> get_instance(const xg::Guid& instance_GUID)
-			{
-				// Check if an instance with this GUID exists.
-				auto lookup = stored_instances.find(instance_GUID);
+    /// @brief Deletes a specific instance.
+    /// @param instance The instance which will be deleted.
+    /// @return The numer of deleted instances.
+    std::size_t delete_instance(const std::shared_ptr<T> &instance)
+    {
+        return delete_instance(get_GUID_from_instance(instance));
+    }
 
-				// We've found a key value pair!
-				if(!(stored_instances.end() == lookup))
-				{
-					return std::optional<std::shared_ptr<T>>{ lookup->second };
-				}
+    /// @brief Deletes all instances.
+    void delete_all_instances()
+    {
+        // Use lock guard to ensure thread safety for this write operation!
+        std::lock_guard<std::mutex> lock(instance_manager_mutex);
+        stored_instances.clear();
+    }
 
-				// We've found nothing!
-				return std::nullopt;
-			}
+    /// TODO: Get all instances of a certain type.
+};
 
-
-			/// @brief Returns all instances.
-			/// @return A std::unordered_map of GUIDs (key) and stored instances (value).
-			std::unordered_map<xg::Guid, std::shared_ptr<T>> get_all_instances() const
-			{
-				// TODO: Does this make sense?
-				return stored_instances;
-			}
-
-
-			/// @brief Deletes a specific instance
-			/// @param instance_GUID The GUID of the instance which should be deleted!
-			/// @return The number of deleted instances.
-			std::size_t delete_instance(const xg::Guid& instance_GUID)
-			{
-				// Use lock guard to ensure thread safety for this write operation!
-				std::lock_guard<std::mutex> lock(instance_manager_mutex);
-
-				// Erase the instance.
-				return stored_instances.erase(instance_GUID);
-			}
-
-
-			/// @brief Deletes a specific instance.
-			/// @param instance The instance which will be deleted.
-			/// @return The numer of deleted instances.
-			std::size_t delete_instance(const std::shared_ptr<T>& instance)
-			{
-				return delete_instance(get_GUID_from_instance(instance));
-			}
-
-
-			/// @brief Deletes all instances.
-			void delete_all_instances()
-			{
-				// Use lock guard to ensure thread safety for this write operation!
-				std::lock_guard<std::mutex> lock(instance_manager_mutex);
-				stored_instances.clear();
-			}
-
-			/// TODO: Get all instances of a certain type.
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/managers/manager-templates/TypeManagerTemplate.hpp
+++ src/entity-system/managers/manager-templates/TypeManagerTemplate.hpp
@@ -1,227 +1,208 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
+#include <crossguid/guid.hpp>
 #include <memory>
 #include <mutex>
-#include <crossguid/guid.hpp>
+#include <string>
+#include <unordered_map>
 
 // When using template classes it is not possible
 // to separate definitions of class methods from their declaration.
 // This is the reason why there is no cpp file for this header file!
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class TypeManagerTemplate
-	/// @brief A template base class for type manager classes.
-	template <typename T>
-	class TypeManagerTemplate
-	{
-		private:
+/// @class TypeManagerTemplate
+/// @brief A template base class for type manager classes.
+template <typename T> class TypeManagerTemplate
+{
+    private:
+    /// An unordered map which stores all available type names as key and their corresponding GUID as value.
+    std::unordered_map<std::string, xg::Guid> stored_type_names;
 
-			/// An unordered map which stores all available type names as key and their corresponding GUID as value.
-			std::unordered_map<std::string, xg::Guid> stored_type_names;
+    /// An unordered map which stored all available GUIDs as key and their corresponding type names as value.
+    std::unordered_map<xg::Guid, std::string> stored_type_GUIDs;
 
-			/// An unordered map which stored all available GUIDs as key and their corresponding type names as value.
-			std::unordered_map<xg::Guid, std::string> stored_type_GUIDs;
+    /// An unordered map which stores all available types.
+    std::unordered_map<xg::Guid, std::shared_ptr<T>> stored_types;
 
-			/// An unordered map which stores all available types.
-			std::unordered_map<xg::Guid, std::shared_ptr<T>> stored_types;
+    /// The mutex for this base class.
+    std::mutex type_manager_mutex;
 
-			/// The mutex for this base class.
-			std::mutex type_manager_mutex;
+    protected:
+    // All of those methods must be protected since the
+    // actual implementation of the type manager class
+    // will contain a method which has an individual name
+    // but calls back to those base class methods here.
 
-	protected:
+    /// @brief Constructor.
+    TypeManagerTemplate() = default;
 
-		// All of those methods must be protected since the
-		// actual implementation of the type manager class
-		// will contain a method which has an individual name
-		// but calls back to those base class methods here.
+    /// @brief Destructor.
+    ~TypeManagerTemplate() = default;
 
-		/// @brief Constructor.
-		TypeManagerTemplate()
-		{
-		}
+    /// @brief Checks if a type already exists.
+    /// @param type_name The name of the type.
+    /// @return True if a type with this name already exists, false otherwise.
+    bool does_type_exist(const std::string &type_name)
+    {
+        // Get the GUID of this type by name.
+        xg::Guid type_GUID = get_GUID_by_type_name(type_name);
 
-		/// @brief Destructor.
-		~TypeManagerTemplate()
-		{
-		}
+        // No mutex required as this is a read-only operation.
+        return !(stored_types.end() == stored_types.find(type_GUID));
+    }
 
-		/// @brief Checks if a type already exists.
-		/// @param type_name The name of the type.
-		/// @return True if a type with this name already exists, false otherwise.
-		bool does_type_exist(const std::string& type_name)
-		{
-			// Get the GUID of this type by name.
-			xg::Guid type_GUID = get_GUID_by_type_name(type_name);
+    /// @brief Checks if a type already exists.
+    /// @param type_GUID The name of the type.
+    /// @return True if a type with this name already exists, false otherwise.
+    bool does_type_exist(const xg::Guid &type_GUID)
+    {
+        // No mutex required as this is a read-only operation.
+        return !(stored_types.end() == stored_types.find(type_GUID));
+    }
 
-			// No mutex required as this is a read-only operation.
-			return ! (stored_types.end() == stored_types.find(type_GUID));
-		}
+    /// @brief Checks if the name of a type is valid.
+    /// @param type_name The name of the type.
+    /// @return True if the name is valid, false otherwise.
+    bool is_type_name_valid(const std::string &type_name)
+    {
+        if (0 == type_name.compare(""))
+            return false;
+        if (does_type_exist(type_name))
+            return false;
+        // Add further validation methods here.
+        return true;
+    }
 
+    /// @brief Adds a new type.
+    /// @param type_name The name of the new type.
+    /// @param type_GUID The GUID of the new type.
+    /// @param new_type A shared pointer reference to the new type.
+    void add_type(const std::string &type_name, const xg::Guid &type_GUID, const std::shared_ptr<T> &new_type)
+    {
+        // Use lock guard to ensure thread safety for this write operation!
+        std::lock_guard<std::mutex> lock(type_manager_mutex);
 
-		/// @brief Checks if a type already exists.
-		/// @param type_GUID The name of the type.
-		/// @return True if a type with this name already exists, false otherwise.
-		bool does_type_exist(const xg::Guid& type_GUID)
-		{
-			// No mutex required as this is a read-only operation.
-			return !(stored_types.end() == stored_types.find(type_GUID));
-		}
+        stored_type_names[type_name] = type_GUID;
+        stored_type_GUIDs[type_GUID] = type_name;
+        stored_types[type_GUID] = new_type;
+    }
 
+    /// @brief Returns the GUID of a type by name.
+    /// @param type_name The name of the type.
+    /// @return The GUID of the type.
+    xg::Guid get_GUID_by_type_name(const std::string &type_name)
+    {
+        // No mutex required as this is a read-only operation.
+        return stored_type_names[type_name];
+    }
 
-		/// @brief Checks if the name of a type is valid.
-		/// @param type_name The name of the type.
-		/// @return True if the name is valid, false otherwise.
-		bool is_type_name_valid(const std::string& type_name)
-		{
-			if(0 == type_name.compare("")) return false;
-			if(does_type_exist(type_name)) return false;
-			// Add further validation methods here.
-			return true;
-		}
+    /// @brief Returns the name of a type by given GUID.
+    /// @param type_GUID The GUID of the type.
+    /// @return The name of the type.
+    std::string get_type_name_by_GUID(const xg::Guid &type_GUID)
+    {
+        // No mutex required as this is a read-only operation.
+        return stored_type_GUIDs[type_GUID];
+    }
 
+    /// @brief Searches for the desired type by name and returns it.
+    /// @param type_name the name of the type to search for.
+    /// @return A const shared pointer to the type.
+    std::shared_ptr<T> get_type(const std::string &type_name)
+    {
+        // Get the GUID of this type by name.
+        xg::Guid type_GUID = get_GUID_by_type_name(type_name);
 
-		/// @brief Adds a new type.
-		/// @param type_name The name of the new type.
-		/// @param type_GUID The GUID of the new type.
-		/// @param new_type A shared pointer reference to the new type.
-		void add_type(const std::string& type_name, const xg::Guid& type_GUID, const std::shared_ptr<T>& new_type)
-		{
-			// Use lock guard to ensure thread safety for this write operation!
-			std::lock_guard<std::mutex> lock(type_manager_mutex);
+        // No mutex required as this is a read-only operation.
+        return stored_types[type_name];
+    }
 
-			stored_type_names[type_name] = type_GUID;
-			stored_type_GUIDs[type_GUID] = type_name;
-			stored_types[type_GUID] = new_type;
-		}
+    /// @brief Searches for the desired type by name.
+    /// @param type_GUID The GUID of the type.
+    /// @return The type which has the requested GUID.
+    std::shared_ptr<T> get_type(const xg::Guid &type_GUID)
+    {
+        // No mutex required as this is a read-only operation.
+        return stored_types[type_GUID];
+    }
 
+    /// @brief Returns the number of existing types.
+    /// @return The number of existing types.
+    std::size_t get_type_count() const
+    {
+        // No mutex required as this is a read-only operation.
+        return stored_types.size();
+    }
 
-		/// @brief Returns the GUID of a type by name.
-		/// @param type_name The name of the type.
-		/// @return The GUID of the type.
-		xg::Guid get_GUID_by_type_name(const std::string& type_name)
-		{
-			// No mutex required as this is a read-only operation.
-			return stored_type_names[type_name];
-		}
+    /// @brief Deletes a specific type.
+    /// @param type_name The name of the type which will be deleted.
+    /// @return The number of deleted types.
+    /// @todo Does this return type make sense?
+    std::size_t delete_type(const std::string &type_name)
+    {
+        // Look up the GUID of the type which will be deleted.
+        xg::Guid type_GUID_to_delete = get_GUID_by_type_name(type_name);
 
+        // Use lock guard to ensure thread safety for this write operation!
+        std::lock_guard<std::mutex> lock(type_manager_mutex);
 
-		/// @brief Returns the name of a type by given GUID.
-		/// @param type_GUID The GUID of the type.
-		/// @return The name of the type.
-		std::string get_type_name_by_GUID(const xg::Guid& type_GUID)
-		{
-			// No mutex required as this is a read-only operation.
-			return stored_type_GUIDs[type_GUID];
-		}
+        // Erase type name.
+        stored_type_names.erase(type_name);
 
+        // Erase GUID.
+        stored_type_GUIDs.erase(type_GUID_to_delete);
 
-		/// @brief Searches for the desired type by name and returns it.
-		/// @param type_name the name of the type to search for.
-		/// @return A const shared pointer to the type.
-		std::shared_ptr<T> get_type(const std::string& type_name)
-		{
-			// Get the GUID of this type by name.
-			xg::Guid type_GUID = get_GUID_by_type_name(type_name);
+        // Erase type.
+        std::size_t deleted_types = stored_types.erase(type_name);
 
-			// No mutex required as this is a read-only operation.
-			return stored_types[type_name];
-		}
+        return deleted_types;
+    }
 
+    /// @brief Deletes a type by GUID.
+    /// @param type_GUID The GUID of the type.
+    /// @return The number of deleted types.
+    /// @todo Does this return type make sense?
+    std::size_t delete_type(const xg::Guid &type_GUID)
+    {
+        // We do not need a mutex for these read operations here.
+        std::string type_name_to_delete = get_type_name_by_GUID(type_GUID);
 
-		/// @brief Searches for the desired type by name.
-		/// @param type_GUID The GUID of the type.
-		/// @return The type which has the requested GUID.
-		std::shared_ptr<T> get_type(const xg::Guid& type_GUID)
-		{
-			// No mutex required as this is a read-only operation.
-			return stored_types[type_GUID];
-		}
+        // Look up the GUID of the type which will be deleted.
+        xg::Guid type_GUID_to_delete = get_GUID_by_type_name(type_name_to_delete);
 
+        // Use lock guard to ensure thread safety for this write operations!
+        std::lock_guard<std::mutex> lock(type_manager_mutex);
 
-		/// @brief Returns the number of existing types.
-		/// @return The number of existing types.
-		std::size_t get_type_count() const
-		{
-			// No mutex required as this is a read-only operation.
-			return stored_types.size();
-		}
+        // Erase type name.
+        stored_type_names.erase(type_name_to_delete);
 
+        // Erase GUID.
+        stored_type_GUIDs.erase(type_GUID_to_delete);
 
-		/// @brief Deletes a specific type.
-		/// @param type_name The name of the type which will be deleted.
-		/// @return The number of deleted types.
-		/// @todo Does this return type make sense?
-		std::size_t delete_type(const std::string& type_name)
-		{
-			// Look up the GUID of the type which will be deleted.
-			xg::Guid type_GUID_to_delete = get_GUID_by_type_name(type_name);
+        // Erase type.
+        std::size_t deleted_types = stored_types.erase(type_GUID);
 
-			// Use lock guard to ensure thread safety for this write operation!
-			std::lock_guard<std::mutex> lock(type_manager_mutex);
+        return deleted_types;
+    }
 
-			// Erase type name.
-			stored_type_names.erase(type_name);
+    /// @brief Deletes all types.
+    /// @note At this point it must be made sure that all the type instances have been deleted first.
+    void delete_all_types()
+    {
+        // Use lock guard to ensure thread safety during write operations!
+        std::lock_guard<std::mutex> lock(type_manager_mutex);
 
-			// Erase GUID.
-			stored_type_GUIDs.erase(type_GUID_to_delete);
+        // Erase type names.
+        stored_type_names.clear();
 
-			// Erase type.
-			std::size_t deleted_types = stored_types.erase(type_name);
+        // Erase GUIDs.
+        stored_type_GUIDs.clear();
 
-			return deleted_types;
-		}
+        // Erase types.
+        stored_types.clear();
+    }
+};
 
-
-		/// @brief Deletes a type by GUID.
-		/// @param type_GUID The GUID of the type.
-		/// @return The number of deleted types.
-		/// @todo Does this return type make sense?
-		std::size_t delete_type(const xg::Guid& type_GUID)
-		{
-			// We do not need a mutex for these read operations here.
-			std::string type_name_to_delete = get_type_name_by_GUID(type_GUID);
-
-			// Look up the GUID of the type which will be deleted.
-			xg::Guid type_GUID_to_delete = get_GUID_by_type_name(type_name_to_delete);
-
-			// Use lock guard to ensure thread safety for this write operations!
-			std::lock_guard<std::mutex> lock(type_manager_mutex);
-
-			// Erase type name.
-			stored_type_names.erase(type_name_to_delete);
-
-			// Erase GUID.
-			stored_type_GUIDs.erase(type_GUID_to_delete);
-
-			// Erase type.
-			std::size_t deleted_types = stored_types.erase(type_GUID);
-
-			return deleted_types;
-		}
-
-
-		/// @brief Deletes all types.
-		/// @note At this point it must be made sure that all the type instances have been deleted first.
-		void delete_all_types()
-		{
-			// Use lock guard to ensure thread safety during write operations!
-			std::lock_guard<std::mutex> lock(type_manager_mutex);
-
-			// Erase type names.
-			stored_type_names.clear();
-
-			// Erase GUIDs.
-			stored_type_GUIDs.clear();
-
-			// Erase types.
-			stored_types.clear();
-		}
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
