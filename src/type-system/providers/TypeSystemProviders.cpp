a42e6beeec0b3b672f41ca0835dd72d507b64bf3
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-28 00:56:02 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/type-system/providers/TypeSystemProviders.cpp
+++ src/type-system/providers/TypeSystemProviders.cpp
@@ -1,40 +1,30 @@
 #include "TypeSystemProviders.hpp"
 
-namespace inexor {
-namespace entity_system {
-namespace type_system {
+#include <utility>
 
-	TypeSystemProviders::TypeSystemProviders(
-		EntityTypeBuilderFactoryPtr entity_type_builder_factory,
-		DataProvidersPtr data_providers,
-		GeneratorProvidersPtr generator_providers,
-		InOutProvidersPtr in_out_providers,
-		MathProvidersPtr math_providers,
-		LogicalProvidersPtr logical_providers
-	)
-	{
-		this->entity_type_builder_factory = entity_type_builder_factory;
-		this->data_providers = data_providers;
-		this->generator_providers = generator_providers;
-		this->in_out_providers = in_out_providers;
-		this->math_providers = math_providers;
-		this->logical_providers = logical_providers;
-	}
+namespace inexor::entity_system::type_system {
 
-	TypeSystemProviders::~TypeSystemProviders()
-	{
-	}
-
-	void TypeSystemProviders::init()
-	{
-		entity_type_builder_factory->init();
-		data_providers->init();
-		generator_providers->init();
-		in_out_providers->init();
-		math_providers->init();
-		logical_providers->init();
-	}
-
+TypeSystemProviders::TypeSystemProviders(EntityTypeBuilderFactoryPtr entity_type_builder_factory, DataProvidersPtr data_providers, GeneratorProvidersPtr generator_providers, InOutProvidersPtr in_out_providers,
+                                         MathProvidersPtr math_providers, LogicalProvidersPtr logical_providers)
+{
+    this->entity_type_builder_factory = std::move(entity_type_builder_factory);
+    this->data_providers = std::move(data_providers);
+    this->generator_providers = std::move(generator_providers);
+    this->in_out_providers = std::move(in_out_providers);
+    this->math_providers = std::move(math_providers);
+    this->logical_providers = std::move(logical_providers);
 }
+
+TypeSystemProviders::~TypeSystemProviders() = default;
+
+void TypeSystemProviders::init()
+{
+    entity_type_builder_factory->init();
+    data_providers->init();
+    generator_providers->init();
+    in_out_providers->init();
+    math_providers->init();
+    logical_providers->init();
 }
-}
+
+} // namespace inexor::entity_system::type_system
