e5d67bcd866de30576e2d62f3235f65e5bd0bd68
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 12:55:56 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy
    * Consistent parameter names
    * Consistent variable names

--- src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.hpp
+++ src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.hpp
@@ -2,49 +2,45 @@
 
 #include "entity-system/managers/manager-templates/InstanceManagerTemplate.hpp"
 #include "entity-system/model/entity-attributes/entity-attribute-instances/EntityAttributeInstance.hpp"
-#include "entity-system/model/relations/relation-types/RelationType.hpp"
 #include "entity-system/model/relations/relation-instances/RelationInstance.hpp"
+#include "entity-system/model/relations/relation-types/RelationType.hpp"
 
 #include <optional>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// These using instructions help to shorten the following code.
-	using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
-	using RelationAttributeInstancePtr = std::shared_ptr<RelationAttributeInstance>;
-	using RelationAttributeInstancePtrOpt = std::optional<RelationAttributeInstancePtr>;
+/// These using instructions help to shorten the following code.
+using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
+using RelationAttributeInstancePtr = std::shared_ptr<RelationAttributeInstance>;
+using RelationAttributeInstancePtrOpt = std::optional<RelationAttributeInstancePtr>;
 
-	/// @class RelationAttributeInstanceManager
-	/// A manager class for relation attribute instances.
-	class RelationAttributeInstanceManager : public InstanceManagerTemplate<RelationAttributeInstance>
-	{
-		public:
+/// @class RelationAttributeInstanceManager
+/// A manager class for relation attribute instances.
+class RelationAttributeInstanceManager : public InstanceManagerTemplate<RelationAttributeInstance>
+{
+    public:
+    /// @brief Constructor.
+    RelationAttributeInstanceManager();
 
-			/// @brief Constructor.
-			RelationAttributeInstanceManager();
+    /// @brief Destructor.
+    ~RelationAttributeInstanceManager();
 
-			/// @brief Destructor.
-			~RelationAttributeInstanceManager();
+    /// @brief Creates an relation attribute instance.
+    /// @param rel_attr_type A const reference to a shared pointer of an relation attribute type of which an instance will be created.
+    /// @return A shared pointer to the relation attribute instance which was created. <br>If the creation fails std::nullopt will be returned.
+    RelationAttributeInstancePtr create_relation_attribute_instance(const RelationAttributeTypePtr &rel_attr_type);
 
-			/// @brief Creates an relation attribute instance.
-			/// @param rel_attr_type A const reference to a shared pointer of an relation attribute type of which an instance will be created.
-			/// @return A shared pointer to the relation attribute instance which was created. <br>If the creation fails std::nullopt will be returned.
-			RelationAttributeInstancePtr create_relation_attribute_instance(const RelationAttributeTypePtr& rel_attr_type);
+    /// @brief Gets a relation attribute instance.
+    /// @param rel_attr_inst_GUID The GUID of the relation attribute instance.
+    /// @return A std::optional shared pointer to the relation attribute instance.
+    std::optional<RelationAttributeInstancePtr> get_relation_attribute_instance(const xg::Guid &rel_attr_inst_GUID);
 
-			/// @brief Gets a relation attribute instance.
-			/// @param rel_attr_inst_GUID The GUID of the relation attribute instance.
-			/// @return A std::optional shared pointer to the relation attribute instance.
-			std::optional<RelationAttributeInstancePtr> get_relation_attribute_instance(const xg::Guid& rel_attr_inst_GUID);
+    /// @brief Returns the number of existing relation attribute type instances.
+    /// @return The number of existing relation attribute type instances.
+    std::size_t get_relation_attribute_instance_count() const;
 
-			/// @brief Returns the number of existing relation attribute type instances.
-			/// @return The number of existing relation attribute type instances.
-			std::size_t get_relation_attribute_instance_count() const;
+    /// @brief Deletes all instances of relation attributes.
+    void delete_all_relation_attribute_instances();
+};
 
-			/// @brief Deletes all instances of relation attributes.
-			void delete_all_relation_attribute_instances();
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/managers/relation-attributes/relation-attribute-type-manager/RelationAttributeTypeManager.hpp
+++ src/entity-system/managers/relation-attributes/relation-attribute-type-manager/RelationAttributeTypeManager.hpp
@@ -1,44 +1,40 @@
 #pragma once
 
-#include "entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp"
 #include "entity-system/managers/manager-templates/TypeManagerTemplate.hpp"
 #include "entity-system/model/relation-attributes/relation-attribute-instances/RelationAttributeInstance.hpp"
+#include "entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp"
 
 #include <optional>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
-	using RelationAttributeTypePtrOpt = std::optional<RelationAttributeTypePtr>;
+using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
+using RelationAttributeTypePtrOpt = std::optional<RelationAttributeTypePtr>;
 
-	/// @class RelationAttributeTypeManager
-	/// @brief A manager class for relation attribute types.
-	class RelationAttributeTypeManager : public TypeManagerTemplate<RelationAttributeType>
-	{
-		public:
+/// @class RelationAttributeTypeManager
+/// @brief A manager class for relation attribute types.
+class RelationAttributeTypeManager : public TypeManagerTemplate<RelationAttributeType>
+{
+    public:
+    /// @brief Constructor.
+    RelationAttributeTypeManager();
 
-			/// @brief Constructor.
-			RelationAttributeTypeManager();
+    /// @brief Destructor.
+    ~RelationAttributeTypeManager();
 
-			/// @brief Destructor.
-			~RelationAttributeTypeManager();
+    /// @brief Creates an relation attribute type.
+    /// @param rel_attr_type_name The name of the new relation attribute type.
+    /// @param rel_attr_type_data_type The data type of the new relation attribute type.
+    /// @param rel_attr_type_features The features of the new relation attribute type.
+    /// @return A shared pointer to the relation attribute type which was created.
+    RelationAttributeTypePtrOpt create_relation_attribute_type(const std::string &rel_attr_type_name, const DataType &rel_attr_type_data_type, const EnumSet<Feature> &rel_attr_type_features);
 
-			/// @brief Creates an relation attribute type.
-			/// @param rel_attr_type_name The name of the new relation attribute type.
-			/// @param rel_attr_type_data_type The data type of the new relation attribute type.
-			/// @param rel_attr_type_features The features of the new relation attribute type.
-			/// @return A shared pointer to the relation attribute type which was created.
-			RelationAttributeTypePtrOpt create_relation_attribute_type(const std::string&, const DataType&, const EnumSet<Feature>&);
+    /// @brief Returns the number of existing types of relation attributes.
+    /// @return The number of existing types of relation attributes.
+    std::size_t get_relation_attribute_type_count() const;
 
-			/// @brief Returns the number of existing types of relation attributes.
-			/// @return The number of existing types of relation attributes.
-			std::size_t get_relation_attribute_type_count() const;
+    /// @brief Deletes all types of relation attributes.
+    void delete_all_relation_attribute_types();
+};
 
-			/// @brief Deletes all types of relation attributes.
-			void delete_all_relation_attribute_types();
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
