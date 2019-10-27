9cf92bec2365802bc51f98dc5eab2a01535350a0
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:40:01 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/relation-attributes/relation-attribute-instances/RelationAttributeInstance.cpp
+++ src/entity-system/model/relation-attributes/relation-attribute-instances/RelationAttributeInstance.cpp
@@ -1,27 +1,21 @@
 #include "RelationAttributeInstance.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationAttributeInstance::RelationAttributeInstance(const RelationAttributeTypePtr& rel_attr_type)
-		: InstanceBase<RelationAttributeType>(rel_attr_type),
-		DataContainer(rel_attr_type->get_attribute_data_type())
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_attribute_instance_mutex);
-		
-		this->type = rel_attr_type->get_attribute_data_type();
-	}
+RelationAttributeInstance::RelationAttributeInstance(const RelationAttributeTypePtr &rel_attr_type) : InstanceBase<RelationAttributeType>(rel_attr_type), DataContainer(rel_attr_type->get_attribute_data_type())
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_attribute_instance_mutex);
 
-	RelationAttributeInstance::~RelationAttributeInstance()
-	{
-	}
-
-	RelationAttributeTypePtr RelationAttributeInstance::get_relation_attribute_type() const
-	{
-		// No mutex required as this is a read-only operation.
-		return get_type();
-	}
-
+    this->type = rel_attr_type->get_attribute_data_type();
 }
+
+RelationAttributeInstance::~RelationAttributeInstance() = default;
+
+RelationAttributeTypePtr RelationAttributeInstance::get_relation_attribute_type() const
+{
+    // No mutex required as this is a read-only operation.
+    return get_type();
 }
+
+} // namespace inexor::entity_system
--- src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp
+++ src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp
@@ -3,29 +3,25 @@
 #include "entity-system/model/base/attribute/AttributeBase.hpp"
 #include "entity-system/model/base/type/TypeBase.hpp"
 
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class RelationAttributeType
-	/// @brief A base class for entity relation attribute types.
-	class RelationAttributeType : public TypeBase, public AttributeBase, public GUIDBase
-	{
-		public:
+/// @class RelationAttributeType
+/// @brief A base class for entity relation attribute types.
+class RelationAttributeType : public TypeBase, public AttributeBase, public GUIDBase
+{
+    public:
+    /// @brief Constructs the relation attribute type.
+    /// @note The GUID of this relation attribute type will be created automatically by inheriting from GUIDBase!
+    /// @param rel_attr_type_name The name of this relation attribute type.
+    /// @param rel_attr_type_data_type The data type of this relation attribute type.
+    /// @param rel_attr_type_features The features of this relation attribute type.
+    RelationAttributeType(const std::string &rel_attr_type_name, const DataType &rel_attr_type_data_type, const EnumSet<Feature> &rel_attr_type_features);
 
-			/// @brief Constructs the relation attribute type.
-			/// @note The GUID of this relation attribute type will be created automatically by inheriting from GUIDBase!
-			/// @param rel_attr_type_name The name of this relation attribute type.
-			/// @param rel_attr_type_data_type The data type of this relation attribute type.
-			/// @param rel_attr_type_features The features of this relation attribute type.
-			RelationAttributeType(const std::string& rel_attr_type_name, const DataType& rel_attr_type_data_type, const EnumSet<Feature>& rel_attr_type_features);
+    /// @brief Destructs the relation attribute type.
+    ~RelationAttributeType();
+};
 
-			/// @brief Destructs the relation attribute type.
-			~RelationAttributeType();
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
