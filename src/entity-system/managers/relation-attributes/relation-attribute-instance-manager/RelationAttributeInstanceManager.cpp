8b3514f2a72f882a04b9eea291d74594f90bb46d
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 12:55:56 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy
    * Consistent parameter names
    * Consistent variable names

--- src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.cpp
+++ src/entity-system/managers/relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.cpp
@@ -1,48 +1,44 @@
 #include "RelationAttributeInstanceManager.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationAttributeInstanceManager::RelationAttributeInstanceManager() : InstanceManagerTemplate()
-	{
-	}
-
-	RelationAttributeInstanceManager::~RelationAttributeInstanceManager()
-	{
-	}
-
-	RelationAttributeInstancePtr RelationAttributeInstanceManager::create_relation_attribute_instance(const RelationAttributeTypePtr& rel_attr_type)
-	{
-		// Creates a new relation attribute type instance.
-		RelationAttributeInstancePtr new_rel_attr_inst = std::make_shared<RelationAttributeInstance>(rel_attr_type);
-
-		// Call base template class method.
-		add_instance(new_rel_attr_inst->get_GUID(), new_rel_attr_inst);
-
-		// Read only, no mutex required.
-		return RelationAttributeInstancePtr { new_rel_attr_inst };
-	}
-	
-	std::optional<RelationAttributeInstancePtr> RelationAttributeInstanceManager::get_relation_attribute_instance(const xg::Guid& rel_attr_inst_GUID)
-	{
-		if(does_instance_exist(rel_attr_inst_GUID))
-		{
-			return get_instance(rel_attr_inst_GUID);
-		}
-		return std::nullopt;
-	}
-
-	std::size_t RelationAttributeInstanceManager::get_relation_attribute_instance_count() const
-	{
-		// No mutex required as this is a read-only operation.
-		return get_instance_count();
-	}
-
-	void RelationAttributeInstanceManager::delete_all_relation_attribute_instances()
-	{
-		// No mutex required as this is a read-only operation.
-		return delete_all_instances();
-	}
-
+RelationAttributeInstanceManager::RelationAttributeInstanceManager() : InstanceManagerTemplate()
+{
 }
+
+RelationAttributeInstanceManager::~RelationAttributeInstanceManager() = default;
+
+RelationAttributeInstancePtr RelationAttributeInstanceManager::create_relation_attribute_instance(const RelationAttributeTypePtr &rel_attr_type)
+{
+    // Creates a new relation attribute type instance.
+    RelationAttributeInstancePtr new_rel_attr_inst = std::make_shared<RelationAttributeInstance>(rel_attr_type);
+
+    // Call base template class method.
+    add_instance(new_rel_attr_inst->get_GUID(), new_rel_attr_inst);
+
+    // Read only, no mutex required.
+    return RelationAttributeInstancePtr{new_rel_attr_inst};
 }
+
+std::optional<RelationAttributeInstancePtr> RelationAttributeInstanceManager::get_relation_attribute_instance(const xg::Guid &rel_attr_inst_GUID)
+{
+    if (does_instance_exist(rel_attr_inst_GUID))
+    {
+        return get_instance(rel_attr_inst_GUID);
+    }
+    return std::nullopt;
+}
+
+std::size_t RelationAttributeInstanceManager::get_relation_attribute_instance_count() const
+{
+    // No mutex required as this is a read-only operation.
+    return get_instance_count();
+}
+
+void RelationAttributeInstanceManager::delete_all_relation_attribute_instances()
+{
+    // No mutex required as this is a read-only operation.
+    return delete_all_instances();
+}
+
+} // namespace inexor::entity_system
