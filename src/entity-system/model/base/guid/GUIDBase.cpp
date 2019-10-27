26f6ebf63677d86b3b7465de25d58e4fa21db256
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:19:54 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/base/guid/GUIDBase.cpp
+++ src/entity-system/model/base/guid/GUIDBase.cpp
@@ -2,38 +2,37 @@
 
 namespace inexor::entity_system {
 
-	GUIDBase::GUIDBase()
-	{
-		// If the GUID is not given to us like in the copy constructor
-		// we generate a new one in this standard constructor.
-		generate_new_GUID();
-	}
-
-	GUIDBase::GUIDBase(const xg::Guid& param_GUID)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(guid_base_mutex);
-
-		// Copy the GUID which is given as parameter.
-		this->globally_unique_identifier = param_GUID;
-	}
-
-	GUIDBase::~GUIDBase()
-	= default;
-
-	xg::Guid GUIDBase::get_GUID() const
-	{
-		// No mutex required as this is a read-only operation.
-		return globally_unique_identifier;
-	}
-
-	void GUIDBase::generate_new_GUID()
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(guid_base_mutex);
-
-		// Generate a new GUID.
-		globally_unique_identifier = xg::newGuid();
-	}
-
+GUIDBase::GUIDBase()
+{
+    // If the GUID is not given to us like in the copy constructor
+    // we generate a new one in this standard constructor.
+    generate_new_GUID();
 }
+
+GUIDBase::GUIDBase(const xg::Guid &param_GUID)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(guid_base_mutex);
+
+    // Copy the GUID which is given as parameter.
+    this->globally_unique_identifier = param_GUID;
+}
+
+GUIDBase::~GUIDBase() = default;
+
+xg::Guid GUIDBase::get_GUID() const
+{
+    // No mutex required as this is a read-only operation.
+    return globally_unique_identifier;
+}
+
+void GUIDBase::generate_new_GUID()
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(guid_base_mutex);
+
+    // Generate a new GUID.
+    globally_unique_identifier = xg::newGuid();
+}
+
+} // namespace inexor::entity_system
