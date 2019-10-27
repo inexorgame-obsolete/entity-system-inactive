04709cd4a88d10de0c0e0286056a05afff4c23cd
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:24:09 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/base/instance/InstanceBase.hpp
+++ src/entity-system/model/base/instance/InstanceBase.hpp
@@ -1,53 +1,45 @@
 #pragma once
 
-#include <string>
 #include <memory>
 #include <mutex>
+#include <string>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class InstanceBase
-	/// @brief A template base class for instances.
-	/// @param T The type of the instance.
-	template <typename T> class InstanceBase
-	{
-		private:
+/// @class InstanceBase
+/// @brief A template base class for instances.
+/// @param T The type of the instance.
+template <typename T> class InstanceBase
+{
+    private:
+    /// A shared pointer to the type of this instance.
+    std::shared_ptr<T> type_pointer;
 
-			/// A shared pointer to the type of this instance.
-			std::shared_ptr<T> type_pointer;
+    /// The mutex of this class.
+    std::mutex instance_base_mutex;
 
-			/// The mutex of this class.
-			std::mutex instance_base_mutex;
+    protected:
+    /// @brief Returns a shared pointer to the underlying type of this instance.
+    /// @return A shared pointer to the underlying type of this instance.
+    std::shared_ptr<T> get_type() const
+    {
+        // No mutex required as this is a read-only operation.
+        return type_pointer;
+    }
 
-		protected:
+    public:
+    /// @brief Constructor.
+    /// @param type_ptr A shared pointer to the underlying type of this instance.
+    explicit InstanceBase(const std::shared_ptr<T> &type_ptr)
+    {
+        // Use lock guard to ensure thread safety during write operations!
+        std::lock_guard<std::mutex> lock(instance_base_mutex);
 
-			/// @brief Returns a shared pointer to the underlying type of this instance.
-			/// @return A shared pointer to the underlying type of this instance.
-			std::shared_ptr<T> get_type() const
-			{
-				// No mutex required as this is a read-only operation.
-				return type_pointer;
-			}
+        type_pointer = type_ptr;
+    }
 
-		public:
+    /// @brief Destructor.
+    ~InstanceBase() = default;
+};
 
-			/// @brief Constructor.
-			/// @param type_ptr A shared pointer to the underlying type of this instance.
-			InstanceBase(const std::shared_ptr<T>& type_ptr)
-			{
-				// Use lock guard to ensure thread safety during write operations!
-				std::lock_guard<std::mutex> lock(instance_base_mutex);
-
-				type_pointer = type_ptr;
-			}
-
-			/// @brief Destructor.
-			~InstanceBase()
-			{
-			}
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/model/base/type/TypeBase.hpp
+++ src/entity-system/model/base/type/TypeBase.hpp
@@ -1,40 +1,34 @@
 #pragma once
 
-#include "entity-system/model/base/uuid/GUIDBase.hpp"
+#include "entity-system/model/base/guid/GUIDBase.hpp"
 
 #include <string>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class TypeBase
-	/// @brief A template base class for types.
-	class TypeBase
-	{
-		private:
+/// @class TypeBase
+/// @brief A base class for types.
+class TypeBase
+{
+    private:
+    /// The unique name of this type.
+    std::string type_title;
 
-			/// The unique name of this type.
-			std::string type_title;
+    /// The mutex of this class.
+    std::mutex type_base_mutex;
 
-			/// The mutex of this class.
-			std::mutex type_base_mutex;
+    protected:
+    /// @brief Constructor.
+    /// @param type_name The unique name of this type.
+    explicit TypeBase(const std::string &type_name);
 
-		protected:
+    /// @brief Destructor.
+    ~TypeBase();
 
-			/// @brief Constructor.
-			/// @param type_name The unique name of this type.
-			TypeBase(const std::string& type_name);
+    public:
+    /// @brief Returns the name of this type.
+    /// @return The name of this type.
+    [[nodiscard]] std::string get_type_name() const;
+};
 
-			/// @brief Destructor.
-			~TypeBase();
-
-		public:
-
-			/// @brief Returns the name of this type.
-			/// @return The name of this type.
-			std::string get_type_name() const;
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
