e7526a96f1eba63b29450f958f518922dcc32ae8
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:41:48 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/relations/relation-instances/RelationInstance.cpp
+++ src/entity-system/model/relations/relation-instances/RelationInstance.cpp
@@ -1,71 +1,64 @@
 #include "RelationInstance.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationInstance::RelationInstance(const RelationTypePtr& rel_type, const EntityInstancePtr& ent_inst_source, const EntityInstancePtr& ent_inst_target)
-		: GUIDBase(), InstanceBase(rel_type)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_instance_mutex);
+RelationInstance::RelationInstance(const RelationTypePtr &rel_type, const EntityInstancePtr &ent_inst_source, const EntityInstancePtr &ent_inst_target) : GUIDBase(), InstanceBase(rel_type)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_instance_mutex);
 
-		// Store source entity instances.
-		source_entity_instance = ent_inst_source;
+    // Store source entity instances.
+    source_entity_instance = ent_inst_source;
 
-		// Store target entity instances.
-		destination_entity_instance = ent_inst_target;
-	}
-
-	RelationInstance::RelationInstance(const xg::Guid& inst_GUID, const RelationTypePtr& rel_type, const EntityInstancePtr& ent_inst_source, const EntityInstancePtr& ent_inst_target)
-		: GUIDBase(inst_GUID), InstanceBase(rel_type)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_instance_mutex);
-
-		// Store source entity instances.
-		source_entity_instance = ent_inst_source;
-
-		// Store target entity instances.
-		destination_entity_instance = ent_inst_target;
-	}
-
-	RelationInstance::~RelationInstance()
-	{
-	}
-
-	void RelationInstance::register_relation_attribute_instance(const RelationAttributeTypePtr& rel_attr_type, const RelationAttributeInstancePtr& rel_attr_inst)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_instance_mutex);
-
-		// Add relation attribute instance.
-		relation_attribute_instances[rel_attr_type] = rel_attr_inst;
-	}
-
-
-	RelationTypePtr RelationInstance::get_relation_type() const
-	{
-		// No mutex required as this is a read-only operation.
-		return get_type();
-	}
-
-	std::unordered_map<RelationAttributeTypePtr, RelationAttributeInstancePtr> RelationInstance::get_relation_attribute_instances() const
-	{
-		// No mutex required as this is a read-only operation.
-		return relation_attribute_instances;
-	}
-
-	EntityInstancePtr RelationInstance::get_source_entity_instance() const
-	{
-		// No mutex required as this is a read-only operation.
-		return source_entity_instance;
-	}
-
-	EntityInstancePtr RelationInstance::get_target_entity_instance() const
-	{
-		// No mutex required as this is a read-only operation.
-		return destination_entity_instance;
-	}
-
+    // Store target entity instances.
+    destination_entity_instance = ent_inst_target;
 }
+
+RelationInstance::RelationInstance(const xg::Guid &inst_GUID, const RelationTypePtr &rel_type, const EntityInstancePtr &ent_inst_source, const EntityInstancePtr &ent_inst_target) : GUIDBase(inst_GUID), InstanceBase(rel_type)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_instance_mutex);
+
+    // Store source entity instances.
+    source_entity_instance = ent_inst_source;
+
+    // Store target entity instances.
+    destination_entity_instance = ent_inst_target;
 }
+
+RelationInstance::~RelationInstance() = default;
+
+void RelationInstance::register_relation_attribute_instance(const RelationAttributeTypePtr &rel_attr_type, const RelationAttributeInstancePtr &rel_attr_inst)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_instance_mutex);
+
+    // Add relation attribute instance.
+    relation_attribute_instances[rel_attr_type] = rel_attr_inst;
+}
+
+RelationTypePtr RelationInstance::get_relation_type() const
+{
+    // No mutex required as this is a read-only operation.
+    return get_type();
+}
+
+std::unordered_map<RelationAttributeTypePtr, RelationAttributeInstancePtr> RelationInstance::get_relation_attribute_instances() const
+{
+    // No mutex required as this is a read-only operation.
+    return relation_attribute_instances;
+}
+
+EntityInstancePtr RelationInstance::get_source_entity_instance() const
+{
+    // No mutex required as this is a read-only operation.
+    return source_entity_instance;
+}
+
+EntityInstancePtr RelationInstance::get_target_entity_instance() const
+{
+    // No mutex required as this is a read-only operation.
+    return destination_entity_instance;
+}
+
+} // namespace inexor::entity_system
--- src/entity-system/model/relations/relation-types/RelationType.cpp
+++ src/entity-system/model/relations/relation-types/RelationType.cpp
@@ -1,47 +1,39 @@
 #include "RelationType.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationType::RelationType(const std::string& rel_type_name, const EntityTypePtr& ent_type_source, const EntityTypePtr& ent_type_target)
-		: GUIDBase(),
-		TypeBase(rel_type_name)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_type_mutex);
+RelationType::RelationType(const std::string &rel_type_name, const EntityTypePtr &ent_type_source, const EntityTypePtr &ent_type_target) : GUIDBase(), TypeBase(rel_type_name)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_type_mutex);
 
-		source_entity_type = ent_type_source;
-		target_entity_type = ent_type_target;
-	}
-
-	RelationType::RelationType(const xg::Guid& rel_type_GUID, const std::string& rel_type_name, const EntityTypePtr& ent_type_source, const EntityTypePtr& ent_type_target)
-		: GUIDBase(rel_type_GUID),
-		TypeBase(rel_type_name)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_type_mutex);
-
-		source_entity_type = ent_type_source;
-		target_entity_type = ent_type_target;
-	}
-
-	RelationType::~RelationType()
-	{
-	}
-
-	void RelationType::link_relation_attribute_type(const RelationAttributeTypePtr& ent_rel_attr_type)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_type_mutex);
-
-		linked_rel_attr_types.push_back(ent_rel_attr_type);
-	}
-
-	std::vector<RelationAttributeTypePtr> RelationType::get_linked_attribute_types() const
-	{
-		// No mutex required as this is a read-only operation.
-		return linked_rel_attr_types;
-	}
-
+    source_entity_type = ent_type_source;
+    target_entity_type = ent_type_target;
 }
+
+RelationType::RelationType(const xg::Guid &rel_type_GUID, const std::string &rel_type_name, const EntityTypePtr &ent_type_source, const EntityTypePtr &ent_type_target) : GUIDBase(rel_type_GUID), TypeBase(rel_type_name)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_type_mutex);
+
+    source_entity_type = ent_type_source;
+    target_entity_type = ent_type_target;
 }
+
+RelationType::~RelationType() = default;
+
+void RelationType::link_relation_attribute_type(const RelationAttributeTypePtr &ent_rel_attr_type)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_type_mutex);
+
+    linked_rel_attr_types.push_back(ent_rel_attr_type);
+}
+
+std::vector<RelationAttributeTypePtr> RelationType::get_linked_attribute_types() const
+{
+    // No mutex required as this is a read-only operation.
+    return linked_rel_attr_types;
+}
+
+} // namespace inexor::entity_system
