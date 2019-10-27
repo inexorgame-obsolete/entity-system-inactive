e90ad949467170a6fc1dd2d9856b8031eca1b8e7
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-28 00:25:26 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/type-system/factories/TypeSystemFactories.cpp
+++ src/type-system/factories/TypeSystemFactories.cpp
@@ -1,42 +1,32 @@
 #include "TypeSystemFactories.hpp"
 
-namespace inexor {
-namespace entity_system {
-namespace type_system {
+#include <utility>
 
-	TypeSystemFactories::TypeSystemFactories(
-		DataFactoriesPtr data_factories,
-		InOutFactoriesPtr in_out_factories,
-		GeneratorFactoriesPtr generator_factories,
-		LogicalFactoriesPtr logical_factories,
-		MathFactoriesPtr math_factories
-	)
-	{
-		this->data_factories = data_factories;
-		this->in_out_factories = in_out_factories;
-		this->generator_factories = generator_factories;
-		this->logical_factories = logical_factories;
-		this->math_factories = math_factories;
-	}
+namespace inexor::entity_system::type_system {
 
-	TypeSystemFactories::~TypeSystemFactories()
-	{
-	}
-
-	void TypeSystemFactories::init()
-	{
-		data_factories->init();
-		in_out_factories->init();
-		generator_factories->init();
-		logical_factories->init();
-		math_factories->init();
-	}
-
-	InOutFactoriesPtr TypeSystemFactories::get_in_out_factories()
-	{
-		return in_out_factories;
-	}
-
+TypeSystemFactories::TypeSystemFactories(DataFactoriesPtr data_factories, InOutFactoriesPtr in_out_factories, GeneratorFactoriesPtr generator_factories, LogicalFactoriesPtr logical_factories, MathFactoriesPtr math_factories)
+{
+    this->data_factories = std::move(data_factories);
+    this->in_out_factories = std::move(in_out_factories);
+    this->generator_factories = std::move(generator_factories);
+    this->logical_factories = std::move(logical_factories);
+    this->math_factories = std::move(math_factories);
 }
+
+TypeSystemFactories::~TypeSystemFactories() = default;
+
+void TypeSystemFactories::init()
+{
+    data_factories->init();
+    in_out_factories->init();
+    generator_factories->init();
+    logical_factories->init();
+    math_factories->init();
 }
+
+InOutFactoriesPtr TypeSystemFactories::get_in_out_factories()
+{
+    return in_out_factories;
 }
+
+} // namespace inexor::entity_system::type_system
