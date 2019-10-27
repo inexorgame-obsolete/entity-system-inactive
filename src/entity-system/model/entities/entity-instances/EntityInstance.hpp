92d8307d68f512bf14d394b7a69586b174993c26
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:37:24 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/entities/entity-instances/EntityInstance.hpp
+++ src/entity-system/model/entities/entity-instances/EntityInstance.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include "entity-system/model/base/guid/GUIDBase.hpp"
 #include "entity-system/model/base/instance/InstanceBase.hpp"
-#include "entity-system/model/base/uuid/GUIDBase.hpp"
 #include "entity-system/model/entities/entity-types/EntityType.hpp"
 #include "entity-system/model/entity-attributes/entity-attribute-instances/EntityAttributeInstance.hpp"
 #include "entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.hpp"
@@ -9,94 +9,89 @@
 #include <mutex>
 #include <optional>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// These using instructions help to shorten the following code.
-	using EntityTypePtr = std::shared_ptr<EntityType>;
-	using EntityAttributeTypePtr = std::shared_ptr<EntityAttributeType>;
-	using EntityAttributeInstancePtr = std::shared_ptr<EntityAttributeInstance>;
-	using EntityAttributeInstancePtrOpt = std::optional<EntityAttributeInstancePtr>;
+/// These using instructions help to shorten the following code.
+using EntityTypePtr = std::shared_ptr<EntityType>;
+using EntityAttributeTypePtr = std::shared_ptr<EntityAttributeType>;
+using EntityAttributeInstancePtr = std::shared_ptr<EntityAttributeInstance>;
+using EntityAttributeInstancePtrOpt = std::optional<EntityAttributeInstancePtr>;
 
-	/// @class EntityInstance
-	/// @brief A base class for entity instances.
-	class EntityInstance : public InstanceBase<EntityType>, public GUIDBase
-	{
-		private:
+/// @class EntityInstance
+/// @brief A base class for entity instances.
+class EntityInstance : public InstanceBase<EntityType>, public GUIDBase
+{
+    private:
+    /// A map for storing the instances by type.
+    /// Every EntityInstance can have only one instance of a certain unique EntityAttributeType.
+    std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr> instances;
 
-			/// A map for storing the instances by type.
-			/// Every EntityInstance can have only one instance of a certain unique EntityAttributeType.
-			std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr> instances;
+    /// The mutex for this class.
+    std::mutex entity_instance_mutex;
 
-			/// The mutex for this class.
-			std::mutex entity_instance_mutex;
+    public:
+    /// @brief Constructor.
+    /// @note The GUID of the new entity type instance will be created automatically by the inheritance of GUIDBase!
+    /// @param ent_type The entity type of which an instance will be created.
+    explicit EntityInstance(const EntityTypePtr &ent_type);
 
-		public:
+    /// @brief Constructor.
+    /// @param ent_type A const reference of a shared pointer to the entity type of which an instance will be created.
+    /// @param ent_inst_GUID The GUID of the entity instance which will be created.
+    /// @param ent_type The entity type of which an enity instance will be created.
+    EntityInstance(const xg::Guid &ent_inst_GUID, const EntityTypePtr &ent_type);
 
-			/// @brief Constructor.
-			/// @note The GUID of the new entity type instance will be created automatically by the inheritance of GUIDBase!
-			/// @param ent_type The entity type of which an instance will be created.
-			EntityInstance(const EntityTypePtr& ent_type);
+    /// @brief Destructor.
+    ~EntityInstance();
 
-			/// @brief Constructor.
-			/// @param ent_type A const reference of a shared pointer to the entity type of which an instance will be created.
-			/// @param ent_inst_GUID The GUID of the entity instance which will be created.
-			/// @param ent_type The entity type of which an enity instance will be created.
-			EntityInstance(const xg::Guid& ent_inst_GUID, const EntityTypePtr& ent_type);
+    /// @brief Returns the entity type.
+    /// @return The entity type of the entity instance.
+    EntityTypePtr get_entity_type() const;
 
-			/// @brief Destructor.
-			~EntityInstance();
+    /// @brief Adds an entity attribute instance to the entity instance.
+    /// @param ent_attr_type The entity attribute type.
+    /// @param ent_attr_inst The entity attribute instance.
+    void add_entity_attribute_instance(const EntityAttributeTypePtr &ent_attr_type, const EntityAttributeInstancePtr &ent_attr_inst);
 
-			/// @brief Returns the entity type.
-			/// @return The entity type of the entity instance.
-			EntityTypePtr get_entity_type() const;
+    /// @brief Returns all the attribute instances.
+    /// @return All existing attribute instances.
+    /// TODO: don't return an optional but just an empty map!
+    std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>> get_instances() const;
 
-			/// @brief Adds an entity attribute instance to the entity instance.
-			/// @param ent_attr_type The entity attribute type.
-			/// @param ent_attr_inst The entity attribute instance.
-			void add_entity_attribute_instance(const EntityAttributeTypePtr& ent_attr_type, const EntityAttributeInstancePtr& ent_attr_inst);
+    /// @brief Gets the attribute instance by name.
+    /// @return The attribute instance with the given name.
+    EntityAttributeInstancePtrOpt get_attribute_instance(const std::string &attr_name);
 
-			/// @brief Returns all the attribute instances.
-			/// @return All existing attribute instances.
-			std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>> get_instances() const;
+    /// @brief Gets the attribute instance by name.
+    /// @note The attribute must exist! No checks are done.
+    /// @TODO Add checks if the attribute exists!
+    /// @param The name of the attribute.
+    /// @return The attribute instance with the given name.
+    EntityAttributeInstancePtrOpt operator[](const std::string &attr_name);
 
-			/// @brief Gets the attribute instance by name.
-			/// @return The attribute instance with the given name.
-			EntityAttributeInstancePtrOpt get_attribute_instance(const std::string& attr_name);
+    /// @brief Returns the value of the attribute instance by name.
+    /// @tparam I The data type.
+    /// @param The name of the attribute.
+    /// @return The value of the attribute instance.
+    template <std::size_t I> auto get(const std::string &attr_name)
+    {
+        // No mutex required as this is a read-only operation.
+        return std::get<I>(get_attribute_instance(attr_name).value()->value.Value());
+    }
 
-			/// @brief Gets the attribute instance by name.
-			/// @note The attribute must exist! No checks are done.
-			/// @TODO Add checks if the attribute exists!
-			/// @param The name of the attribute.
-			/// @return The attribute instance with the given name.
-			EntityAttributeInstancePtrOpt operator[](const std::string& attr_name);
+    /// @brief Sets the value of an attribute.
+    /// @param attr_name The name of the EntityAttributeType.
+    /// @param value The value of the EntityAttributeInstance.
+    void set_own_value(const std::string &attr_name, const DataValue &value);
 
-			/// @brief Returns the value of the attribute instance by name.
-			/// @tparam I The data type.
-			/// @param The name of the attribute.
-			/// @return The value of the attribute instance.
-			template <std::size_t I>
-			auto get(const std::string& attr_name)
-			{
-				// No mutex required as this is a read-only operation.
-			    return std::get<I>(get_attribute_instance(attr_name).value()->value.Value());
-			}
+    /// @TODO Set value by GUID!
 
-			/// @brief Sets the value of an attribute.
-			/// @param attr_name The name of the EntityAttributeType.
-			/// @param value The value of the EntityAttributeInstance.
-			void set_own_value(const std::string& attr_name, DataValue value);
+    /// @brief Toggles the own value of the attribute instance by name.
+    /// @warning The attribute must exist! No checks are done.
+    /// @warning The attribute has to be of data type BOOL. No checks are done.
+    /// @param The name of the attribute.
+    /// @todo: Add type check here! Toggle should only be available for bool.
+    void toggle(const std::string &attr_name);
+};
 
-			/// @TODO Set value by GUID!
-
-			/// @brief Toggles the own value of the attribute instance by name.
-			/// @warning The attribute must exist! No checks are done.
-			/// @warning The attribute has to be of data type BOOL. No checks are done.
-			/// @param The name of the attribute.
-			/// @todo: Add type check here! Toggle should only be available for bool.
-			void toggle(const std::string& attr_name);
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/model/entities/entity-types/EntityType.hpp
+++ src/entity-system/model/entities/entity-types/EntityType.hpp
@@ -1,87 +1,74 @@
 #pragma once
 
-#include "entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.hpp"
 #include "entity-system/model/base/type/TypeBase.hpp"
+#include "entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.hpp"
 
 #include <mutex>
 #include <optional>
 #include <unordered_map>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	using EntityAttributeTypePtr = std::shared_ptr<EntityAttributeType>;
-	using EntityAttributeTypePtrList = std::vector<EntityAttributeTypePtr>;
+using EntityAttributeTypePtr = std::shared_ptr<EntityAttributeType>;
+using EntityAttributeTypePtrList = std::vector<EntityAttributeTypePtr>;
 
-	/// @class EntityType
-	/// @brief A base class for entity types.
-	class EntityType : public TypeBase, public GUIDBase
-	{
-		private:
+/// @class EntityType
+/// @brief A base class for entity types.
+class EntityType : public TypeBase, public GUIDBase
+{
+    private:
+    /// A vector of stored entity attributes.
+    EntityAttributeTypePtrList entity_attribute_types;
 
-			/// A vector of stored entity attributes.
-			EntityAttributeTypePtrList entity_attribute_types;
+    /// Mutex for the entity type class.
+    std::mutex entity_type_mutex;
 
-			/// Mutex for the entity type class.
-			std::mutex entity_type_mutex;
+    public:
+    /// @brief Constructor.
+    /// @note The GUID of the new entity type will be created automatically by the inheritance of GUIDBase!
+    /// @param ent_type_name The unique name of the entity type which will be created.
+    explicit EntityType(const std::string &ent_type_name);
 
-		public:
+    /// @brief Constructor.
+    /// @param ent_type_GUID The GUID of the entity type which will be created.
+    /// @param ent_type_name The unique name of the entity type which will be created.
+    EntityType(const xg::Guid &ent_type_GUID, const std::string &ent_type_name);
 
-			/// @brief Constructor.
-			/// @note The GUID of the new entity type will be created automatically by the inheritance of GUIDBase!
-			/// @param ent_type_name The unique name of the entity type which will be created.
-			EntityType(const std::string& ent_type_name);
+    /// @brief Destructor.
+    ~EntityType();
 
+    /// @brief Check if an entity attribute type is already linked to this entity type.
+    /// @param ent_attr_type_name The name of the entity attribute type of which will be checked if it is associated with this entity type.
+    /// @return True if this entity attribute type is already linked, false otherwise.
+    bool has_attribute_type(const std::string &ent_attr_type_name);
 
-			/// @brief Constructor.
-			/// @param ent_type_GUID The GUID of the entity type which will be created.
-			/// @param ent_type_name The unique name of the entity type which will be created.
-			EntityType(const xg::Guid& ent_type_GUID, const std::string& ent_type_name);
+    /// @brief Check if an entity attribute type is already linked to this entity type.
+    /// @param ent_attr_type_GUID The GUID of the entity attribute type of which will be checked if it is associated with this entity type.
+    /// @return True if this entity attribute type is already linked, false otherwise.
+    bool has_attribute_type(const xg::Guid &ent_attr_type_GUID);
 
+    /// @brief Check if an entity attribute type is already linked to this entity type.
+    /// @param ent_attr_type A const reference of a shared pointer to the entity attribute type
+    /// of which will be checked if it is linked with this entity type.
+    /// @return True if this entity attribute type is already associated, false otherwise.
+    bool has_attribute_type(const EntityAttributeTypePtr &ent_attr_type);
 
-			/// @brief Destructor.
-			~EntityType();
+    /// @brief Links an entity attribute type to this entity type.
+    /// @param ent_attr_type A const reference of a shared pointer to an entity attribute type which will be linked to this entity type.
+    /// @return True if linking succeeded.
+    bool link_attribute_type(const EntityAttributeTypePtr &ent_attr_type);
 
+    /// @brief Returns the number of entity attribute types which are linked to this entity type.
+    /// @return The number of entity attribute types which are linked to this entity type.
+    [[nodiscard]] std::size_t get_linked_attributes_count() const;
 
-			/// @brief Check if an entity attribute type is already linked to this entity type.
-			/// @param ent_attr_type_name The name of the entity attribute type of which will be checked if it is associated with this entity type.
-			/// @return True if this entity attribute type is already linked, false otherwise.
-			bool has_attribute_type(const std::string& ent_attr_type_name);
+    /// @brief Returns a vector of entity attribute types which are linked to this entity type.
+    /// @return a vector of entity attribute types which are linked to this entity type.
+    /// TODO: Don't return an optional but an vector which can be checked for emptiness
+    [[nodiscard]] std::optional<EntityAttributeTypePtrList> get_linked_attribute_types() const;
 
+    /// @brief Deletes all entity attribute types which are linked to this entity type.
+    void reset_linked_attribute_types();
+};
 
-			/// @brief Check if an entity attribute type is already linked to this entity type.
-			/// @param ent_attr_type_GUID The GUID of the entity attribute type of which will be checked if it is associated with this entity type.
-			/// @return True if this entity attribute type is already linked, false otherwise.
-			bool has_attribute_type(const xg::Guid& ent_attr_type_GUID);
-
-
-			/// @brief Check if an entity attribute type is already linked to this entity type.
-			/// @param ent_attr_type A const reference of a shared pointer to the entity attribute type
-			/// of which will be checked if it is linked with this entity type.
-			/// @return True if this entity attribute type is already associated, false otherwise.
-			bool has_attribute_type(const EntityAttributeTypePtr& ent_attr_type);
-
-
-			/// @brief Links an entity attribute type to this entity type.
-			/// @param ent_attr_type A const reference of a shared pointer to an entity attribute type which will be linked to this entity type.
-			/// @return True if linking succeeded.
-			bool link_attribute_type(const EntityAttributeTypePtr& ent_attr_type);
-
-
-			/// @brief Returns the number of entity attribute types which are linked to this entity type.
-			/// @return The number of entity attribute types which are linked to this entity type.
-			std::size_t get_linked_attributes_count() const;
-
-
-			/// @brief Returns a vector of entity attribute types which are linked to this entity type.
-			/// @return a vector of entity attribute types which are linked to this entity type.
-			std::optional<EntityAttributeTypePtrList> get_linked_attribute_types() const;
-
-
-			/// @brief Deletes all entity attribute types which are linked to this entity type.
-			void reset_linked_attribute_types();
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
