97f44e10caacddb46783e969b9a8f707a72eed03
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 19:05:20 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/renderer/RendererModule.cpp
+++ src/renderer/RendererModule.cpp
@@ -6,75 +6,68 @@
 
 #include <GLFW/glfw3.h>
 
-namespace inexor {
-namespace renderer {
+#include <utility>
 
-	RendererModule::RendererModule(
-		MonitorManagerPtr monitor_manager,
-		WindowManagerPtr window_manager,
-		FontManagerPtr font_manager,
-		LoadingScreenPtr loading_screen,
-		TriangleExamplePtr triangle_example
-	)
-	{
-		this->monitor_manager = monitor_manager;
-		this->window_manager = window_manager;
-		this->font_manager = font_manager;
-		this->loading_screen = loading_screen;
-		this->triangle_example = triangle_example;
-	}
+namespace inexor::renderer {
 
-	RendererModule::~RendererModule()
-	{
-	}
-
-	void RendererModule::init()
-	{
-		// TODO: Move initialization of GLFW upwards (ClientModule)
-		// Initialize the GLFW library.
-
-		// Set error callback
-		glfwSetErrorCallback([] (int error_code, const char* error_message) {
-			if (error_code == 65545) return;
-			spdlog::error("GLFW ERROR {}: {}", error_code, error_message);
-		});
-
-		if(!glfwInit())
-		{
-			spdlog::error("Failed to initialize glfw!");
-			std::exit(1);
-		}
-
-		monitor_manager->init();
-		window_manager->init();
-		font_manager->init();
-		loading_screen->init();
-		triangle_example->init();
-	}
-
-	void RendererModule::shutdown()
-	{
-		triangle_example->shutdown();
-		loading_screen->shutdown();
-		font_manager->shutdown();
-		window_manager->shutdown();
-		monitor_manager->shutdown();
-
-		// Ensure we have unset all callbacks and then terminate
-		glfwSetErrorCallback(nullptr);
-
-		glfwTerminate();
-	}
-
-	void RendererModule::update()
-	{
-		// Wrap all events in a single reactive transaction
-		// in order to avoid feedback cycles
-		DoTransaction<D>([] {
-			// Poll for and process events
-			glfwPollEvents();
-		});
-	}
-
+RendererModule::RendererModule(MonitorManagerPtr monitor_manager, WindowManagerPtr window_manager, FontManagerPtr font_manager, LoadingScreenPtr loading_screen, TriangleExamplePtr triangle_example)
+{
+    this->monitor_manager = std::move(monitor_manager);
+    this->window_manager = std::move(window_manager);
+    this->font_manager = std::move(font_manager);
+    this->loading_screen = std::move(loading_screen);
+    this->triangle_example = std::move(triangle_example);
 }
+
+RendererModule::~RendererModule() = default;
+
+void RendererModule::init()
+{
+    // TODO: Move initialization of GLFW upwards (ClientModule)
+    // Initialize the GLFW library.
+
+    // Set error callback
+    glfwSetErrorCallback([](int error_code, const char *error_message) {
+        if (error_code == 65545)
+            return;
+        spdlog::error("GLFW ERROR {}: {}", error_code, error_message);
+    });
+
+    if (!glfwInit())
+    {
+        spdlog::error("Failed to initialize glfw!");
+        std::exit(1);
+    }
+
+    monitor_manager->init();
+    window_manager->init();
+    font_manager->init();
+    loading_screen->init();
+    triangle_example->init();
 }
+
+void RendererModule::shutdown()
+{
+    triangle_example->shutdown();
+    loading_screen->shutdown();
+    font_manager->shutdown();
+    window_manager->shutdown();
+    monitor_manager->shutdown();
+
+    // Ensure we have unset all callbacks and then terminate
+    glfwSetErrorCallback(nullptr);
+
+    glfwTerminate();
+}
+
+void RendererModule::update()
+{
+    // Wrap all events in a single reactive transaction
+    // in order to avoid feedback cycles
+    DoTransaction<D>([] {
+        // Poll for and process events
+        glfwPollEvents();
+    });
+}
+
+} // namespace inexor::renderer
--- src/renderer/RendererModule.hpp
+++ src/renderer/RendererModule.hpp
@@ -1,79 +1,69 @@
 #pragma once
 
-#include "renderer/managers/MonitorManager.hpp"
-#include "renderer/managers/WindowManager.hpp"
 #include "renderer/managers/FontManager.hpp"
 #include "renderer/managers/LoadingScreen.hpp"
+#include "renderer/managers/MonitorManager.hpp"
 #include "renderer/managers/TriangleExample.hpp"
+#include "renderer/managers/WindowManager.hpp"
 
-namespace inexor {
-namespace renderer {
+namespace inexor::renderer {
 
-	/// @class RendererModule
-	/// @brief This module is responsible for the management of the windows,
-	/// the rendering of the world (octree) and the rendering of the user
-	/// interface.
-	class RendererModule
-	{
-		using MonitorManagerPtr = std::shared_ptr<MonitorManager>;
-		using WindowManagerPtr = std::shared_ptr<WindowManager>;
-		using FontManagerPtr = std::shared_ptr<FontManager>;
-		using LoadingScreenPtr = std::shared_ptr<LoadingScreen>;
-		using TriangleExamplePtr = std::shared_ptr<TriangleExample>;
+/// @class RendererModule
+/// @brief This module is responsible for the management of the windows,
+/// the rendering of the world (octree) and the rendering of the user
+/// interface.
+class RendererModule
+{
+    using MonitorManagerPtr = std::shared_ptr<MonitorManager>;
+    using WindowManagerPtr = std::shared_ptr<WindowManager>;
+    using FontManagerPtr = std::shared_ptr<FontManager>;
+    using LoadingScreenPtr = std::shared_ptr<LoadingScreen>;
+    using TriangleExamplePtr = std::shared_ptr<TriangleExample>;
 
-		public:
+    public:
+    /// @brief Constructor.
+    /// @note The dependencies of this class will be injected automatically.
+    /// @note The TriangleExample is only an example right now.
+    /// @param monitor_manager The monitor manager.
+    /// @param window_manager The window manager.
+    /// @param loading_screen The loading screen.
+    /// @param triangle_example The triangle example.
+    RendererModule(MonitorManagerPtr monitor_manager, WindowManagerPtr window_manager, FontManagerPtr font_manager, LoadingScreenPtr loading_screen, TriangleExamplePtr triangle_example
+                   // UserInterfaceRenderer
+                   // WorldRenderer
+                   // ...
+    );
 
-			/// @brief Constructor.
-			/// @note The dependencies of this class will be injected automatically.
-			/// @note The TriangleExample is only an example right now.
-			/// @param monitor_manager The monitor manager.
-			/// @param window_manager The window manager.
-			/// @param loading_screen The loading screen.
-			/// @param triangle_example The triangle example.
-			RendererModule(
-				MonitorManagerPtr monitor_manager,
-				WindowManagerPtr window_manager,
-				FontManagerPtr font_manager,
-				LoadingScreenPtr loading_screen,
-				TriangleExamplePtr triangle_example
-				// UserInterfaceRenderer
-				// WorldRenderer
-				// ...
-			);
+    /// Destructor.
+    ~RendererModule();
 
-			/// Destructor.
-			~RendererModule();
+    /// Initialization of the renderer module.
+    void init();
 
-			/// Initialization of the renderer module.
-			void init();
+    /// Shutdown of the renderer module.
+    void shutdown();
 
-			/// Shutdown of the renderer module.
-			void shutdown();
+    /// Updates the frame in main thread.
+    void update();
 
-			/// Updates the frame in main thread.
-			void update();
+    //			/// Returns the window manager.
+    //			WindowManagerPtr get_window_manager();
 
-//			/// Returns the window manager.
-//			WindowManagerPtr get_window_manager();
+    private:
+    /// The monitor manager.
+    MonitorManagerPtr monitor_manager;
 
-		private:
+    /// The window manager.
+    WindowManagerPtr window_manager;
 
-			/// The monitor manager.
-			MonitorManagerPtr monitor_manager;
+    /// The font manager.
+    FontManagerPtr font_manager;
 
-			/// The window manager.
-			WindowManagerPtr window_manager;
+    /// The loading screen.
+    LoadingScreenPtr loading_screen;
 
-			/// The font manager.
-			FontManagerPtr font_manager;
+    /// The triangle example.
+    TriangleExamplePtr triangle_example;
+};
 
-			/// The loading screen.
-			LoadingScreenPtr loading_screen;
-
-			/// The triangle example.
-			TriangleExamplePtr triangle_example;
-
-	};
-
-}
-}
+} // namespace inexor::renderer
