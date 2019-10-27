1009dad199138a55aa75903abd515d3ac8ae6aaf
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:07:55 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/managers/EntityManager.cpp
+++ src/entity-system/managers/EntityManager.cpp
@@ -1,38 +1,32 @@
 #include "EntityManager.hpp"
 
-namespace inexor {
-namespace entity_system {
+#include <utility>
 
-	EntityManager::EntityManager(
-		std::shared_ptr<inexor::entity_system::EntityTypeManager> entity_type_manager,
-		std::shared_ptr<inexor::entity_system::EntityInstanceManager> entity_instance_manager,
-		std::shared_ptr<inexor::entity_system::EntityAttributeTypeManager> entity_attribute_type_manager,
-		std::shared_ptr<inexor::entity_system::EntityAttributeInstanceManager> entity_attribute_instance_manager
-	)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_manager_mutex);
+namespace inexor::entity_system {
 
-		this->entity_type_manager = entity_type_manager;
-		this->entity_instance_manager = entity_instance_manager;
-		this->entity_attribute_type_manager = entity_attribute_type_manager;
-		this->entity_attribute_instance_manager = entity_attribute_instance_manager;
-	}
+EntityManager::EntityManager(std::shared_ptr<inexor::entity_system::EntityTypeManager> entity_type_manager, std::shared_ptr<inexor::entity_system::EntityInstanceManager> entity_instance_manager,
+                             std::shared_ptr<inexor::entity_system::EntityAttributeTypeManager> entity_attribute_type_manager, std::shared_ptr<inexor::entity_system::EntityAttributeInstanceManager> entity_attribute_instance_manager)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_manager_mutex);
 
-	EntityManager::~EntityManager()
-	{
-	}
-
-	void EntityManager::reset_attributes_and_types()
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_manager_mutex);
-		
-		entity_attribute_instance_manager->delete_all_entity_attribute_instances();
-		entity_attribute_type_manager->delete_all_entity_attribute_types();
-		entity_instance_manager->delete_all_entity_instances();
-		entity_type_manager->delete_all_entity_types();
-	}
-
+    this->entity_type_manager = std::move(entity_type_manager);
+    this->entity_instance_manager = std::move(entity_instance_manager);
+    this->entity_attribute_type_manager = std::move(entity_attribute_type_manager);
+    this->entity_attribute_instance_manager = std::move(entity_attribute_instance_manager);
 }
+
+EntityManager::~EntityManager() = default;
+
+void EntityManager::reset_attributes_and_types()
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_manager_mutex);
+
+    entity_attribute_instance_manager->delete_all_entity_attribute_instances();
+    entity_attribute_type_manager->delete_all_entity_attribute_types();
+    entity_instance_manager->delete_all_entity_instances();
+    entity_type_manager->delete_all_entity_types();
 }
+
+} // namespace inexor::entity_system
--- src/entity-system/managers/RelationManager.cpp
+++ src/entity-system/managers/RelationManager.cpp
@@ -1,38 +1,32 @@
 #include "RelationManager.hpp"
 
+#include <utility>
+
 using namespace inexor::entity_system;
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationManager::RelationManager(
-		std::shared_ptr<RelationTypeManager> relation_type_manager,
-		std::shared_ptr<RelationInstanceManager> relation_instance_manager,
-		std::shared_ptr<RelationAttributeTypeManager> relation_attribute_type_manager,
-		std::shared_ptr<RelationAttributeInstanceManager> relation_attribute_instance_manager
-	)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(relation_manager_mutex);
+RelationManager::RelationManager(std::shared_ptr<RelationTypeManager> relation_type_manager, std::shared_ptr<RelationInstanceManager> relation_instance_manager, std::shared_ptr<RelationAttributeTypeManager> relation_attribute_type_manager,
+                                 std::shared_ptr<RelationAttributeInstanceManager> relation_attribute_instance_manager)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(relation_manager_mutex);
 
-		this->relation_type_manager = relation_type_manager;
-		this->relation_instance_manager = relation_instance_manager;
-		this->relation_attribute_type_manager = relation_attribute_type_manager;
-		this->relation_attribute_instance_manager = relation_attribute_instance_manager;
-	}
-
-	RelationManager::~RelationManager()
-	{
-	}
-
-	void RelationManager::reset_relations_and_relation_attributes()
-	{
-		// TODO: Use lock_guard here as well?
-		relation_attribute_instance_manager->delete_all_relation_attribute_instances();
-		relation_attribute_type_manager->delete_all_relation_attribute_types();
-		relation_instance_manager->delete_all_relation_instances();
-		relation_type_manager->delete_all_relation_types();
-	}
-
+    this->relation_type_manager = std::move(relation_type_manager);
+    this->relation_instance_manager = std::move(relation_instance_manager);
+    this->relation_attribute_type_manager = std::move(relation_attribute_type_manager);
+    this->relation_attribute_instance_manager = std::move(relation_attribute_instance_manager);
 }
+
+RelationManager::~RelationManager() = default;
+
+void RelationManager::reset_relations_and_relation_attributes()
+{
+    // TODO: Use lock_guard here as well?
+    relation_attribute_instance_manager->delete_all_relation_attribute_instances();
+    relation_attribute_type_manager->delete_all_relation_attribute_types();
+    relation_instance_manager->delete_all_relation_instances();
+    relation_type_manager->delete_all_relation_types();
 }
+
+} // namespace inexor::entity_system
