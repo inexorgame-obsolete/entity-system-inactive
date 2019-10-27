4b66806ecd44305de6ee1b74c8d97e5c073824ac
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:40:01 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/relation-attributes/relation-attribute-instances/RelationAttributeInstance.hpp
+++ src/entity-system/model/relation-attributes/relation-attribute-instances/RelationAttributeInstance.hpp
@@ -1,39 +1,34 @@
 #pragma once
 
-#include "entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp"
 #include "entity-system/model/base/instance/InstanceBase.hpp"
+#include "entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// This using instruction helps to shorten the following code.
-	using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
+/// This using instruction helps to shorten the following code.
+using RelationAttributeTypePtr = std::shared_ptr<RelationAttributeType>;
 
-	/// @class RelationAttributeInstance
-	/// @brief A base class for relation attribute instances.
-	class RelationAttributeInstance : public InstanceBase<RelationAttributeType>, public DataContainer, public GUIDBase
-	{
-		private:
+/// @class RelationAttributeInstance
+/// @brief A base class for relation attribute instances.
+class RelationAttributeInstance : public InstanceBase<RelationAttributeType>, public DataContainer, public GUIDBase
+{
+    private:
+    /// The mutex of this class
+    std::mutex relation_attribute_instance_mutex;
 
-			/// The mutex of this class
-			std::mutex relation_attribute_instance_mutex;
-		
-		public:
+    public:
+    /// @brief Constructs the relation attribute instance.
+    /// @note The GUID of this relation attribute instance will be created automatically by inheriting from GUIDBase!
+    /// @param rel_attr_type A const reference to a shared pointer of a relation attribute type of which an instance will be created.
+    /// @todo Add a mutex to ensure thread-safety!
+    explicit RelationAttributeInstance(const RelationAttributeTypePtr &rel_attr_type);
 
-			/// @brief Constructs the relation attribute instance.
-			/// @note The GUID of this relation attribute instance will be created automatically by inheriting from GUIDBase!
-			/// @param rel_attr_type A const reference to a shared pointer of a relation attribute type of which an instance will be created.
-			/// @todo Add a mutex to ensure thread-safety!
-			RelationAttributeInstance(const RelationAttributeTypePtr& rel_attr_type);
+    /// @brief Destructs the relation attribute instance.
+    ~RelationAttributeInstance();
 
-			/// @brief Destructs the relation attribute instance.
-			~RelationAttributeInstance();
+    /// @brief Returns the relation attribute type.
+    /// @return The attribte type.
+    [[nodiscard]] RelationAttributeTypePtr get_relation_attribute_type() const;
+};
 
-			/// @brief Returns the relation attribute type.
-			/// @return The attribte type.
-			RelationAttributeTypePtr get_relation_attribute_type() const;
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.cpp
+++ src/entity-system/model/relation-attributes/relation-attribute-types/RelationAttributeType.cpp
@@ -1,16 +1,12 @@
 #include "RelationAttributeType.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	RelationAttributeType::RelationAttributeType(const std::string& rel_attr_type_name, const DataType& rel_attr_type_data_type, const EnumSet<Feature>& rel_attr_type_features)
-		: TypeBase(rel_attr_type_name), AttributeBase(rel_attr_type_data_type, rel_attr_type_features)
-	{
-	}
-
-	RelationAttributeType::~RelationAttributeType()
-	{
-	}
-
+RelationAttributeType::RelationAttributeType(const std::string &rel_attr_type_name, const DataType &rel_attr_type_data_type, const EnumSet<Feature> &rel_attr_type_features)
+    : TypeBase(rel_attr_type_name), AttributeBase(rel_attr_type_data_type, rel_attr_type_features)
+{
 }
-}
+
+RelationAttributeType::~RelationAttributeType() = default;
+
+} // namespace inexor::entity_system
