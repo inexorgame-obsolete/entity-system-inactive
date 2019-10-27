656ac22e58b28ed0fe91108807ba3e3ac0bd17a2
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:39:03 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.cpp
+++ src/entity-system/model/entity-attributes/entity-attribute-types/EntityAttributeType.cpp
@@ -1,21 +1,17 @@
 #include "EntityAttributeType.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	EntityAttributeType::EntityAttributeType(const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features)
-		: TypeBase(ent_attr_type_name), AttributeBase(ent_attr_type_datatype, ent_attr_type_features), GUIDBase()
-	{
-	}
-
-	EntityAttributeType::EntityAttributeType(const xg::Guid& ent_attr_type_GUID, const std::string& ent_attr_type_name, const DataType& ent_attr_type_datatype, const EnumSet<Feature>& ent_attr_type_features)
-		: TypeBase(ent_attr_type_name), AttributeBase(ent_attr_type_datatype, ent_attr_type_features), GUIDBase(ent_attr_type_GUID)
-	{
-	}
-
-	EntityAttributeType::~EntityAttributeType()
-	{
-	}
-
+EntityAttributeType::EntityAttributeType(const std::string &ent_attr_type_name, const DataType &ent_attr_type_datatype, const EnumSet<Feature> &ent_attr_type_features)
+    : TypeBase(ent_attr_type_name), AttributeBase(ent_attr_type_datatype, ent_attr_type_features), GUIDBase()
+{
 }
+
+EntityAttributeType::EntityAttributeType(const xg::Guid &ent_attr_type_GUID, const std::string &ent_attr_type_name, const DataType &ent_attr_type_datatype, const EnumSet<Feature> &ent_attr_type_features)
+    : TypeBase(ent_attr_type_name), AttributeBase(ent_attr_type_datatype, ent_attr_type_features), GUIDBase(ent_attr_type_GUID)
+{
 }
+
+EntityAttributeType::~EntityAttributeType() = default;
+
+} // namespace inexor::entity_system
