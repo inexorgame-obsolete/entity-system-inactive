293607cc4b7c4314e2df8a78bb89b913200a35fb
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:24:09 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/base/type/TypeBase.cpp
+++ src/entity-system/model/base/type/TypeBase.cpp
@@ -1,24 +1,20 @@
 #include "TypeBase.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	TypeBase::TypeBase(const std::string& type_name)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(type_base_mutex);
-		type_title = type_name;
-	}
-
-	TypeBase::~TypeBase()
-	{
-	}
-
-	std::string TypeBase::get_type_name() const
-	{
-		// No mutex required as this is a read-only operation.
-		return type_title;
-	}
-
+TypeBase::TypeBase(const std::string &type_name)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(type_base_mutex);
+    type_title = type_name;
 }
+
+TypeBase::~TypeBase() = default;
+
+std::string TypeBase::get_type_name() const
+{
+    // No mutex required as this is a read-only operation.
+    return type_title;
 }
+
+} // namespace inexor::entity_system
