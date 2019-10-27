43b158fdd8c9bb2373d445eb3ac768a76ff9d2a1
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:07:55 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/managers/EntityManager.hpp
+++ src/entity-system/managers/EntityManager.hpp
@@ -7,64 +7,55 @@
 
 using namespace inexor::entity_system;
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class EntityManager
-	/// @brief A manager class which bundles
-	/// <ol>
-	///		<li> EntityTypeManager
-	///		<li> EntityInstanceManager
-	///		<li> EntityAttributeTypeManager
-	///		<li> EntityAttributeInstanceManager
-	/// </ol>
-	class EntityManager
-	{
-		public:
+/// @class EntityManager
+/// @brief A manager class which bundles
+/// <ol>
+///		<li> EntityTypeManager
+///		<li> EntityInstanceManager
+///		<li> EntityAttributeTypeManager
+///		<li> EntityAttributeInstanceManager
+/// </ol>
+class EntityManager
+{
+    public:
+    /// @brief Constructor.
+    /// @param entity_type_manager The entity type manager.
+    /// @param entity_instance_manager The entity instance manager.
+    /// @param entity_attribute_type_manager The entity attribute type manager.
+    /// @param entity_attribute_instance_manager The entity attribute instance manager.
+    EntityManager(std::shared_ptr<EntityTypeManager> entity_type_manager, std::shared_ptr<EntityInstanceManager> entity_instance_manager, std::shared_ptr<EntityAttributeTypeManager> entity_attribute_type_manager,
+                  std::shared_ptr<EntityAttributeInstanceManager> entity_attribute_instance_manager);
 
-			/// @brief Constructor.
-			/// @param entity_type_manager The entity type manager.
-			/// @param entity_instance_manager The entity instance manager.
-			/// @param entity_attribute_type_manager The entity attribute type manager.
-			/// @param entity_attribute_instance_manager The entity attribute instance manager.
-			EntityManager(
-				std::shared_ptr<EntityTypeManager> entity_type_manager,
-				std::shared_ptr<EntityInstanceManager> entity_instance_manager,
-				std::shared_ptr<EntityAttributeTypeManager> entity_attribute_type_manager,
-				std::shared_ptr<EntityAttributeInstanceManager> entity_attribute_instance_manager
-			);
+    /// @brief Destructor.
+    ~EntityManager();
 
-			/// @brief Destructor.
-			~EntityManager();
+    /// @brief Resets (in order of destruction) entity types, entity instances, attribute types and attribute instances.
+    /// @note Order of destruction:
+    /// <ol>
+    ///		<li> entity attribute instances
+    ///		<li> entity attribute types
+    ///		<li> entity instances
+    ///		<li> entity types
+    /// </ol>
+    void reset_attributes_and_types();
 
-			/// @brief Resets (in order of destruction) entity types, entity instances, attribute types and attribute instances.
-			/// @note Order of destruction:
-			/// <ol>
-			///		<li> entity attribute instances
-			///		<li> entity attribute types
-			///		<li> entity instances
-			///		<li> entity types
-			/// </ol>
-			void reset_attributes_and_types();
+    private:
+    /// The entity type manager.
+    std::shared_ptr<EntityTypeManager> entity_type_manager;
 
-		private:
+    /// The entity instance manager.
+    std::shared_ptr<EntityInstanceManager> entity_instance_manager;
 
-			/// The entity type manager.
-			std::shared_ptr<EntityTypeManager> entity_type_manager;
+    /// The entity attribute type manager.
+    std::shared_ptr<EntityAttributeTypeManager> entity_attribute_type_manager;
 
-			/// The entity instance manager.
-			std::shared_ptr<EntityInstanceManager> entity_instance_manager;
+    /// The entity attribute instance manager.
+    std::shared_ptr<EntityAttributeInstanceManager> entity_attribute_instance_manager;
 
-			/// The entity attribute type manager.
-			std::shared_ptr<EntityAttributeTypeManager> entity_attribute_type_manager;
+    /// The mutex of this class.
+    std::mutex entity_manager_mutex;
+};
 
-			/// The entity attribute instance manager.
-			std::shared_ptr<EntityAttributeInstanceManager> entity_attribute_instance_manager;
-
-			/// The mutex of this class.
-			std::mutex entity_manager_mutex;
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
--- src/entity-system/managers/RelationManager.hpp
+++ src/entity-system/managers/RelationManager.hpp
@@ -1,64 +1,55 @@
 #pragma once
 
-#include "relations/relation-instance-manager/RelationInstanceManager.hpp"
-#include "relations/relation-type-manager/RelationTypeManager.hpp"
 #include "relation-attributes/relation-attribute-instance-manager/RelationAttributeInstanceManager.hpp"
 #include "relation-attributes/relation-attribute-type-manager/RelationAttributeTypeManager.hpp"
+#include "relations/relation-instance-manager/RelationInstanceManager.hpp"
+#include "relations/relation-type-manager/RelationTypeManager.hpp"
 
 using namespace inexor::entity_system;
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class RelationManager
-	/// @brief A manager class for relations between entities.
-	class RelationManager
-	{
-		public:
+/// @class RelationManager
+/// @brief A manager class for relations between entities.
+class RelationManager
+{
+    public:
+    /// @brief Constructor.
+    /// @param entity_relation_type_manager The relation type manager.
+    /// @param entity_relation_instance_manager The relation instance manager.
+    /// @param entity_relation_attribute_type_manager The relation attribute type manager.
+    /// @param entity_relation_attribute_instance_manager The relation attribute instance manager.
+    RelationManager(std::shared_ptr<RelationTypeManager> entity_relation_type_manager, std::shared_ptr<RelationInstanceManager> entity_relation_instance_manager,
+                    std::shared_ptr<RelationAttributeTypeManager> entity_relation_attribute_type_manager, std::shared_ptr<RelationAttributeInstanceManager> entity_relation_attribute_instance_manager);
 
-			/// @brief Constructor.
-			/// @param entity_relation_type_manager The relation type manager.
-			/// @param entity_relation_instance_manager The relation instance manager.
-			/// @param entity_relation_attribute_type_manager The relation attribute type manager.
-			/// @param entity_relation_attribute_instance_manager The relation attribute instance manager.
-			RelationManager(
-				std::shared_ptr<RelationTypeManager> entity_relation_type_manager,
-				std::shared_ptr<RelationInstanceManager> entity_relation_instance_manager,
-				std::shared_ptr<RelationAttributeTypeManager> entity_relation_attribute_type_manager,
-				std::shared_ptr<RelationAttributeInstanceManager> entity_relation_attribute_instance_manager
-			);
+    /// @brief Destructor.
+    ~RelationManager();
 
-			/// @brief Destructor.
-			~RelationManager();
+    /// @brief Resets relation types, relation instances, relation attribute types and relation attribute instances.
+    /// @note Order of destruction:
+    /// <ol>
+    ///		<li> relation attribute instances
+    ///		<li> relation attribute types
+    ///		<li> relation instances
+    ///		<li> relation types
+    /// </ol>
+    void reset_relations_and_relation_attributes();
 
-			/// @brief Resets relation types, relation instances, relation attribute types and relation attribute instances.
-			/// @note Order of destruction:
-			/// <ol>
-			///		<li> relation attribute instances
-			///		<li> relation attribute types
-			///		<li> relation instances
-			///		<li> relation types
-			/// </ol>
-			void reset_relations_and_relation_attributes();
+    private:
+    /// The relation type manager.
+    std::shared_ptr<RelationTypeManager> relation_type_manager;
 
-		private:
+    /// The relation instance manager.
+    std::shared_ptr<RelationInstanceManager> relation_instance_manager;
 
-			/// The relation type manager.
-			std::shared_ptr<RelationTypeManager> relation_type_manager;
+    /// The relation attribute type manager.
+    std::shared_ptr<RelationAttributeTypeManager> relation_attribute_type_manager;
 
-			/// The relation instance manager.
-			std::shared_ptr<RelationInstanceManager> relation_instance_manager;
+    /// The relation attribute instance manager.
+    std::shared_ptr<RelationAttributeInstanceManager> relation_attribute_instance_manager;
 
-			/// The relation attribute type manager.
-			std::shared_ptr<RelationAttributeTypeManager> relation_attribute_type_manager;
+    /// The mutex of this class.
+    std::mutex relation_manager_mutex;
+};
 
-			/// The relation attribute instance manager.
-			std::shared_ptr<RelationAttributeInstanceManager> relation_attribute_instance_manager;
-
-			/// The mutex of this class.
-			std::mutex relation_manager_mutex;
-
-		};
-
-}
-}
+} // namespace inexor::entity_system
