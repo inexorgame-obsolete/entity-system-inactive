d5077476eb4c566ac18e9c51fd9e9af462fce9bc
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:37:24 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/model/entities/entity-instances/EntityInstance.cpp
+++ src/entity-system/model/entities/entity-instances/EntityInstance.cpp
@@ -1,97 +1,90 @@
 #include "EntityInstance.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	EntityInstance::EntityInstance(const EntityTypePtr& ent_type)
-		: GUIDBase(),
-		InstanceBase(ent_type)
-	{
-	}
-
-	EntityInstance::EntityInstance(const xg::Guid& ent_inst_GUID, const EntityTypePtr& ent_type)
-		: GUIDBase(ent_inst_GUID),
-		InstanceBase(ent_type)
-	{
-	}
-
-	EntityInstance::~EntityInstance()
-	{
-	}
-
-	EntityTypePtr EntityInstance::get_entity_type() const
-	{
-		// No mutex required as this is a read-only operation.
-		return get_type();
-	}
-
-	void EntityInstance::add_entity_attribute_instance(const EntityAttributeTypePtr& ent_attr_type, const EntityAttributeInstancePtr& ent_attr_inst)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_instance_mutex);
-
-		instances[ent_attr_type] = ent_attr_inst;
-	}
-
-	std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>> EntityInstance::get_instances() const
-	{
-		if(0 == instances.size())
-		{
-			// No mutex required as this is a read-only operation.
-			return std::nullopt;
-		}
-		// No mutex required as this is a read-only operation.
-		return std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>> { instances };
-	}
-
-	EntityAttributeInstancePtrOpt EntityInstance::get_attribute_instance(const std::string& attr_name)
-	{
-		for(auto& ent_attr_entry : instances)
-		{
-			if(attr_name == ent_attr_entry.first->get_type_name())
-			{
-				// No mutex required as this is a read-only operation.
-				return EntityAttributeInstancePtrOpt { ent_attr_entry.second };
-			}
-		}
-		// No mutex required as this is a read-only operation.
-		return std::nullopt;
-	}
-
-	EntityAttributeInstancePtrOpt EntityInstance::operator[](const std::string& attr_name)
-	{
-		for(auto& ent_attr_entry : instances)
-		{
-			if(attr_name == ent_attr_entry.first->get_type_name())
-			{
-				// No mutex required as this is a read-only operation.
-				return EntityAttributeInstancePtrOpt { ent_attr_entry.second };
-			}
-		}
-		// No mutex required as this is a read-only operation.
-		return std::nullopt;
-	}
-
-	void EntityInstance::set_own_value(const std::string& attr_name, DataValue value)
-	{
-		EntityAttributeInstancePtr attr = get_attribute_instance(attr_name).value();
-
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_instance_mutex);
-
-		attr->own_value.Set(value);
-	}
-
-	void EntityInstance::toggle(const std::string& attr_name)
-	{
-		EntityAttributeInstancePtr attr = get_attribute_instance(attr_name).value();
-		
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_instance_mutex);
-		
-		// Toggle the value of the bool container.
-		attr->own_value.Set(! attr->get<DataType::BOOL>());
-	}
-
+EntityInstance::EntityInstance(const EntityTypePtr &ent_type) : GUIDBase(), InstanceBase(ent_type)
+{
 }
+
+EntityInstance::EntityInstance(const xg::Guid &ent_inst_GUID, const EntityTypePtr &ent_type) : GUIDBase(ent_inst_GUID), InstanceBase(ent_type)
+{
 }
+
+EntityInstance::~EntityInstance() = default;
+
+EntityTypePtr EntityInstance::get_entity_type() const
+{
+    // No mutex required as this is a read-only operation.
+    return get_type();
+}
+
+void EntityInstance::add_entity_attribute_instance(const EntityAttributeTypePtr &ent_attr_type, const EntityAttributeInstancePtr &ent_attr_inst)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_instance_mutex);
+
+    instances[ent_attr_type] = ent_attr_inst;
+}
+
+/// TODO: don't return an optional but just an empty map!
+std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>> EntityInstance::get_instances() const
+{
+    if (instances.empty())
+    {
+        // No mutex required as this is a read-only operation.
+        return std::nullopt;
+    }
+    // No mutex required as this is a read-only operation.
+    return std::optional<std::unordered_map<EntityAttributeTypePtr, EntityAttributeInstancePtr>>{instances};
+}
+
+EntityAttributeInstancePtrOpt EntityInstance::get_attribute_instance(const std::string &attr_name)
+{
+    for (auto &ent_attr_entry : instances)
+    {
+        if (attr_name == ent_attr_entry.first->get_type_name())
+        {
+            // No mutex required as this is a read-only operation.
+            return EntityAttributeInstancePtrOpt{ent_attr_entry.second};
+        }
+    }
+    // No mutex required as this is a read-only operation.
+    return std::nullopt;
+}
+
+EntityAttributeInstancePtrOpt EntityInstance::operator[](const std::string &attr_name)
+{
+    for (auto &ent_attr_entry : instances)
+    {
+        if (attr_name == ent_attr_entry.first->get_type_name())
+        {
+            // No mutex required as this is a read-only operation.
+            return EntityAttributeInstancePtrOpt{ent_attr_entry.second};
+        }
+    }
+    // No mutex required as this is a read-only operation.
+    return std::nullopt;
+}
+
+void EntityInstance::set_own_value(const std::string &attr_name, const DataValue &value)
+{
+    EntityAttributeInstancePtr attr = get_attribute_instance(attr_name).value();
+
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_instance_mutex);
+
+    attr->own_value.Set(value);
+}
+
+void EntityInstance::toggle(const std::string &attr_name)
+{
+    EntityAttributeInstancePtr attr = get_attribute_instance(attr_name).value();
+
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_instance_mutex);
+
+    // Toggle the value of the bool container.
+    attr->own_value.Set(!attr->get<DataType::BOOL>());
+}
+
+} // namespace inexor::entity_system
--- src/entity-system/model/entities/entity-types/EntityType.cpp
+++ src/entity-system/model/entities/entity-types/EntityType.cpp
@@ -1,105 +1,98 @@
 #include "EntityType.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	EntityType::EntityType(const std::string& ent_type_name)
-		: GUIDBase(),
-		TypeBase(ent_type_name)
-	{
-	}
-
-	EntityType::EntityType(const xg::Guid& ent_type_GUID, const std::string& ent_type_name)
-		: GUIDBase(ent_type_GUID),
-		TypeBase(ent_type_name)
-	{
-	}
-
-	EntityType::~EntityType()
-	{
-	}
-
-	bool EntityType::has_attribute_type(const std::string& ent_attr_type_name)
-	{
-		// Look through all linked entity attribute types.
-		for(int i = 0; i < entity_attribute_types.size(); i++)
-		{
-			if(0 == entity_attribute_types[i]->get_type_name().compare(ent_attr_type_name))
-			{
-				// Yes, this entity attribute type is
-				// already linked to this entity type.
-				return true;
-			}
-		}
-		// No, this entity attribute type is
-		// not already linked to this entity type.
-		return false;
-	}
-
-	bool EntityType::has_attribute_type(const xg::Guid& ent_attr_type_GUID)
-	{
-		// Look through all linked entity attribute types.
-		for(int i = 0; i < entity_attribute_types.size(); i++)
-		{
-			// TODO: Does this even work? DEBUG!
-			if (ent_attr_type_GUID == entity_attribute_types[i]->get_GUID())
-			{
-				// Yes, this entity attribute type is
-				// already linked to this entity type.
-				return true;
-			}
-		}
-		// No, this entity attribute type is
-		// not already linked to this entity type.
-		return false;
-	}
-
-	bool EntityType::has_attribute_type(const EntityAttributeTypePtr& ent_attr_type)
-	{
-		// No mutex required as this is a read-only operation.
-		return has_attribute_type(ent_attr_type->get_type_name());
-	}
-
-	bool EntityType::link_attribute_type(const EntityAttributeTypePtr& ent_attr_type)
-	{
-		// Is the entity attribute type already linked to this entity type?
-		if(!has_attribute_type(ent_attr_type->get_type_name()))
-		{
-			// Use lock guard to ensure thread safety for this write operation!
-			std::lock_guard<std::mutex> lock(entity_type_mutex);
-			
-			// Link entity attribute type to this entity type.
-			entity_attribute_types.push_back(ent_attr_type);
-			return true;
-		}
-		return false;
-	}
-
-	std::size_t EntityType::get_linked_attributes_count() const
-	{
-		// No mutex required as this is a read-only operation.
-		return entity_attribute_types.size();
-	}
-
-	std::optional<EntityAttributeTypePtrList> EntityType::get_linked_attribute_types() const
-	{
-		if(0 == entity_attribute_types.size())
-		{
-			// There are no linked entity attribute instances.
-			return std::nullopt;
-		}
-
-		// No mutex required as this is a read-only operation.
-		return std::optional<EntityAttributeTypePtrList> { entity_attribute_types };
-	}
-
-	void EntityType::reset_linked_attribute_types()
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_type_mutex);
-
-		entity_attribute_types.clear();
-	}
-
+EntityType::EntityType(const std::string &ent_type_name) : GUIDBase(), TypeBase(ent_type_name)
+{
 }
+
+EntityType::EntityType(const xg::Guid &ent_type_GUID, const std::string &ent_type_name) : GUIDBase(ent_type_GUID), TypeBase(ent_type_name)
+{
 }
+
+EntityType::~EntityType() = default;
+
+bool EntityType::has_attribute_type(const std::string &ent_attr_type_name)
+{
+    // Look through all linked entity attribute types.
+    for (int i = 0; i < entity_attribute_types.size(); i++)
+    {
+        if (0 == entity_attribute_types[i]->get_type_name().compare(ent_attr_type_name))
+        {
+            // Yes, this entity attribute type is
+            // already linked to this entity type.
+            return true;
+        }
+    }
+    // No, this entity attribute type is
+    // not already linked to this entity type.
+    return false;
+}
+
+bool EntityType::has_attribute_type(const xg::Guid &ent_attr_type_GUID)
+{
+    // Look through all linked entity attribute types.
+    for (auto &entity_attribute_type : entity_attribute_types)
+    {
+        // TODO: Does this even work? DEBUG!
+        if (ent_attr_type_GUID == entity_attribute_type->get_GUID())
+        {
+            // Yes, this entity attribute type is
+            // already linked to this entity type.
+            return true;
+        }
+    }
+    // No, this entity attribute type is
+    // not already linked to this entity type.
+    return false;
+}
+
+bool EntityType::has_attribute_type(const EntityAttributeTypePtr &ent_attr_type)
+{
+    // No mutex required as this is a read-only operation.
+    return has_attribute_type(ent_attr_type->get_type_name());
+}
+
+bool EntityType::link_attribute_type(const EntityAttributeTypePtr &ent_attr_type)
+{
+    // Is the entity attribute type already linked to this entity type?
+    if (!has_attribute_type(ent_attr_type->get_type_name()))
+    {
+        // Use lock guard to ensure thread safety for this write operation!
+        std::lock_guard<std::mutex> lock(entity_type_mutex);
+
+        // Link entity attribute type to this entity type.
+        entity_attribute_types.push_back(ent_attr_type);
+        return true;
+    }
+    return false;
+}
+
+std::size_t EntityType::get_linked_attributes_count() const
+{
+    // No mutex required as this is a read-only operation.
+    return entity_attribute_types.size();
+}
+
+/// TODO: Don't return an optional but an vector which can be checked for emptiness
+std::optional<EntityAttributeTypePtrList> EntityType::get_linked_attribute_types() const
+{
+    if (entity_attribute_types.empty())
+    {
+        // There are no linked entity attribute instances.
+        return std::nullopt;
+    }
+
+    // No mutex required as this is a read-only operation.
+    return std::optional<EntityAttributeTypePtrList>{entity_attribute_types};
+}
+
+void EntityType::reset_linked_attribute_types()
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_type_mutex);
+
+    entity_attribute_types.clear();
+}
+
+} // namespace inexor::entity_system
