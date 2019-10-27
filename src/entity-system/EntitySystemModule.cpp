ceb3e7a608d3016fbb50e0dfdf1706fe21be780b
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:42:44 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/EntitySystemModule.cpp
+++ src/entity-system/EntitySystemModule.cpp
@@ -1,41 +1,34 @@
 #include "EntitySystemModule.hpp"
 
-namespace inexor {
-namespace entity_system {
+#include <utility>
 
-	EntitySystemModule::EntitySystemModule(
-		EntityManagerPtr entity_manager,
-		RelationManagerPtr relation_manager,
-		BuilderFactoriesPtr builder_factories,
-		LogManagerPtr log_manager
-	)
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_system_module);
+namespace inexor::entity_system {
 
-		this->entity_manager = entity_manager;
-		this->relation_manager = relation_manager;
-		this->builder_factories = builder_factories;
-		this->log_manager = log_manager;
-	}
+EntitySystemModule::EntitySystemModule(EntityManagerPtr entity_manager, RelationManagerPtr relation_manager, BuilderFactoriesPtr builder_factories, LogManagerPtr log_manager)
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_system_module);
 
-	EntitySystemModule::~EntitySystemModule()
-	{
-	}
-
-	void EntitySystemModule::reset_entity_system()
-	{
-		// Use lock guard to ensure thread safety during write operations!
-		std::lock_guard<std::mutex> lock(entity_system_module);
-
-		spdlog::get(LOGGER_NAME)->debug("Resetting entity system.");
-
-		// Reset (in order of destruction) relation attribute instances, relation attribute types, relation instances and relation types.
-		relation_manager->reset_relations_and_relation_attributes();
-
-		// Reset (in order of destruction) attribute instances, attribute types, entity instances and entity types.
-		entity_manager->reset_attributes_and_types();
-	}
-
+    this->entity_manager = std::move(entity_manager);
+    this->relation_manager = std::move(relation_manager);
+    this->builder_factories = std::move(builder_factories);
+    this->log_manager = std::move(log_manager);
 }
+
+EntitySystemModule::~EntitySystemModule() = default;
+
+void EntitySystemModule::reset_entity_system()
+{
+    // Use lock guard to ensure thread safety during write operations!
+    std::lock_guard<std::mutex> lock(entity_system_module);
+
+    spdlog::get(LOGGER_NAME)->debug("Resetting entity system.");
+
+    // Reset (in order of destruction) relation attribute instances, relation attribute types, relation instances and relation types.
+    relation_manager->reset_relations_and_relation_attributes();
+
+    // Reset (in order of destruction) attribute instances, attribute types, entity instances and entity types.
+    entity_manager->reset_attributes_and_types();
 }
+
+} // namespace inexor::entity_system
--- src/entity-system/EntitySystemModule.hpp
+++ src/entity-system/EntitySystemModule.hpp
@@ -5,75 +5,65 @@
 #include "entity-system/managers/RelationManager.hpp"
 #include "logging/managers/LogManager.hpp"
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class EntitySystemModule
-	/// @brief The entity system module.
-	class EntitySystemModule
-	{
-		public:
+/// @class EntitySystemModule
+/// @brief The entity system module.
+class EntitySystemModule
+{
+    public:
+    /// These using instructions help to shorten the following code.
+    using EntityManagerPtr = std::shared_ptr<EntityManager>;
+    using RelationManagerPtr = std::shared_ptr<RelationManager>;
+    using BuilderFactoriesPtr = std::shared_ptr<BuilderFactories>;
+    using LogManagerPtr = std::shared_ptr<logging::LogManager>;
 
-			/// These using instructions help to shorten the following code.
-			using EntityManagerPtr = std::shared_ptr<EntityManager>;
-			using RelationManagerPtr = std::shared_ptr<RelationManager>;
-			using BuilderFactoriesPtr = std::shared_ptr<BuilderFactories>;
-			using LogManagerPtr = std::shared_ptr<logging::LogManager>;
+    /// @brief Constructs the entity system module.
+    /// @note The dependencies of this class will be injected automatically.<br>
+    /// BOOST_DI_INJECT constructor parameters is limited to BOOST_DI_CFG_CTOR_LIMIT_SIZE,<br>
+    /// which by default is set to 10. Not more than 10 arguments can be passed to the DI constructor!<br>
+    /// https://boost-experimental.github.io/di/user_guide/index.html
+    /// @param entity_manager The manager for entity types, entity instances, attribute types and attribute instances.
+    /// @param relation_manager The manager for relation types, relation instances, relation attribute types and relation attribute instances.
+    /// @param builder_factories The manager for the builder factories.
+    /// @param log_manager The log manager.
+    EntitySystemModule(EntityManagerPtr entity_manager, RelationManagerPtr relation_manager, BuilderFactoriesPtr builder_factories, LogManagerPtr log_manager);
 
-			/// @brief Constructs the entity system module.
-			/// @note The dependencies of this class will be injected automatically.<br>
-			/// BOOST_DI_INJECT constructor parameters is limited to BOOST_DI_CFG_CTOR_LIMIT_SIZE,<br>
-			/// which by default is set to 10. Not more than 10 arguments can be passed to the DI constructor!<br>
-			/// https://boost-experimental.github.io/di/user_guide/index.html
-			/// @param entity_manager The manager for entity types, entity instances, attribute types and attribute instances.
-			/// @param relation_manager The manager for relation types, relation instances, relation attribute types and relation attribute instances.
-			/// @param builder_factories The manager for the builder factories.
-			/// @param log_manager The log manager.
-			EntitySystemModule(
-				EntityManagerPtr entity_manager,
-				RelationManagerPtr relation_manager,
-				BuilderFactoriesPtr builder_factories,
-				LogManagerPtr log_manager
-			);
+    /// @brief Destructs the entity system module.
+    ~EntitySystemModule();
 
-			/// @brief Destructs the entity system module.
-			~EntitySystemModule();
+    /// @brief Completely resets the entity system.
+    /// @note Order of destruction:
+    /// <ol>
+    ///   <li> relation attribute instances
+    ///   <li> relation attribute types
+    ///   <li> relation instances
+    ///   <li> relation types
+    ///   <li> entity attribute instances
+    ///   <li> entity attribute types
+    ///   <li> entity instances
+    ///   <li> entity types
+    /// </ol>
+    void reset_entity_system();
 
-			/// @brief Completely resets the entity system.
-			/// @note Order of destruction:
-			/// <ol>
-			///   <li> relation attribute instances
-			///   <li> relation attribute types
-			///   <li> relation instances
-			///   <li> relation types
-			///   <li> entity attribute instances
-			///   <li> entity attribute types
-			///   <li> entity instances
-			///   <li> entity types
-			/// </ol>
-			void reset_entity_system();
+    private:
+    /// The manager for entity types, entity instances, attribute types and attribute instances.
+    EntityManagerPtr entity_manager;
 
-		private:
+    /// The manager for relation types, relation instances, relation attribute types and relation attribute instances.
+    RelationManagerPtr relation_manager;
 
-			/// The manager for entity types, entity instances, attribute types and attribute instances.
-			EntityManagerPtr entity_manager;
+    /// The manager for the builder factories.
+    BuilderFactoriesPtr builder_factories;
 
-			/// The manager for relation types, relation instances, relation attribute types and relation attribute instances.
-			RelationManagerPtr relation_manager;
+    /// The manager for logging.
+    LogManagerPtr log_manager;
 
-			/// The manager for the builder factories.
-            BuilderFactoriesPtr builder_factories;
+    /// The logger name of this service.
+    static constexpr char LOGGER_NAME[] = "inexor.entsys";
 
-			/// The manager for logging.
-			LogManagerPtr log_manager;
+    /// The mutex of this class.
+    std::mutex entity_system_module;
+};
 
-			/// The logger name of this service.
-			static constexpr char LOGGER_NAME[] = "inexor.entsys";
-
-			/// The mutex of this class.
-			std::mutex entity_system_module;
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
