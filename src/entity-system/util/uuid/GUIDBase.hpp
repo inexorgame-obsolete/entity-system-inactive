d3b5131c722d3ed1c9b162fbe2dfd472940bb3ec
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-27 15:15:12 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/entity-system/util/uuid/GUIDBase.hpp
+++ src/entity-system/util/uuid/GUIDBase.hpp
@@ -1,47 +1,41 @@
 #pragma once
 
-#include <mutex>
 #include <crossguid/guid.hpp>
+#include <mutex>
 
-namespace inexor {
-namespace entity_system {
+namespace inexor::entity_system {
 
-	/// @class GUIDBase
-	/// @brief A base class for globally unique identifiers (GUIDs).
-	/// @note This implementation is based on crossguid.
-	/// https://github.com/graeme-hill/crossguid
-	class GUIDBase
-	{
-		private:
+/// @class GUIDBase
+/// @brief A base class for globally unique identifiers (GUIDs).
+/// @note This implementation is based on crossguid.
+/// https://github.com/graeme-hill/crossguid
+class GUIDBase
+{
+    private:
+    /// The mutex of this class.
+    std::mutex guid_base_mutex;
 
-			/// The mutex of this class.
-			std::mutex guid_base_mutex;
+    /// The GUID of this class.
+    xg::Guid globally_unique_identifier;
 
-			/// The GUID of this class.
-			xg::Guid globally_unique_identifier;
+    protected:
+    /// @brief Constructor.
+    GUIDBase();
 
-		protected:
+    /// @brief Copy Constructor.
+    /// @param param_GUID The GUID which will be copied.
+    explicit GUIDBase(const xg::Guid &param_GUID);
 
-			/// @brief Constructor.
-			GUIDBase();
+    /// @brief Destructor.
+    ~GUIDBase();
 
-			/// @brief Copy Constructor.
-			/// @param param_GUID The GUID which will be copied.
-			GUIDBase(const xg::Guid& param_GUID);
+    public:
+    /// @brief Returns the GUID.
+    /// @return The GUID.
+    [[nodiscard]] xg::Guid get_GUID() const;
 
-			/// @brief Destructor.
-			~GUIDBase();
+    /// @brief Generates a new GUID.
+    void generate_new_GUID();
+};
 
-		public:
-
-			/// @brief Returns the GUID.
-			/// @return The GUID.
-			xg::Guid get_GUID() const;
-
-			/// @brief Generates a new GUID.
-			void generate_new_GUID();
-
-	};
-
-}
-}
+} // namespace inexor::entity_system
