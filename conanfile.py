2b5e37ed9ac9d53d3251d72d58357f89d72c2b17
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-29 00:19:35 +0100

    Try to fix dynamic linking on macosx

--- conanfile.py
+++ conanfile.py
@@ -54,11 +54,14 @@ class InexorConan(ConanFile):
     }
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
-        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+        # Copies all dll files from packages bin folder to my "bin" folder (win)
+        self.copy("*.dll", dst="bin", src="bin")
+        # Copies all dylib files from packages lib folder to my "lib" folder (macosx)
+        self.copy("*.dylib*", dst="lib", src="lib") # From lib to lib
+        # Copies all so files from packages lib folder to my "lib" folder (linux)
+        self.copy("*.so*", dst="lib", src="lib") # From lib to lib
 
-        
-    def build(self):
+def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
