8422b9aec33f95c4144856986c4f3972349060a2
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-28 21:49:24 +0100

    Fixed wrong includes sort order

--- src/logging/managers/LogManager.hpp
+++ src/logging/managers/LogManager.hpp
@@ -9,10 +9,12 @@
 #include "type-system/factories/inout/logger/LoggerFactory.hpp"
 #include "type-system/providers/inout/logger/LoggerEntityTypeProvider.hpp"
 
+// clang-format off
+#include "spdlog/spdlog.h"
 #include "spdlog/async.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
+// clang-format on
 
 namespace inexor::logging {
 
