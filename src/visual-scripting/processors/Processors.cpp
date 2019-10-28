b269be419fdd515d0bb873cab519ccef09f51d66
Andreas Schaeffer <hanack@nooblounge.net>
2019-10-28 21:33:18 +0100

    Performed code formatting using clang-format and applied fixes proposed by clang-tidy

--- src/visual-scripting/processors/Processors.cpp
+++ src/visual-scripting/processors/Processors.cpp
@@ -1,44 +1,36 @@
 #include "Processors.hpp"
 
-namespace inexor {
-namespace visual_scripting {
+#include <utility>
 
-	Processors::Processors(
-		ProcessorRegistryPtr processor_registry,
-		GeneratorProcessorsPtr generator_processors,
-		InOutProcessorsPtr inout_processors,
-		LogicalProcessorsPtr logical_processors,
-		MathProcessorsPtr math_processors
-	)
-	{
-		this->processor_registry = processor_registry;
-		this->generator_processors = generator_processors;
-		this->inout_processors = inout_processors;
-		this->logical_processors = logical_processors;
-		this->math_processors = math_processors;
-	}
+namespace inexor::visual_scripting {
 
-	Processors::~Processors()
-	{
-	}
-
-	void Processors::init()
-	{
-		this->processor_registry->init();
-		this->generator_processors->init();
-		this->inout_processors->init();
-		this->logical_processors->init();
-		this->math_processors->init();
-	}
-
-	void Processors::shutdown()
-	{
-//		this->math_processors->shutdown();
-//		this->logical_processors->shutdown();
-//		this->inout_processors->shutdown();
-		this->generator_processors->shutdown();
-//		this->processor_registry->shutdown();
-	}
-
+Processors::Processors(ProcessorRegistryPtr processor_registry, GeneratorProcessorsPtr generator_processors, InOutProcessorsPtr inout_processors, LogicalProcessorsPtr logical_processors, MathProcessorsPtr math_processors)
+{
+    this->processor_registry = std::move(processor_registry);
+    this->generator_processors = std::move(generator_processors);
+    this->inout_processors = std::move(inout_processors);
+    this->logical_processors = std::move(logical_processors);
+    this->math_processors = std::move(math_processors);
 }
+
+Processors::~Processors() = default;
+
+void Processors::init()
+{
+    this->processor_registry->init();
+    this->generator_processors->init();
+    this->inout_processors->init();
+    this->logical_processors->init();
+    this->math_processors->init();
 }
+
+void Processors::shutdown()
+{
+    //		this->math_processors->shutdown();
+    //		this->logical_processors->shutdown();
+    //		this->inout_processors->shutdown();
+    this->generator_processors->shutdown();
+    //		this->processor_registry->shutdown();
+}
+
+} // namespace inexor::visual_scripting
--- src/visual-scripting/processors/Processors.hpp
+++ src/visual-scripting/processors/Processors.hpp
@@ -1,69 +1,58 @@
 #pragma once
 
 #include "visual-scripting/managers/ProcessorRegistry.hpp"
+#include "visual-scripting/model/Processor.hpp"
 #include "visual-scripting/processors/generators/GeneratorProcessors.hpp"
 #include "visual-scripting/processors/inout/InOutProcessors.hpp"
 #include "visual-scripting/processors/logical/LogicalProcessors.hpp"
 #include "visual-scripting/processors/math/MathProcessors.hpp"
-#include "visual-scripting/model/Processor.hpp"
 
-namespace inexor {
-namespace visual_scripting {
+namespace inexor::visual_scripting {
 
-	using ProcessorRegistryPtr = std::shared_ptr<ProcessorRegistry>;
-	using GeneratorProcessorsPtr = std::shared_ptr<GeneratorProcessors>;
-	using InOutProcessorsPtr = std::shared_ptr<InOutProcessors>;
-	using LogicalProcessorsPtr = std::shared_ptr<LogicalProcessors>;
-	using MathProcessorsPtr = std::shared_ptr<MathProcessors>;
+using ProcessorRegistryPtr = std::shared_ptr<ProcessorRegistry>;
+using GeneratorProcessorsPtr = std::shared_ptr<GeneratorProcessors>;
+using InOutProcessorsPtr = std::shared_ptr<InOutProcessors>;
+using LogicalProcessorsPtr = std::shared_ptr<LogicalProcessors>;
+using MathProcessorsPtr = std::shared_ptr<MathProcessors>;
 
-    /// @class Processors
-    /// @brief Provides the processors.
-	class Processors
-	{
-		public:
+/// @class Processors
+/// @brief Provides the processors.
+class Processors
+{
+    public:
+    /// @brief Constructs the processors.
+    /// @note The dependencies of this class will be injected automatically.
+    /// @param processor_registry The registry for processors.
+    /// @param generator_processors The processors for generators like counters and timers.
+    /// @param inout_processors The processors for input and output.
+    /// @param logical_processors The processors for logical operations.
+    /// @param math_processors The processors for mathematical operations.
+    Processors(ProcessorRegistryPtr processor_registry, GeneratorProcessorsPtr generator_processors, InOutProcessorsPtr inout_processors, LogicalProcessorsPtr logical_processors, MathProcessorsPtr math_processors);
 
-            /// @brief Constructs the processors.
-            /// @note The dependencies of this class will be injected automatically.
-            /// @param processor_registry The registry for processors.
-            /// @param generator_processors The processors for generators like counters and timers.
-            /// @param inout_processors The processors for input and output.
-            /// @param logical_processors The processors for logical operations.
-            /// @param math_processors The processors for mathematical operations.
-			Processors(
-				ProcessorRegistryPtr processor_registry,
-				GeneratorProcessorsPtr generator_processors,
-				InOutProcessorsPtr inout_processors,
-				LogicalProcessorsPtr logical_processors,
-				MathProcessorsPtr math_processors
-			);
+    /// Destructor.
+    ~Processors();
 
-            /// Destructor.
-			~Processors();
+    /// Initialization of the processors.
+    void init();
 
-			/// Initialization of the processors.
-			void init();
+    /// Shut down the processors.
+    void shutdown();
 
-			/// Shut down the processors.
-			void shutdown();
+    private:
+    /// The registry for processors.
+    ProcessorRegistryPtr processor_registry;
 
-		private:
+    /// The processors for generators like counters and timers.
+    GeneratorProcessorsPtr generator_processors;
 
-			/// The registry for processors.
-			ProcessorRegistryPtr processor_registry;
+    /// The processors for input and output.
+    InOutProcessorsPtr inout_processors;
 
-			/// The processors for generators like counters and timers.
-			GeneratorProcessorsPtr generator_processors;
+    /// The processors for logical operations.
+    LogicalProcessorsPtr logical_processors;
 
-			/// The processors for input and output.
-			InOutProcessorsPtr inout_processors;
+    /// The processors for mathematical operations.
+    MathProcessorsPtr math_processors;
+};
 
-			/// The processors for logical operations.
-			LogicalProcessorsPtr logical_processors;
-
-			/// The processors for mathematical operations.
-			MathProcessorsPtr math_processors;
-
-	};
-
-}
-}
+} // namespace inexor::visual_scripting
