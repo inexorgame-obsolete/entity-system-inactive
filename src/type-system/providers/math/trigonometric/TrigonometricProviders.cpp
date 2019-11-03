35e4c5c9c1b35bedac9dde868450a4afcdaf3b58
Andreas Schaeffer <hanack@nooblounge.net>
2019-11-03 19:26:39 +0100

    Removed TanEntityTypeProvider and TanFactory

--- src/type-system/providers/math/trigonometric/TrigonometricProviders.cpp
+++ src/type-system/providers/math/trigonometric/TrigonometricProviders.cpp
@@ -4,11 +4,10 @@
 
 namespace inexor::entity_system::type_system {
 
-TrigonometricProviders::TrigonometricProviders(SinEntityTypeProviderPtr sin_entity_type_provider, CosEntityTypeProviderPtr cos_entity_type_provider, TanEntityTypeProviderPtr tan_entity_type_provider)
+TrigonometricProviders::TrigonometricProviders(SinEntityTypeProviderPtr sin_entity_type_provider, CosEntityTypeProviderPtr cos_entity_type_provider)
 {
     this->sin_entity_type_provider = std::move(sin_entity_type_provider);
     this->cos_entity_type_provider = std::move(cos_entity_type_provider);
-    this->tan_entity_type_provider = std::move(tan_entity_type_provider);
 }
 
 TrigonometricProviders::~TrigonometricProviders() = default;
--- src/type-system/providers/math/trigonometric/TrigonometricProviders.hpp
+++ src/type-system/providers/math/trigonometric/TrigonometricProviders.hpp
@@ -2,7 +2,6 @@
 
 #include "type-system/providers/math/trigonometric/CosEntityTypeProvider.hpp"
 #include "type-system/providers/math/trigonometric/SinEntityTypeProvider.hpp"
-#include "type-system/providers/math/trigonometric/TanEntityTypeProvider.hpp"
 
 #include <memory>
 
@@ -10,7 +9,6 @@ namespace inexor::entity_system::type_system {
 
 using SinEntityTypeProviderPtr = std::shared_ptr<SinEntityTypeProvider>;
 using CosEntityTypeProviderPtr = std::shared_ptr<CosEntityTypeProvider>;
-using TanEntityTypeProviderPtr = std::shared_ptr<TanEntityTypeProvider>;
 
 /// @class TrigonometricProviders
 /// @brief The trigonometric entity type providers.
@@ -21,8 +19,7 @@ class TrigonometricProviders
     /// @note The dependencies of this class will be injected automatically.
     /// @param sin_entity_type_provider Provides the entity type SIN.
     /// @param cos_entity_type_provider Provides the entity type COS.
-    /// @param tan_entity_type_provider Provides the entity type TAN.
-    TrigonometricProviders(SinEntityTypeProviderPtr sin_entity_type_provider, CosEntityTypeProviderPtr cos_entity_type_provider, TanEntityTypeProviderPtr tan_entity_type_provider);
+    TrigonometricProviders(SinEntityTypeProviderPtr sin_entity_type_provider, CosEntityTypeProviderPtr cos_entity_type_provider);
 
     /// Destructor.
     ~TrigonometricProviders();
@@ -36,9 +33,6 @@ class TrigonometricProviders
 
     /// Provides the entity type COS.
     CosEntityTypeProviderPtr cos_entity_type_provider;
-
-    /// Provides the entity type TAN.
-    TanEntityTypeProviderPtr tan_entity_type_provider;
 };
 
 } // namespace inexor::entity_system::type_system
