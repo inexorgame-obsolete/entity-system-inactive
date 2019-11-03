8fa908c178bac29f764c49cf9381d78f4d36a529
Andreas Schaeffer <hanack@nooblounge.net>
2019-11-03 19:14:25 +0100

    Added feature "NONE"

--- src/entity-system/model/data/DataTypes.hpp
+++ src/entity-system/model/data/DataTypes.hpp
@@ -12,6 +12,6 @@ enum DataType { BOOL = 0, INT, BIG_INT, DOUBLE, FLOAT, STRING /*, VEC3, VEC4 */ 
 
 /// @class Features
 /// @brief The features on a attribute.
-enum class Features { ENGINE_INPUT = 1 << 0, INPUT = 1 << 1, OUTPUT = 1 << 2, SYNC_SERVER = 1 << 3, SYNC_BROADCAST_CLIENTS = 1 << 4, SERVER_CONFIG = 1 << 5, CLIENT_CONFIG = 1 << 6 };
+enum class Features { NONE = 0, ENGINE_INPUT = 1 << 0, INPUT = 1 << 1, OUTPUT = 1 << 2, SYNC_SERVER = 1 << 3, SYNC_BROADCAST_CLIENTS = 1 << 4, SERVER_CONFIG = 1 << 5, CLIENT_CONFIG = 1 << 6 };
 
 } // namespace inexor::entity_system
