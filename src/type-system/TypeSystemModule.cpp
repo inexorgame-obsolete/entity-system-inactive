25f225940d84f0c7eea89d143605e53edc864691
Andreas Schaeffer <hanack@nooblounge.net>
2019-11-03 19:33:12 +0100

    Load entity types from JSON

--- src/type-system/TypeSystemModule.cpp
+++ src/type-system/TypeSystemModule.cpp
@@ -4,8 +4,9 @@
 
 namespace inexor::entity_system::type_system {
 
-TypeSystemModule::TypeSystemModule(TypeSystemProvidersPtr type_system_providers, TypeSystemFactoriesPtr type_system_factories)
+TypeSystemModule::TypeSystemModule(TypeSystemInitializerPtr type_system_initializer, TypeSystemProvidersPtr type_system_providers, TypeSystemFactoriesPtr type_system_factories)
 {
+    this->type_system_initializer = std::move(type_system_initializer);
     this->type_system_providers = std::move(type_system_providers);
     this->type_system_factories = std::move(type_system_factories);
 }
@@ -16,6 +17,7 @@ TypeSystemModule::~TypeSystemModule()
 
 void TypeSystemModule::init()
 {
+    type_system_initializer->init();
     type_system_providers->init();
     type_system_factories->init();
 }
